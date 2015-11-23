0ace9b387b576b500b49eb34d89ec439ee84792f
Michael Richer <msricher1993@gmail.com>
2015-11-23 15:52:26 -0500

    Yet another improvement that still doesn't fix the problem

--- geminals/Geminal.py
+++ geminals/Geminal.py
@@ -119,7 +119,7 @@ class Geminal(object):
             for ps in [2*p, 2*p + 1]:
                 for q in range(self.norbs):
                     for qs in [2*q, 2*q + 1]:
-                        phi_new = excite_single(phi, ps, qs)
+                        phi_new = self.excite(phi, ps, qs)
                         result += one[p,q]*self.overlap(phi_new, C)
         for p in range(self.norbs):
             for ps in [2*p, 2*p + 1]:
@@ -129,7 +129,7 @@ class Geminal(object):
                             for ss in [2*s, 2*s + 1]:
                                 for q in range(self.norbs):
                                     for qs in [2*q, 2*q + 1]:
-                                        phi_new = self.excite_double(phi, ps, rs, ss, qs)
+                                        phi_new = self.excite(phi, ps, rs, ss, qs)
                                         result += 0.5*two[p,q,r,s]*self.overlap(phi_new, C)
         return result
 
@@ -159,6 +159,32 @@ class Geminal(object):
         numerator = numerator.dot(numerator)
         denominator = Svec.dot(Svec)
         return numerator/denominator
+
+
+    @classmethod
+    def excite(cls, phi, *args):
+        assert (len(args) % 2) == 0, \
+            "An equal number of annihilations and creations must occur."
+        halfway = len(args)//2
+
+        for i in args[:halfway]:
+            if cls.occupied(phi, i):
+                phi &= ~(1 << i)
+            else:
+                return 0
+
+        for i in args[halfway:]:
+            if cls.occupied(phi, i):
+                return 0
+            else:
+                phi |= 1 << i
+
+        return phi
+
+
+    @staticmethod
+    def occupied(phi, orbital):
+        return bool(phi & (1 << orbital))
 
 
 # vim: set textwidth=90 :
--- geminals/test/test_Geminal.py
+++ geminals/test/test_Geminal.py
@@ -5,18 +5,33 @@ from __future__ import absolute_import, division, print_function
 from Geminal import *
 from HortonWrapper import *
 
-
+# Test permanent evaluation
 matrix = np.array([ [1,2,3],
                     [4,5,6],
                     [7,8,9] ])
 assert Geminal.permanent(matrix) == 450
 
+# Test excitations
+assert Geminal.excite(0b001, 0, 2) == 0b100
+assert Geminal.excite(0b000011, 0, 1, 4, 5) == 0b110000
 
+# Test annihilation --> vacuum on virtual orbitals
+assert Geminal.excite(0b010, 0, 2) == 0
+assert Geminal.excite(0b000101, 0, 1, 4, 5) == 0
+
+# Test creation --> vacuum on occupied orbitals
+assert Geminal.excite(0b101, 0, 2) == 0
+assert Geminal.excite(0b110011, 0, 1, 4, 5) == 0
+
+#
+# Test APIG optimization
+#
+
+# Define user input
 fn = 'test/li2.xyz'
 basis = 'sto-3g'
 nocc = 3
-inpt = from_horton(fn=fn, basis=basis, nocc=nocc, guess=None)
-maxiter = 5
+maxiter = 10
 solver=quasinewton
 options = { 'options': { 'maxiter':maxiter,
                          'disp': True,
@@ -25,33 +40,28 @@ options = { 'options': { 'maxiter':maxiter,
             'method': 'krylov',
           }
 
+# Make geminal, and guess, from HORTON's AP1roG module
+inpt = from_horton(fn=fn, basis=basis, nocc=nocc, guess=None)
 gem = Geminal(nocc, inpt['basis'].nbasis)
 guess = np.zeros(gem.norbs*gem.npairs + 1)
 guess[0] += inpt['energy']
 guess[1:] += inpt['coeffs'].ravel()
 
+# Projected Slater determinants are all single and double excitations
 dets = []
 ground = min(gem.pspace)
 for i in range(2*gem.npairs):
     for j in range(2*gem.npairs, 2*gem.norbs):
-        dets.append(gem.excite_single(ground, i, j))
+        dets.append(gem.excite(ground, i, j))
 for i in range(0, 2*gem.npairs, 2):
     for j in range(2*gem.npairs, 2*gem.norbs, 2):
-        dets.append(gem.excite_double(ground, i, i+1, j, j+1))
-#dets.extend(gem.pspace)
+        dets.append(gem.excite(ground, i, i+1, j, j+1))
 dets = list(set(dets))
 if 0 in dets:
     dets.remove(0)
 
-coeff = np.zeros((gem.npairs, gem.norbs))
-coeff[:,:gem.npairs] += np.eye(gem.npairs)
-coeff[:,gem.npairs:] += 0.1*np.random.rand(gem.npairs, (gem.norbs - gem.npairs)) - 0.05
-
-guess = np.zeros(1 + gem.norbs*gem.npairs)
-guess[0] = -14.5
-guess[1:] += coeff.ravel()
-
-result = gem(guess, *inpt['ham'], dets=dets, solver=solver, options=options)
+# Run the optimization
+#result = gem(guess, *inpt['ham'], dets=dets, solver=solver, options=options)
 
 
 # vim: set textwidth=90 :
