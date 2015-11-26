286999addc4fd77fe8af625a2d559b2b7bd0aac5
Taewon D. Kim <kimt33@mcmaster.ca>
2015-11-26 13:32:31 -0500

    Add pair operations in slater_det.py
    What:
      1. Added remove_pairs, add_pairs, excite_pairs, which operate on both alpha and beta
         orbital pairs
      2. Add tests
    Why:
      1. Convenience when working with spatial orbitals
    Issue:
      1. Adding same orbital multiple times results in weird behaviour
         e.g.
         add_orbs(0b001, 1) = 0b11
         add_orbs(0b001, 1, 1) = 0
         add_orbs(0b001, 1, 1, 1) = 0b10
      2. Same problem for pairs
      3. Pair operations assume a certain structure on the alpha and beta spin orbital
         ordering. There is no way to check for this structure. I think we need to create
         a class to fix the SD structure

--- geminals/slater_det.py
+++ geminals/slater_det.py
@@ -61,7 +61,7 @@ def add_orbs(bin_sd, *indices):
     return bin_sd
 
 def excite(bin_sd, *indices):
-    """ Doubly excites Slater determinant
+    """ Excites Slater determinant
 
     Parameters
     ----------
@@ -69,20 +69,10 @@ def excite(bin_sd, *indices):
         Integer that in binary form describes the orbitals used to make the
         Slater determinant
 
-    index_a : int
-        Index for the occupied orbital
-        Start from 0
-
-    index_b : int
-        Index for the occupied orbital
-        Start from 0
-
-    index_v : int
-        Index for the virtual orbital
-        Start from 0
-
-    index_v : int
-        Index for the virtual orbital
+    indices : list of int
+        First half contains the indices for the occupied orbitals
+        Second half contains the indices for the unoccupied orbitals
+        Occupied orbitals are replaced by the unoccupied orbitals
         Start from 0
 
     Returns
@@ -90,6 +80,12 @@ def excite(bin_sd, *indices):
     bin_sd : int
         Integer that in binary form describes the orbitals used to make the
         excited Slater determinant
+
+    Raises
+    ------
+    AssertionError
+        If indices do not have even number of elements (i.e. cannot be divided
+        in half)
     """
     assert (len(indices) % 2) == 0, \
         "An equal number of annihilations and creations must occur."
@@ -100,6 +96,136 @@ def excite(bin_sd, *indices):
     bin_sd = remove_orbs(bin_sd, *indices[:halfway])
     return bin_sd
 
+
+def remove_pairs(bin_sd, *indices):
+    """ Removes alpha and beta orbitals from Slater determinent specified by indices
+
+    Parameters
+    ----------
+    bin_sd : int
+        Integer that in binary form describes the spin orbitals used to make the
+        Slater determinant
+        Even indices from the right (starting from 0) describe the alpha orbitals
+        Odd indices from the right (starting from 0) describe the beta orbitals
+
+    indices : list of int
+        List of indices that describes the spatial orbital that will be removed
+        Start from 0
+
+    Returns
+    -------
+    bin_sd : int
+        Integer that in binary form describes the Slater determinant that with
+        the specified orbitals removed
+        Zero if the selected orbitals are not occupied
+
+    Example
+    -------
+    remove_pairs(bin_sd, 0) would remove the 0th spatial orbital, i.e. 0th and
+    1st spin orbital, or 0th alpha and 0th beta orbitals.
+
+    Note
+    ----
+    This code ASSUMES a certain structure for the Slater determinant indexing.
+    If the alpha and beta orbitals are ordered differently, then this code will
+    break
+    """
+    for spatial_index in indices:
+        alpha_index = spatial_index*2
+        beta_index = spatial_index*2 + 1
+        bin_sd = remove_orbs(bin_sd, alpha_index, beta_index)
+    return bin_sd
+
+def add_pairs(bin_sd, *indices):
+    """ Adds alpha and beta orbitals from Slater determinent specified by indices
+
+    Parameters
+    ----------
+    bin_sd : int
+        Integer that in binary form describes the spin orbitals used to make the
+        Slater determinant
+        Even indices from the right (starting from 0) describe the alpha orbitals
+        Odd indices from the right (starting from 0) describe the beta orbitals
+
+    indices : list of int
+        List of indices that describes the spatial orbital that will be addd
+        Start from 0
+
+    Returns
+    -------
+    bin_sd : int
+        Integer that in binary form describes the Slater determinant that with
+        the specified orbitals addd
+        Zero if the selected orbitals are not occupied
+
+    Example
+    -------
+    add_pairs(bin_sd, 0) would add the 0th spatial orbital, i.e. 0th and
+    1st spin orbital, or 0th alpha and 0th beta orbitals.
+
+    Note
+    ----
+    This code ASSUMES a certain structure for the Slater determinant indexing.
+    If the alpha and beta orbitals are ordered differently, then this code will
+    break
+    """
+    for spatial_index in indices:
+        alpha_index = spatial_index*2
+        beta_index = spatial_index*2 + 1
+        bin_sd = add_orbs(bin_sd, alpha_index, beta_index)
+    return bin_sd
+
+def excite_pairs(bin_sd, *indices):
+    """ Excites the alpha beta pairs of a Slater determinant
+
+    Parameters
+    ----------
+    bin_sd : int
+        Integer that in binary form describes the spin orbitals used to make the
+        Slater determinant
+        Even indices from the right (starting from 0) describe the alpha orbitals
+        Odd indices from the right (starting from 0) describe the beta orbitals
+
+    indices : list of int
+        First half contains the indices for the occupied spatial orbitals
+        Second half contains the indices for the unoccupied spatial orbitals
+        Occupied spatial orbitals are replaced by the unoccupied spatial orbitals
+        Start from 0
+
+    Returns
+    -------
+    bin_sd : int
+        Integer that in binary form describes the spin orbitals used to make the
+        excited Slater determinant
+
+    Raises
+    ------
+    AssertionError
+        If indices do not have even number of elements (i.e. cannot be divided
+        in half)
+
+    Example
+    -------
+    excite_pairs(bin_sd, 0, 1) would replace the 0th spatial orbital with the
+    1st spatial orbital, i.e. 0th and 1st spin orbital, or 0th alpha and 0th
+    beta orbitals are replaced with the 2nd and 3rd spin orbitals, or the 1st
+    alpha and beta orbitals
+
+    Note
+    ----
+    This code ASSUMES a certain structure for the Slater determinant indexing.
+    If the alpha and beta orbitals are ordered differently, then this code will
+    break
+
+    """
+    assert (len(indices) % 2) == 0, \
+        "An equal number of annihilations and creations must occur."
+    halfway = len(indices)//2
+    # Add virtuals (Needs to be first because if it was last, we can still add virtuals)
+    bin_sd = add_pairs(bin_sd, *indices[halfway:])
+    # Remove occupieds
+    bin_sd = remove_pairs(bin_sd, *indices[:halfway])
+    return bin_sd
 
 def is_occupied(bin_sd, orb_index):
     """ Checks if orbital is used in the slater determinant
--- geminals/test/test_slater_det.py
+++ geminals/test/test_slater_det.py
@@ -2,6 +2,7 @@
 """
 
 from slater_det import remove_orbs, add_orbs, excite, is_occupied
+from slater_det import remove_pairs, add_pairs, excite_pairs
 
 def test_remove_orbs():
     """ Test remove_orbs function
@@ -41,8 +42,10 @@ def test_add_orbs():
     assert add_orbs(0b01000, 1, 2) == 0b1110
     assert add_orbs(0b01000, 2, 1) == 0b1110
     # Add orb multiple times
-    assert add_orbs(0b01010, 2, 2) == 0
     assert add_orbs(0b01100, 1, 1) == 0
+    assert add_orbs(0b01010, 2, 2) == 0
+    assert add_orbs(0b01100, 1, 1, 1) == 0
+    assert add_orbs(0b01010, 2, 2, 2) == 0
     # Large index
     n = 9999999
     assert add_orbs(0b1, n) == 0b1 | 1 << n
@@ -61,6 +64,72 @@ def test_excite():
     assert excite(0b1001, 3, 0) == 0
     # Large index
     assert excite(0b1001, 3, 999999) == 0b0001 | 1 << 999999
+
+def test_remove_pairs():
+    """ Test remove_pairs function
+    """
+    # Remove pairs that are not occupied
+    assert remove_pairs(0b001100, 0) == 0
+    assert remove_pairs(0b001100, 2) == 0
+    assert remove_pairs(0b001100, 3) == 0
+    # Remove pairs that are partially occupied
+    assert remove_pairs(0b001010, 0) == 0
+    assert remove_pairs(0b000101, 0) == 0
+    # Remove pairs that are occupied
+    assert remove_pairs(0b001111, 1) == 0b11
+    # Remove multiple pairs
+    assert remove_pairs(0b11111, 0, 1) == 0b10000
+    assert remove_pairs(0b11111, 1, 0) == 0b10000
+    # Remove pairs multiple times
+    assert remove_pairs(0b001111, 1, 1) == 0
+    assert remove_pairs(0b001111, 1, 1, 1) == 0
+    # Large index
+    n = 999999
+    assert remove_pairs(0b1 | 0b11 << n*2, n) == 0b1
+
+def test_add_pairs():
+    """ Test add_pairs function
+    """
+    # Add pairs that are not occupied
+    assert add_pairs(0b001100, 0) == 0b1111
+    assert add_pairs(0b001100, 2) == 0b111100
+    assert add_pairs(0b001100, 3) == 0b11001100
+    assert add_pairs(0b001100, 4) == 0b1100001100
+    # Add pairs that are occupied
+    assert add_pairs(0b001100, 1) == 0
+    # Add pairs that are partially occupied
+    assert add_pairs(0b001001, 0) == 0
+    assert add_pairs(0b001001, 1) == 0
+    # Add multiple pairs
+    assert add_pairs(0b01100, 0, 2) == 0b111111
+    assert add_pairs(0b01100, 2, 0) == 0b111111
+    # Add pairs multiple times
+    assert add_pairs(0b00011, 0, 0) == 0
+    assert add_pairs(0b00011, 1, 1) == 0
+    assert add_pairs(0b00011, 0, 0, 0) == 0
+    assert add_pairs(0b00011, 1, 1, 1) == 0
+    # Large index
+    n = 9999999
+    assert add_pairs(0b1, n) == 0b1 | 0b11 << n*2
+
+def test_excite_pairs():
+    """ Test excite_pairs function
+    """
+    # Excite_Pairs from occupied to virtual
+    assert excite_pairs(0b0011, 0, 1) == 0b1100
+    assert excite_pairs(0b0011, 0, 2) == 0b110000
+    assert excite_pairs(0b110000, 2, 0) == 0b11
+    # Excite_Pairs from virtual to virtual
+    assert excite_pairs(0b00100, 0, 2) == 0
+    assert excite_pairs(0b00100, 9999, 1) == 0
+    # Excite_Pairs from occupied to occupied
+    assert excite_pairs(0b110011, 2, 0) == 0
+    # Excite pairs from partially occupied
+    assert excite_pairs(0b100011, 2, 1) == 0
+    assert excite_pairs(0b100011, 2, 0) == 0
+    assert excite_pairs(0b100011, 2, 3) == 0
+    # Large index
+    assert excite_pairs(0b0111, 0, 999999) == 0b100 | 0b11 << 999999*2
 
 def test_is_occupied():
     """ Test is_occupied function
