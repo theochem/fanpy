Correct mistakes in the formula for the Jacobian matrix in `nonlin_jac`
1. Correct math mistakes; was using ground state all the time for phi_H_psi
instead of the appropriate projected determinant.

2. Correct Jacobian size mistakes; it should be (norbs*npairs,norbs*npairs), but
it was (len(pspace),norbs*npairs), which is not necessarily square.

3. Correct test/test_geminal.py to reflect the above.

geminals/geminal.py
@@ -113,8 +113,6 @@ class APIG(object):
                 jac = self.nonlin_jac
 
         # Run the solver (includes intemediate normalization)
-        print(solver)
-        print(objective)
         result = solver(objective, x0, jac=jac, args=(one, two, proj), **options)
 
         # Update the optimized coefficients
@@ -690,7 +688,7 @@ class APIG(object):
             vec.append(tmp)
             if len(vec) == x0.size:
                 break
-        return vec
+        return np.array(vec)
 
 
     def nonlin_jac(self, x0, one, two, proj):
@@ -719,8 +717,8 @@ class APIG(object):
 
         # Initialize Jacobian tensor and some temporary values
         # (J)_dij = d(F_d)/d(c_ij)
-        jac = np.zeros((len(proj),self.npairs*self.norbs))
-        phi_psi_tmp = np.zeros(len(proj))
+        jac = np.zeros((x0.size, x0.size))
+        phi_psi_tmp = np.zeros(x0.size)
         C = x0.reshape(self.npairs, self.norbs)
 
         # The objective functions {F_d} are of this form:
@@ -729,25 +727,26 @@ class APIG(object):
 
         # Compute the undifferentiated parts
         energy = self.phi_H_psi(self.ground, C, one, two)
-        for d in range(len(proj)):
-            phi_psi_tmp[d] = self.overlap(self.ground, C)
+        for d in range(x0.size):
+            phi_psi_tmp[d] = self.overlap(proj[d], C)
 
         # Compute the differentiated parts; this works by overwriting the geminal's
         # `overlap` method to be the method that describes its PrSchEq's partial
         # derivative wrt coefficient c_ij; we must back up the original method
         tmp_olp = self.overlap
         count = 0
+
+        # Overwrite `overlap` to take the correct partial derivative
         for i in range(self.npairs):
             for j in range(self.norbs):
-                # Overwrite `overlap` to take the correct partial derivative
                 coords = (i, j)
                 def olp_der(sd, gc, overwrite=coords):
                     return tmp_olp(sd, gc, derivative=True, indices=overwrite)
                 self.overlap = olp_der
+
                 # Compute the differentiated parts and construct the whole Jacobian
-                for d in range(len(proj)):
-                    jac[d,count] = self.phi_H_psi(self.ground, C, one, two) \
-                                   * phi_psi_tmp[d] \
+                for d in range(x0.size):
+                    jac[d,count] = self.phi_H_psi(self.ground, C, one, two)*phi_psi_tmp[d] \
                                    + energy*self.overlap(proj[d], C) \
                                    - self.phi_H_psi(proj[d], C, one, two)
                 count += 1

geminals/test/test_geminal.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import, division, print_function
 
 from copy import deepcopy as copy
 from itertools import combinations
+from newton import newton
 from scipy.optimize import root as quasinewton
 from geminal import APIG, AP1roG
 from horton_wrapper import *
@@ -406,8 +407,7 @@ def test_jacobian():
     one = np.ones((norbs, norbs))
     two = np.ones((norbs, norbs, norbs, norbs))
     jac = gem.nonlin_jac(coeffs, one, two, gem.pspace)
-    #print(jac)
-    assert jac.shape == (len(gem.pspace), gem.npairs*gem.norbs)
+    assert jac.shape == (gem.npairs*gem.norbs, gem.npairs*gem.norbs)
 
 test_init()
 test_setters_getters()
@@ -424,10 +424,10 @@ sys.exit()
 
 # Define user input
 fn = 'test/h4.xyz'
-basis = '6-31g'
+basis = 'sto-3g'
 nocc = 2
 maxiter = 100
-solver=quasinewton
+solver=newton
 #solver=lstsq
 
 options = { 'options': { 'maxiter':maxiter,
@@ -449,10 +449,13 @@ inpt = from_horton(fn=fn, basis=basis, nocc=nocc, guess=None)
 basis  = inpt['basis']
 coeffs = inpt['coeffs']
 energy = inpt['energy']
+one = inpt['ham'][0]
+two = inpt['ham'][1]
 core = inpt['ham'][2]
-guess = coeffs.ravel() #- 0.01*np.random.rand(nocc*basis.nbasis)
-#guess = 0.050*(2.0*(np.random.rand(nocc*(basis.nbasis - nocc)) - 1.0))
+#guess = coeffs.ravel() #- 0.01*np.random.rand(nocc*basis.nbasis)
+#guess = 0.050*(2.0*(np.random.rand(nocc*basis.nbasis) - 1.0))
 #guess = np.eye(nocc, M=basis.nbasis)
+guess = np.ones(nocc*basis.nbasis)
 #guess[:,nocc:] = 0.02*(np.random.rand(nocc, basis.nbasis - nocc) - 1.0)
 #guess = guess.ravel()
 gem = APIG(nocc, basis.nbasis)