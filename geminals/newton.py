10733ec1b1acfbc10cdf4493ffc8c9b020ec3915
Michael Richer <msricher1993@gmail.com>
2015-12-01 18:16:57 -0500

    Make `newton` solver actually work with the Jacobian, and SciPy-compatible
    1. Make Newton's solver take the Jacobian and work with it.
    
    2. Make Newton's solver take optional extra arguments to pass to the objective
       function.
    
    3. Make `newton` compatible with the SciPy solvers; I want them to be
       interchangeable without hassle.

--- geminals/newton.py
+++ geminals/newton.py
@@ -5,7 +5,8 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 
 
-def newton(fun, x0, jac=None, tol=1.0e-6, maxiter=1000):
+#def newton(fun, x0, jac=None, tol=1.0e-6, maxiter=1000, args=None):
+def newton(fun, x0, jac=None, args=None, **options):
     """
     Finds the roots of a non-linear set of equations using the Newton-Raphson method,
     given the multi-dimensional function, its Jacobian, and an initial guess `x0'.
@@ -19,12 +20,10 @@ def newton(fun, x0, jac=None, tol=1.0e-6, maxiter=1000):
     jac : callable
         A callable representing the Jacobian of `fun`.  It takes a takes a one-index
         np.ndarray as an argument, and returns a two-index len(x)-by-len(x) np.ndarray.
-    tol : float, optional
-        The convergence threshold; if iteration i+1 does not change any parameter by at
-        least `tol`, then the system is considered solved.
-    maxiter : int, optional
-        The maximum number of iterations to compute before raising the NoConvergence
-        exception.
+    args : unpack-able, optional
+        Extra arguments to be passed to `fun`.
+    options : dict, optional
+        Some Newton-solver specific options that have default values.
 
     Returns
     -------
@@ -35,19 +34,32 @@ def newton(fun, x0, jac=None, tol=1.0e-6, maxiter=1000):
 
     assert jac is not None, "The Jacobian must be specified for the Newton method."
 
+    defaults = { 'tol': 1.0e-6,
+                 'maxiter': 100,
+               }
+    defaults.update(options)
+    tol = defaults['tol']
+    maxiter = defaults['maxiter']
+    if args:
+        objective = lambda x0: fun(x0, *args)
+        jacobian = lambda x0: jac(x0, *args)
+    else:
+        objective = fun
+        jacobian = jac
+
     success = True
     nit = maxiter
 
     for i in range(maxiter):
 
         # Get fun(x) first, so we can check convergence
-        fx = fun(x0)
+        fx = objective(x0)
         if np.sqrt(np.abs(fx.dot(fx))) < tol:
             nit = i + 1
             break
 
         # Not converged, so we need jac(x)
-        jx = jac(x0)
+        jx = jacobian(x0)
 
         # x_new = x_old - inv(J(x))*F(x)
         # J(x)*(x_new - x_old) = -F(x) ==> of the form Ax = b
