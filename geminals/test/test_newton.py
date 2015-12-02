5d345fc1c8c4225a84c5e2efd6a46719b7a4d27a
Michael Richer <msricher1993@gmail.com>
2015-12-02 14:51:07 -0500

    Change test/test_newton.py to reflect previous changes in newton()
    1. Change test/test_newton.py's newton() arguments to reflect the previous few
       changes
    
    2. Add a test that checks whether additional arguments are being passed to the
       objective functions/jacobians

--- geminals/test/test_newton.py
+++ geminals/test/test_newton.py
@@ -2,25 +2,31 @@
 
 from __future__ import absolute_import, division, print_function
 
-from Newton import *
+from newton import newton
+import numpy as np
 import numpy.testing as npt
 from scipy.optimize import rosen, rosen_der
 
-
+# Solve a well-behaved nonlinear system
 tol = 1.0e-4
 guess  = np.array([0.686, 1.371])
 answer = np.array([1.0, 1.0])
-
 lin = lambda x : x[0] - x[1]
 lin_der = lambda x : [1, -1]
-
 def fun(x0):
     return np.array([rosen(x0), lin(x0)])
-
 def jac(x0):
     return np.array([rosen_der(x0), lin_der(x0)])
-
 r_newton = newton(fun, guess, jac=jac)
+npt.assert_allclose(r_newton['x'], answer, atol=tol, rtol=0)
+# Test that additional arguments can be passed into the objective functions/jacobians
+def fun(x0, test0, test1, test2):
+    assert test0 and test1 and test2
+    return np.array([rosen(x0), lin(x0)])
+def jac(x0, test0, test1, test2):
+    assert test0 and test1 and test2
+    return np.array([rosen_der(x0), lin_der(x0)])
+r_newton = newton(fun, guess, jac=jac, args=(True, 1, {'a':'b'}))
 npt.assert_allclose(r_newton['x'], answer, atol=tol, rtol=0)
 
 
