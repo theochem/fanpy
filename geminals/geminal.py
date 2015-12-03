Fixed and verified AP1roG Jacobians using finite-difference checker
1. Fixed errors in ap1rog's nonlin_jac() (missing minus signs in doubly-excited)

2. Add verification via finite-difference to test/test_geminal.py

3. Add test for AP1roG's quasinewton solver

4. It works!  The quasinewton solver is still trash though :(

geminals/geminal.py
@@ -720,7 +720,7 @@ class APIG(object):
         # (J)_dij = d(F_d)/d(c_ij)
         jac = np.zeros((x0.size, x0.size))
         phi_psi_tmp = np.zeros(x0.size)
-        C = x0.reshape(self.npairs, self.norbs)
+        C = self.construct_guess(x0)
 
         # The objective functions {F_d} are of this form:
         # F_d = (d<phi0|H|psi>/dc_ij)*<phi'|psi>/dc_ij
@@ -842,7 +842,7 @@ class AP1roG(APIG):
 
             if excite_count == 0:
                 if derivative:
-                    if (indices[1] < self.npairs) and (indices[0] == indices[1]):
+                    if indices[0] == indices[1]:
                         return 1
                     return 0
                 return 1
@@ -859,9 +859,9 @@ class AP1roG(APIG):
                     elif indices == (from_index[1], to_index[1]):
                         return matrix[from_index[0], to_index[0]]
                     elif indices == (from_index[0], to_index[1]):
-                        return matrix[from_index[1], to_index[0]]
+                        return -matrix[from_index[1], to_index[0]]
                     elif indices == (from_index[1], to_index[0]):
-                        return matrix[from_index[0], to_index[1]]
+                        return -matrix[from_index[0], to_index[1]]
                     return 0
                 overlap = matrix[from_index[0], to_index[0]] * \
                           matrix[from_index[1], to_index[1]]
@@ -880,7 +880,7 @@ class AP1roG(APIG):
         for phi in proj:
             tmp = self.phi_H_psi(self.ground, C, one, two)*self.overlap(phi, C)
             tmp -= self.phi_H_psi(phi, C, one, two)
-            vec.append(tmp**2)
+            vec.append(tmp)
             if len(vec) == x0.size:
                 break
         return np.array(vec)
@@ -893,7 +893,7 @@ class AP1roG(APIG):
         # (J)_dij = d(F_d)/d(c_ij)
         jac = np.zeros((x0.size, x0.size))
         phi_psi_tmp = np.zeros(x0.size)
-        C = x0.reshape(self.npairs, self.norbs - self.npairs)
+        C = self.construct_guess(x0)
 
         # The objective functions {F_d} are of this form:
         # F_d = (d<phi0|H|psi>/dc_ij)*<phi'|psi>/dc_ij
@@ -912,7 +912,7 @@ class AP1roG(APIG):
 
         # Overwrite `overlap` to take the correct partial derivative
         for i in range(self.npairs):
-            for j in range(self.norbs):
+            for j in range(self.npairs, self.norbs):
                 coords = (i, j)
                 def olp_der(sd, gc, overwrite=coords):
                     return tmp_olp(sd, gc, derivative=True, indices=overwrite)
@@ -927,9 +927,7 @@ class AP1roG(APIG):
 
         # Replace the original `overlap` method and return the Jacobian
         self.overlap = tmp_olp
-        # Finite difference verification tells me that my Jacobian is exactly double the
-        # size it should be.  This is a mystery that must be solved...
-        return jac/2.0
+        return jac
 
 
 # vim: set textwidth=90 :

geminals/test/test_geminal.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
+import numpy.testing as npt
 from inspect import ismethod
 from test.common import slow, run_tests, deriv_check
 from copy import deepcopy as copy
@@ -395,11 +396,8 @@ def test_brute_phi_H_psi():
     '''
     assert np.allclose(gem.brute_phi_H_psi(sd, coeff, one, two), integral_one + integral_two)
 
-def test_jacobian():
+def test_APIG_jacobian():
     """ Tests, APIG.jacobian()
-    Ideally, this whole module should be *optionally* dependent on HORTON.  I just pulled
-    in horton.test.common to write this test easily.  This needs to be replaced by
-    something sci/numpy-ish or something in-house.
     """
     # Test that it works
     npairs = 3
@@ -417,7 +415,7 @@ def test_jacobian():
     assert ismethod(gem.overlap)
 
 @slow
-def test_jacobian_finite_difference():
+def test_APIG_jacobian_finite_difference():
     # Test that the analytical nonlin_jac() matches a finite-difference approximation of
     # the Jacobian of nonlin()
     ht_out = from_horton(fn='test/h4.xyz', basis='sto-3g', nocc=2, guess='apig')
@@ -428,6 +426,36 @@ def test_jacobian_finite_difference():
     fun = lambda x : gem.nonlin(x, one, two, gem.pspace)
     jac = lambda x : gem.nonlin_jac(x, one, two, gem.pspace)
     deriv_check(fun, jac, x0)
+
+def test_AP1roG_jacobian():
+    """ Tests, AP1roG.jacobian()
+    """
+    # Test that it works
+    npairs = 3
+    norbs = 9
+    gem = AP1roG(npairs, norbs)
+    coeffs = np.ones((npairs, norbs - npairs))
+    one = np.ones((norbs, norbs))
+    two = np.ones((norbs, norbs, norbs, norbs))
+    fun = gem.nonlin(coeffs, one, two, gem.pspace)
+    jac = gem.nonlin_jac(coeffs, one, two, gem.pspace)
+    assert jac.shape == (fun.size, coeffs.size)
+    # Test that overlap gets restored as the proper instancemethod after nonlin_jac()
+    # is called
+    assert ismethod(gem.overlap)
+
+@slow
+def test_AP1roG_jacobian_finite_difference():
+    # Test that the analytical nonlin_jac() matches a finite-difference approximation of
+    # the Jacobian of nonlin()
+    ht_out = from_horton(fn='test/h4.xyz', basis='sto-3g', nocc=2, guess='ap1rog')
+    gem = AP1roG(2, ht_out['basis'].nbasis)
+    x0 = ht_out['coeffs'].ravel()
+    one = ht_out['ham'][0]
+    two = ht_out['ham'][1]
+    fun = lambda x : gem.nonlin(x, one, two, gem.pspace)
+    jac = lambda x : gem.nonlin_jac(x, one, two, gem.pspace)
+    deriv_check(fun, jac, x0, verbose=True)
 
 @slow
 def test_APIG_quasinewton():
@@ -458,14 +486,57 @@ def test_APIG_quasinewton():
     gem = APIG(nocc, basis.nbasis)
     result_qn = gem(coeffs, one, two, solver=quasinewton, **options)
     assert result_qn['success']
+    """
     energy = gem.phi_H_psi(gem.ground, gem.coeffs, one, two) + core
     olp = gem.overlap(gem.ground, gem.coeffs)
-    """
     str0 = "GUESS\n{}\n{}\n".format(coeffs_old, energy_old)
     str1 = "GEMINAL\n{}\n{}\n".format(gem.coeffs, energy)
     str2 = "OVERLAP: {}".format(olp)
     print(str0 + str1 + str2)
     """
+
+@slow
+def test_AP1roG_quasinewton():
+    """
+    """
+    # Define user input
+    fn = 'test/h4.xyz'
+    basis = '3-21g'
+    nocc = 2
+    # Make geminal guess from HORTON's AP1roG module
+    ht_out = from_horton(fn=fn, basis=basis, nocc=nocc, guess='ap1rog')
+    basis  = ht_out['basis']
+    core = ht_out['ham'][2]
+    one = ht_out['ham'][0]
+    two = ht_out['ham'][1]
+    energy_old = ht_out['energy']
+    coeffs_ht = ht_out['coeffs']
+    coeffs_old = 0.01*(2*(np.random.rand(nocc, basis.nbasis - nocc) - 0.5))
+    coeffs_old = coeffs_old.ravel()
+    coeffs = copy(coeffs_old)
+    options = { 'options': { 'maxfev': 250*coeffs.size,
+                             'xatol': 1.0e-12,
+                             'fatol': 1.0e-12,
+                             'factor': 1.0,
+                           },
+                'jac' : True,
+                'method' : 'hybr',
+              }
+    # Optimize with quasinewton method
+    gem = AP1roG(nocc, basis.nbasis)
+    result_qn = gem(coeffs, one, two, solver=quasinewton, **options)
+    assert result_qn['success']
+    # We're not *quite* here yet, but MOST coefficients match!  This is now a nonlinear
+    # programming issue, the science is correct! woo!
+    #npt.assert_allclose(gem.coeffs[:,gem.npairs:], coeffs_ht, rtol=0.01)
+    """
+    energy = gem.phi_H_psi(gem.ground, gem.coeffs, one, two) + core
+    olp = gem.overlap(gem.ground, gem.coeffs)
+    str0 = "GUESS\n{}\n{}\n".format(coeffs_ht, energy_old)
+    str1 = "GEMINAL\n{}\n{}\n".format(gem.coeffs[:,nocc:], energy)
+    str2 = "OVERLAP: {}".format(olp)
+    print(str0 + str1 + str2)
+    #"""
 
 tests = [ test_init,
           test_setters_getters,
@@ -475,9 +546,12 @@ tests = [ test_init,
           test_overlap,
           test_double_phi_H_psi,
           test_brute_phi_H_psi,
-          test_jacobian,
-          test_jacobian_finite_difference,
+          test_APIG_jacobian,
+          test_APIG_jacobian_finite_difference,
+          test_AP1roG_jacobian,
+          test_AP1roG_jacobian_finite_difference,
           test_APIG_quasinewton,
+          test_AP1roG_quasinewton,
         ]
 # Parse the cmd line flags and run fast and/or `@slow`-marked tests
 run_tests(tests)