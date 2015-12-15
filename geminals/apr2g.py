ef9fa7b42c98c51fe1649bcd77bf29ff5eccacd5
Taewon D. Kim <kimt33@mcmaster.ca>
2015-12-14 21:08:06 -0500

    Add initial guess generator using least squares method
    Issues:
     1. No tests

--- geminals/apr2g.py
+++ geminals/apr2g.py
@@ -54,14 +54,100 @@ class APr2G(APIG):
     # Methods
     #
 
-    def _generate_x0(self):
-        """
-        See APIG._generate_x0().
+    def _generate_x0(self, model_coeffs=None):
+        """ Generates an initial guess
 
+        If model_coeffs is given, we try to find a set of coefficients such that
+        is as closely satisfied as possible, using least squares method
+        ..math::
+            C_{ij} &= \frac{\zeta_i}{\epsilon_i + \lambda_j}\\
+            0 &= \zeta_i - C_{ij} \epsilon_i - C_{ij} \lambda_j\\
+        This is a set of linear equations. These equations will be solved by least
+        squares method.
+
+        The least square has the form of :math:`Ax=b`. Given that the :math:`b=0`
+        and the unknowns are
+        ..math::
+            x = \begin{bmatrix}
+            \zeta_1 \\ \vdots\\ \zeta_P\\
+            \epsilon_1 \\ \vdots\\ \epsilon_P\\
+            \lambda_1 \\ \vdots\\ \lambda_K\\
+            \end{bmatrix},
+        then A must be
+        ..math::
+            A = \begin{bmatrix}
+            1 & 0 & \dots & 0 & -C_{11} & 0 & \dots & 0 & -C_{11} & 0 \dots & 0\\
+            1 & 0 & \dots & 0 & -C_{12} & 0 & \dots & 0 & 0 & -C_{12} & \dots & 0\\
+            \vdots & & & & & & & & & & &\\
+            1 & 0 & \dots & 0 & -C_{1K} & 0 & \dots & 0 & 0 & 0 & \dots & -C_{1K}\\
+            0 & 1 & \dots & 0 & 0 & -C_{2K} & \dots & 0 & -C_{21} & 0 \dots & 0\\
+            \vdots & & & & & & & & & & &\\
+            0 & 1 & \dots & 0 & 0 & -C_{2K} & \dots & 0 & 0 & 0 & \dots & -C_{2K}\\
+            \vdots & & & & & & & & & & &\\
+            0 & 0 & \dots & 1 & 0 & 0 & \dots & -C_{P1} & -C_{P1} & 0 \dots & 0\\
+            \vdots & & & & & & & & & & &\\
+            0 & 0 & \dots & 1 & 0 & 0 & \dots & -C_{PK} & 0 & 0 & \dots & -C_{PK}\\
+            \end{bmatrix}
+
+        Parameter
+        ---------
+        model_coeffs : {None, np.ndarray(P,K)}
+            Coefficients after which the initial guess is modeled
+
+        Example
+        -------
+        Assuming we have a system with 2 electron pairs and 4 spatial orbitals,
+        we have
+        ..math::
+            C = \begin{bmatrix}
+            C_{11} & \dots & C_{1K}\\
+            \vdots & \vdots & \vdots\\
+            C_{P1} & \dots & C_{PK}
+            \end{bmatrix}
+
+        ..math::
+            A = \begin{bmatrix}
+            1 & 0 & -C_{11} & 0 & -C_{11} & 0 & 0 & 0\\
+            1 & 0 & -C_{12} & 0 & 0 & -C_{12} & 0 & 0\\
+            1 & 0 & -C_{13} & 0 & 0 & 0 & -C_{13} & 0\\
+            1 & 0 & -C_{14} & 0 & 0 & 0 & 0 & -C_{14}\\
+            0 & 1 & 0 & -C_{21} & -C_{21} & 0 & 0 & 0\\
+            0 & 1 & 0 & -C_{22} & 0 & -C_{22} & 0 & 0\\
+            0 & 1 & 0 & -C_{23} & 0 & 0 & -C_{23} & 0\\
+            0 & 1 & 0 & -C_{24} & 0 & 0 & 0 & -C_{24}\\
+            \end{bmatrix}
+
+        ..math::
+            x = \begin{bmatrix}
+            \zeta_1 \\ \zeta_2\\
+            \epsilon_1 \\ \epsilon_2\\
+            \lambda_1 \\ \lambda_2 \\ \lambda_3 \\ \lambda_4\\
+            \end{bmatrix}
         """
 
         # This is NOT a good guess, it's a placeholder!
-        x0 = 2.0*(np.random.rand(2 * self.norbs + self.npairs) - 0.5)
+        if model_coeffs is None:
+            x0 = 2.0*(np.random.rand(2 * self.norbs + self.npairs) - 0.5)
+        else:
+            # Here, we try to solve Ax = 0 where
+            # A
+            A = np.array([])
+            for row_ind, row in enumerate(model_coeffs):
+                temp = np.array([])
+                # temporary column of ones
+                temp_col_ones = np.zeros(model_coeffs.T.shape)
+                temp_col_ones[:, row_ind] = 1
+                # coefficients for zetas
+                temp = temp.hstack((temp, temp_col_ones))
+                # coefficients for epsilons
+                temp = temp.hstack((temp, -temp_col_ones*row.reshape(row.size, 1)))
+                # coefficients for lambdas
+                temp = temp.hstack((temp, np.identity(-row)))
+                A = np.vstack((A, temp))
+            # b
+            b = np.zeros((A.shape[0], 1))
+            # Ax = b, solve for x by least squares
+            x0 = np.linalg.lstsq(A, b)
         return x0
 
     def _construct_coeffs(self, x0):
