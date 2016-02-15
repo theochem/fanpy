Make the Jacobian formula 100% correct, and faster
...too bad it's still singular half the time and unusable :(

prowl/lib/apr2g.py
@@ -34,9 +34,9 @@ def generate_guess(self):
         x[self.p + self.k + j] = 1.0
 
     # Add noise
-    x += (np.random.rand(x.size) - 0.5) * 2.0e-3
+    #x += (np.random.rand(x.size) - 0.5) * 2.0e-3
     if x.dtype == np.complex128:
-        x += (np.random.rand(x.size) - 0.5) * 2.0e-6j
+        x += (np.random.rand(x.size) - 0.5) * 1.0e-2j
 
     return x
 
@@ -130,37 +130,32 @@ def jacobian(self, x):
 
         # Impose some normalization constraints (this is currently incorrect)
         if c < self.p:
-            jac[-1, c] = -(x[self.p + self.k + c] / ((x[c] - x[self.p + c]) ** 2)) / olp \
-                - (np.trace(self._C[:, :self.p]) - self.p) * d_olp / (olp ** 2)
-            jac[-2, c] = -np.prod([self._C[i, i] for i in range(self.p) if i != c]) \
-                * (x[self.p + self.k + c] / ((x[c] - x[self.p + c]) ** 2)) / olp \
-                - (np.prod([self._C[i, i] for i in range(self.p)]) - 1) * d_olp / (olp ** 2)
+            jac[-1, c] = -self.x[self.p + self.k + c] / ((self.x[c] - self.x[self.p + c]) ** 2)
+            jac[-2, c] = -np.prod([self._C[i, i] for i in range(self.p)]) / (self.x[c] - self.x[self.p + c])
         elif self.p < c < 2 * self.p:
-            jac[-1, c] = (x[self.p + self.k + c] / ((x[c] - x[self.p + c]) ** 2)) / olp \
-                - (np.trace(self._C[:, :self.p]) - self.p) * d_olp / (olp ** 2)
-            jac[-2, c] = np.prod([self._C[i, i] for i in range(self.p) if i != c]) \
-                * (x[self.p + self.k + c] / ((x[c] - x[self.p + c]) ** 2)) / olp \
-                - (np.prod([self._C[i, i] for i in range(self.p)]) - 1) * d_olp / (olp ** 2)
+            jac[-1, c] = self.x[self.p + self.k + c] / ((self.x[c] - self.x[self.p + c]) ** 2)
+            jac[-2, c] = np.prod([self._C[i, i] for i in range(self.p)]) / (self.x[c] - self.x[self.p + c])
         elif (self.p + self.k) < c < (2 * self.p + self.k):
-            jac[-1, c] = (1 / (x[c] - x[self.p + c])) / olp \
-                - (np.trace(self._C[:, :self.p]) - self.p) * d_olp / (olp ** 2)
-            jac[-2, c] = (np.prod([self.x[self.p + self.k + i] for i in range(self.p) if i != c]) \
-                / np.prod([1 / (self.x[i] + self.x[self.p + i]) for i in range(self.p)])) / olp \
-                - (np.prod([self._C[i, i] for i in range(self.p)]) - 1) * d_olp / (olp ** 2)
+            jac[-1, c] = 1 / (self.x[c] - self.x[self.p + c])
+            jac[-2, c] = np.prod([self._C[i, i] for i in range(self.p) if i != c]) / (self.x[c] - self.x[self.p + c])
         else:
             jac[-1, c] = 0
             jac[-2, c] = 0
+        jac[-1, c] *= self.p
+        jac[-2, c] *= self.p
 
         # Impose (for all SDs in `pspace`) d(<SD|H|Psi> - E<SD|H|Psi>) == 0
         for i, sd in enumerate(self.pspace):
-            jac[i, c] = sum(self.hamiltonian_deriv(sd, c)) \
-                - energy * self.overlap_deriv(sd, c) - d_energy * self.overlap(sd)
+            sd_olp = self.overlap(sd)
+            sd_d_olp = self.overlap_deriv(sd, c)
+            jac[i, c] = (olp * sum(self.hamiltonian_deriv(sd, c)) - d_olp * sum(self.hamiltonian(sd))) / olp ** 2 \
+                - (olp * sd_olp * d_energy + energy * olp * sd_d_olp - energy * sd_olp * d_olp) / olp ** 2
 
     # Fix NaNs (not sure if we can avoid this... but we'll try)
     for i in range(jac.shape[0]):
         for j in range(jac.shape[1]):
             if not np.isfinite(jac[i, j]):
-                jac[i, j] = 0
+                jac[i, j] = 0.0
 
     return jac * 0.5
 
@@ -193,9 +188,12 @@ def objective(self, x):
     obj = np.empty((len(self.pspace) + 2), dtype=x.dtype)
 
     # Impose some normalization constraints
-    # NOTE: multiplied these by self.p to increase their weightings
-    obj[-1] = self.p * (np.trace(self._C[:, :self.p]) - self.p) / olp
-    obj[-2] = self.p * (np.prod(np.diag(self._C[:, :self.p])) - 1) / olp
+    # NOTE: divide these by olp, it might help.
+    obj[-1] = np.sum([self._C[i, i] for i in range(self.p)]) - self.p
+    obj[-2] = np.prod([self._C[i, i] for i in range(self.p)]) - 1
+    #obj[-2] = np.prod(self._C[:, :self.p]) - 1
+    obj[-1] *= self.p
+    obj[-2] *= self.p
 
     # Impose (for all SDs in `pspace`) <SD|H|Psi> - E<SD|H|Psi> == 0
     for i, sd in enumerate(self.pspace):
@@ -272,7 +270,8 @@ def solve(self, **kwargs):
     """
 
     eps = 10 * sys.float_info.epsilon
-    ubound = np.ones_like(self.x) * np.inf
+    ubound = np.ones_like(self.x)
+    ubound[:(self.p + self.k)] *= np.inf
     lbound = -ubound
     # NOTE: need to test bounds further
     #ubound[self.p + self.k] = 1 + eps

prowl/utils/permanent.py
@@ -57,44 +57,11 @@ def dense_deriv(matrix, x, y):
 
 
 def adjugate(matrix):
-
-    # If matrix only has one row
-    if matrix.size == 1:
-        return np.array([1])
-
-    # Get row and column slice lists
-    rows = list(range(matrix.shape[0]))
-    cols = list(range(matrix.shape[1]))
-
-    # Compute the adjugate matrix
-    adj = np.empty_like(matrix)
-    for i in range(matrix.shape[0]):
-        for j in range(matrix.shape[1]):
-            rows.remove(i)
-            cols.remove(j)
-            sgn = -1 if ((i + j) % 2) else 1
-            t_rows = np.array(rows)[:, np.newaxis]
-            adj[j, i] = sgn * np.linalg.det(matrix[t_rows, cols])
-            rows.insert(i, i)
-            cols.insert(j, j)
-
-    return adj
+    return np.linalg.inv(matrix) * np.linalg.det(matrix)
 
 
 def apr2g(matrix):
-
     return np.linalg.det(matrix ** 2) / np.linalg.det(matrix)
-
-
-
-def apr2g_bak(x, p, cols):
-
-    # |C|+ == |F|*|diag(zetas)|
-    k = (x.size - p) // 2
-    F = faribault_factor(x, p, cols)
-    zeta_indices = [p + k + c for c in cols]
-
-    return np.linalg.det(F) * np.prod(x[zeta_indices])
 
 
 def apr2g_deriv(matrix, x, p, cols, dx):
@@ -153,117 +120,6 @@ def apr2g_deriv(matrix, x, p, cols, dx):
         - det_ewsquare * (1 / (det_matrix ** 2)) * np.trace(adj_matrix.dot(d_matrix))
 
     return deriv
-
-
-def apr2g_deriv_bak(x, p, cols, dx):
-
-    # Initialize structures and check for d|C|+/dx == 0
-    k = (x.size - p) // 2
-    indices = list(range(p))
-    indices.extend([p + c for c in cols])
-    indices.extend([p + k + c for c in cols])
-    if dx not in indices:
-        return 0
-    dx = indices.index(dx)
-    xnew = x[indices]
-
-    # Initialize Faribault matrices
-    F = faribault_factor(x, p, cols)
-    dF = np.zeros_like(F)
-    D = xnew[(2 * p):].copy()
-    dD = np.zeros_like(D)
-
-    # dx == lambda_x
-    if dx < p:
-        for i in range(p):
-            for j in range(p):
-                if dx == i == j:
-                    dF[i, j] = -1 / ((xnew[i] - xnew[p + i]) ** 2)
-                elif dx == j != i:
-                    dF[i, j] = -1 / ((xnew[j] - xnew[p + i]) ** 2)
-                elif i == j:
-                    dF[i, j] = -1 / ((xnew[dx] - xnew[p + i]) ** 2)
-                else:
-                    dF[i, j] = 0
-
-    # dx == epsilon_x
-    elif dx < 2 * p:
-        for i in range(p):
-            for j in range(p):
-                if dx == (i + p):
-                    if i == j:
-                        for l in range(p):
-                            if l != i:
-                                dF[i, j] += -1 / ((xnew[p + i] - xnew[p + l]) ** 2)
-                            dF[i, j] += 1 / ((xnew[l] - xnew[p + i]) ** 2)
-                    else:
-                        dF[i, j] = -1 / ((xnew[p + i] - xnew[p + j]) ** 2)
-                elif dx == (j + p) != (i + p):
-                    dF[i, j] = 1 / ((xnew[p + i] - xnew[p + j]) ** 2)
-                elif i == j:
-                    dF[i, j] = 1 / ((xnew[p + i] - xnew[dx]) ** 2)
-                else:
-                    dF[i, j] = 0
-    # dx == zeta_x
-    else:
-        dD[dx - 2 * p] = 1
-
-    # Fix `dF`, `D`, and `dD`'s dimensions
-    D = np.diag(D)
-    dD = np.diag(dD)
-
-    # Use Jacobi's formula with the product rule
-    deriv = np.linalg.det(F) * np.trace(adjugate(D).dot(dD)) \
-        + np.linalg.det(D) * np.trace(adjugate(F).dot(dF))
-
-    return deriv
-
-#    # `dx` is a denominator parameter
-#    dF = np.zeros_like(F)
-#    if dx < (p + k):
-#
-#        # `dx` is lambda
-#        if dx < p:
-#            # Update lambda-containing terms
-#            for i in range(p):
-#                dF[i, i] = -1 / ((x[dx] - x[p + i]) ** 2)
-#
-#        # `dx` is epsilon
-#        else:
-#            for i in range(p):
-#                for j in range(p):
-#                    # `dx` == `i`
-#                    if i == (dx - p):
-#                        # Update diagonal terms
-#                        if i == j:
-#                            for l in range(p):
-#                                dF[i, i] += 1 / ((x[l] - x[dx]) ** 2)
-#                                if l != i:
-#                                    dF[i, i] += -1 / ((x[dx] - x[p + l]) ** 2)
-#                        # Update off-diagonal terms
-#                        else:
-#                            dF[i, j] = -1 / ((x[dx] - x[p + j]) ** 2)
-#                    # `dx` == `j`
-#                    elif j == (dx - p):
-#                        # Update off-diagonal terms
-#                        if i != j:
-#                            dF[i, j] = 1 / ((x[p + i] - x[dx]) ** 2)
-#                    # `dx` != `i`, `dx` != `j`
-#                    else:
-#                        # Update diagonal terms (off-diagonals are zero)
-#                        if i == j:
-#                            dF[i, i] = 1 / ((x[p + i] - x[dx]) ** 2)
-#
-#        # Use Jacobi's formula for denominator `dx`s, then multiply by the zetas
-#        return np.trace(adjugate(F).dot(dF)) * np.prod(x[zeta_indices])
-#
-#    # `dx` is a numerator parameter; just remove the zeta `dx`
-#    else:
-#        dx_index = dx - p - k
-#        zetas = np.diag(x[(p + k):].copy())
-#        dzetas = zetas.copy()
-#        dzetas[dx_index, dx_index] = 1
-#        return np.linalg.det(F) * np.trace(adjugate(zetas).dot(dzetas))
 
 
 def cauchy_factor(x, p):