12f879fe36f63d6564c8a5a4ad97a81e0cd06c12
Taewon D. Kim <kimt33@mcmaster.ca>
2016-02-21 11:40:14 -0500

    Add gaussian fchk reader
    What:
     1. Extracts Hamiltonian (and other information) from gaussian fchk using HORTON
    Why:
     1. Can use Gaussian HF or DFT instead of HORTON's
    Issues:
     1. Some of the MO's from HORTON are negatives of the ones from Gaussian. This shouldn't
        really matter, but something to keep in mind.
     2. Even after you take care of the negatives, you get integrals that are different by
        around 1e-8. Maybe the MO's need to be orthogonalized or normalized.

--- prowl/utils/horton.py
+++ prowl/utils/horton.py
@@ -105,3 +105,61 @@ def ap1rog(fn=None, basis=None, n=None, scf=True, opt=False, x=None):
         "x": x,
         "energy": energy,
     }
+
+def gaussian_fchk(fchk_file):
+    """ Extracts the hamiltonian from gaussian fchk file
+
+    Parameters
+    ----------
+    fchk_file : str
+        Formatted chk file
+
+    Returns
+    -------
+    result : dict
+        Contains "mol", a `horton.IOData` instance;
+        "basis", a `horton.GOBasis` instance;
+        "orb", the spatial orbital coefficients;
+        "H", the one-electron Hamiltonian;
+        "G", the two-electron Hamiltonian;
+
+    Raises
+    ------
+    Assertion Error
+        If beta orbitals exist (orbitals are not spatial)
+    """
+    mol = IOData.from_file(fchk_file)
+    # for spin orbitals
+    exps = [mol.exp_alpha]
+    if hasattr(mol, 'exp_beta'):
+        exps.append(mol.exp_beta)
+    #assert not hasattr(mol, 'exp_beta')
+
+    obasis = mol.obasis
+    kin = obasis.compute_kinetic(mol.lf)._array
+    na = obasis.compute_nuclear_attraction(mol.coordinates, mol.pseudo_numbers, mol.lf)._array
+
+    one_ab = kin
+    one_ab += na
+    two_ab = obasis.compute_electron_repulsion(mol.lf)._array
+
+    # for spin orbitals
+    one_mo = []
+    two_mo = []
+    for i, exp_i in enumerate(exps):
+        for j, exp_j in enumerate(exps):
+            if i <= j:
+                temp = np.einsum('sd,pqrs->pqrd', exp_j.coeffs, two_ab, casting='no', order='C')
+                temp = np.einsum('rc,pqrd->pqcd', exp_i.coeffs, temp, casting='no', order='C')
+                temp = np.einsum('qb,pqcd->pbcd', exp_j.coeffs, temp, casting='no', order='C')
+                temp = np.einsum('pa,pbcd->abcd', exp_i.coeffs, temp, casting='no', order='C')
+                two_mo.append(temp)
+        one_mo.append(exp_i.coeffs.T.dot(one_ab).dot(exp_i.coeffs))
+
+    return {
+        "mol": mol,
+        "basis": obasis,
+        "orb": [exp.coeffs for exp in exps],
+        "H": one_mo[0],
+        "G": two_mo[0]
+    }
