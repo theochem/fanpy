e90e21af1c45e42a5ffe68e2bf0240f677f4c9a6
Taewon D. Kim <kimt33@mcmaster.ca>
2016-05-18 20:09:26 -0400

    Add CISD wavefunction
    What:
     1. Add CISD wavefunction
     2. Add tests for CISD wavefunction
     3. Modify test for CISD wavefunction
    Issues:
     1. CISD energy values for LiH is lower than that of Gaussian
     1. FCI energy values for LiH is lower than that of Gaussian

---
+++ geminals/cisd.py
@@ -0,0 +1,95 @@
+from __future__ import absolute_import, division, print_function
+
+from ci_wavefunction import CIWavefunction
+from sd_list import ci_sd_list
+from ci_matrix import ci_matrix
+from math_tools import binomial
+import slater
+
+class CISD(CIWavefunction):
+    """ Configuration Interaction Singles and Doubles Wavefunction
+
+    Contains the necessary information to variationally solve the CI wavefunction
+
+    Attributes
+    ----------
+    dtype : {np.float64, np.complex128}
+        Numpy data type
+    H : np.ndarray(K,K)
+        One electron integrals for the spatial orbitals
+    Ha : np.ndarray(K,K)
+        One electron integrals for the alpha spin orbitals
+    Hb : np.ndarray(K,K)
+        One electron integrals for the beta spin orbitals
+    G : np.ndarray(K,K,K,K)
+        Two electron integrals for the spatial orbitals
+    Ga : np.ndarray(K,K,K,K)
+        Two electron integrals for the alpha spin orbitals
+    Gb : np.ndarray(K,K,K,K)
+        Two electron integrals for the beta spin orbitals
+    nuc_nuc : float
+        Nuclear nuclear repulsion value
+    nspatial : int
+        Number of spatial orbitals
+    nspin : int
+        Number of spin orbitals (alpha and beta)
+    nelec : int
+        Number of electrons
+    npair : int
+        Number of electron pairs
+        Assumes that the number of electrons is even
+    nparticle : int
+        Number of quasiparticles (electrons)
+    ngeminal : int
+        Number of geminals
+
+    Private
+    -------
+    _methods : dict
+        Default dimension of projection space
+    _energy : float
+        Electronic energy
+    _nci_default : int
+        Number of Slater determinants
+
+    Methods
+    -------
+    compute_civec
+        Generates a list of Slater determinants
+    compute_ci_matrix
+        Generates the Hamiltonian matrix of the Slater determinants
+    """
+    @property
+    def _nci_default(self):
+        """ Total number of configurations
+
+        """
+        num_singles = binomial(self.nelec, 1)*binomial(self.nspin-self.nelec, 1)
+        num_doubles = binomial(self.nelec, 2)*binomial(self.nspin-self.nelec, 2)
+        return 1+num_singles+num_doubles
+
+    def compute_civec(self):
+        """ Generates Slater determinants
+
+        Number of Slater determinants is limited by num_limit. First Slater determinant is the ground
+        state, next are the first excitations from exc_orders, then second excitation from
+        exc_orders, etc
+
+        Returns
+        -------
+        civec : list of ints
+            Integer that describes the occupation of a Slater determinant as a bitstring
+        """
+        return ci_sd_list(self, self._nci_default, [1, 2])
+
+    def compute_ci_matrix(self):
+        """ Returns Hamiltonian matrix in the arbitrary Slater (orthogonal) determinant basis
+
+        ..math::
+            H_{ij} = \big< \Phi_i \big| H \big| \Phi_j \big>
+
+        Returns
+        -------
+        matrix : np.ndarray(K, K)
+        """
+        return ci_matrix(self, 'restricted')
--- geminals/test/test_cisd.py
+++ geminals/test/test_cisd.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, division, print_function
 import sys
 sys.path.append('../')
-from fci import FCI
+from cisd import CISD
 from hort import hartreefock
 
 from nose.tools import assert_raises
 
-def test_fci_wavefunction():
+def test_cisd_wavefunction():
     #### H2 ####
     nelec = 2
     hf_dict = hartreefock(fn="./h2.xyz", basis="6-31g**", nelec=nelec)
@@ -14,10 +14,10 @@ def test_fci_wavefunction():
     H = hf_dict["H"]
     G = hf_dict["G"]
     nuc_nuc = hf_dict["nuc_nuc"]
-    fci = FCI(nelec=nelec, H=H, G=G, nuc_nuc=nuc_nuc)
-    fci()
+    cisd = CISD(nelec=nelec, H=H, G=G, nuc_nuc=nuc_nuc)
+    cisd()
     # compare with number from Gaussian
-    assert abs(fci.compute_energy()-(-1.1651486697)) < 1e-7
+    assert abs(cisd.compute_energy()-(-1.1651486697)) < 1e-7
     #### LiH ####
     nelec = 4
     hf_dict = hartreefock(fn="./lih.xyz", basis="sto-6g", nelec=nelec)
@@ -25,9 +25,9 @@ def test_fci_wavefunction():
     H = hf_dict["H"]
     G = hf_dict["G"]
     nuc_nuc = hf_dict["nuc_nuc"]
-    fci = FCI(nelec=nelec, H=H, G=G, nuc_nuc=nuc_nuc)
-    fci()
-    print(fci.compute_energy(), -7.9723355823)
-    #assert abs(fci.compute_energy()-(-1.1651486697)) < 1e-7
+    cisd = CISD(nelec=nelec, H=H, G=G, nuc_nuc=nuc_nuc)
+    cisd()
+    print(cisd.compute_energy(), -7.9721005124)
+    assert abs(cisd.compute_energy()-(-7.9721005124)) < 1e-7
 
-test_fci_wavefunction()
+test_cisd_wavefunction()
