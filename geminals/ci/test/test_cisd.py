dae63f07da84d0e098674b5cdbedaeeb8c81e028
Taewon D. Kim <kimt33@mcmaster.ca>
2016-09-16 19:12:09 -0400

    Update CISD test (to the right number)
    Gaussian (and Molpro) froze cores by default

--- geminals/ci/test/test_cisd.py
+++ geminals/ci/test/test_cisd.py
@@ -6,6 +6,15 @@ from geminals.wrapper.horton import gaussian_fchk
 
 
 def test_cisd_h2():
+    """ Tests CISD wavefunction using H2 (6-31G**)
+
+    Compared to Gausssian results
+
+    Note
+    ----
+    You have to be careful with Gaussian and molpro calculations because they freeze core by
+    default
+    """
     #### H2 ####
     # HF energy: -1.13126983927
     # FCI energy: -1.1651487496
@@ -29,9 +38,18 @@ def test_cisd_h2():
     assert abs(cisd.compute_energy() - (-1.1651486697)) < 1e-7
 
 def test_cisd_lih():
+    """ Tests CISD wavefunction using LiH (6-31G)
+
+    Compared to Molpro results
+
+    Note
+    ----
+    You have to be careful with Gaussian and molpro calculations because they freeze core by
+    default
+    """
     #### LiH ####
-    # HF energy: -7.97926894940
-    # CISD energy: -7.9980021297
+    # HF energy: -7.97926895
+    # CISD energy: -7.99826182
     data_path = os.path.join(os.path.dirname(__file__), '../../../data/test/lih_hf_631g.fchk')
     hf_dict = gaussian_fchk(data_path)
 
@@ -41,16 +59,16 @@ def test_cisd_lih():
     G = hf_dict["G"]
     nuc_nuc = hf_dict["nuc_nuc"]
 
-    cisd = CISD(nelec=nelec, H=H, G=G, nuc_nuc=nuc_nuc, spin=0)
+    cisd = CISD(nelec=nelec, H=H, G=G, nuc_nuc=nuc_nuc, spin=None)
     ci_matrix = cisd.compute_ci_matrix()
 
     # compare HF numbers
-    assert abs(ci_matrix[0, 0] + cisd.nuc_nuc - (-7.97926894940) < 1e-8)
+    assert abs(ci_matrix[0, 0] + cisd.nuc_nuc - (-7.97926895) < 1e-8)
     # check that hamiltonian is symmetric
     assert np.allclose(ci_matrix, ci_matrix.T)
     # solve
     cisd()
 
     # compare with number from Gaussian
-    print(cisd.compute_energy(), (-7.9980021297))
-    assert abs(cisd.compute_energy() - (-7.9980021297)) < 1e-7
+    print(cisd.compute_energy(), (-7.99826182))
+    assert abs(cisd.compute_energy() - (-7.99826182)) < 1e-7
