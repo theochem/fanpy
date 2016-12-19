baa655e3c00016d51e96e451dfbbec2c0120e7ec
Taewon D. Kim <kimt33@mcmaster.ca>
2016-12-19 02:31:05 -0500

    Quality check wfns.wrapper.pyscf
    What:
     1. Change key name in horton wrapper output
     2. Fix documentations
     3. Add appropriate input checks
     4. Coverage
     5. Formatting and whatnot

--- wfns/wrapper/pyscf.py
+++ wfns/wrapper/pyscf.py
@@ -1,13 +1,26 @@
+""" Wrapper of PySCF
+
+At this moment, we mainly need the integrals (in MO basis) and the energies (electron, nuclear
+nuclear repulsion) to construct a wavefunction. Here, we use PySCF to get these values.
+
+Functions
+---------
+hartreefock(xyz_file, basis, is_unrestricted=False)
+    Runs HF in PySCF
+generate_fci_cimatrix(h1e, eri, nelec, is_chemist_notation=False)
+    Generates the FCI Hamiltonian CI matrix
+"""
 from __future__ import absolute_import, division, print_function
 import os
-from pyscf import gto, scf, ao2mo
-# needed for the generate_ci_matrix part
-import numpy as np
 import ctypes
-from pyscf.fci import cistring
-import pyscf.lib
-libfci = pyscf.lib.load_library('libfci')
+import numpy as np
+from wfns import __file__ as package_path
 from wfns import slater
+from pyscf import gto, scf, ao2mo
+from pyscf.lib import load_library, hermi_triu
+from pyscf.fci import cistring
+
+LIBFCI = load_library('libfci')
 
 def hartreefock(xyz_file, basis, is_unrestricted=False):
     """ Runs HF using PySCF
@@ -25,15 +38,25 @@ def hartreefock(xyz_file, basis, is_unrestricted=False):
     Returns
     -------
     result : dict
-        "energy", electronic energy
-        "nuc_nuc", nuclear repulsion energy
-        "H", tuple of the one-electron Hamiltonian;
-        "G", tuple of the two-electron Hamiltonian;
+        "el_energy"
+            electronic energy
+        "nuc_nuc_energy"
+            nuclear repulsion energy
+        "H"
+            tuple of the one-electron interal
+        "G"
+            tuple of the two-electron integral in Physicist's notation
 
+    Raises
+    ------
+    ValueError
+        If given xyz file does not exist
+    NotImplementedError
+        If calculation is unrestricted or generalized
     """
     # check xyz file
     cwd = os.path.dirname(__file__)
-    data_dir = os.path.join(cwd, '../../data')
+    data_dir = os.path.join(os.path.dirname(package_path), '../data')
     if os.path.isfile(xyz_file):
         pass
     elif os.path.isfile(os.path.join(cwd, xyz_file)):
@@ -42,19 +65,22 @@ def hartreefock(xyz_file, basis, is_unrestricted=False):
         xyz_file = os.path.join(data_dir, xyz_file)
     else:
         raise ValueError('Given xyz_file does not exist')
+
    # get coordinates
     with open(xyz_file, 'r') as f:
         lines = [i.strip() for i in f.readlines()[2:]]
         atoms = ';'.join(lines)
+
     # get mol
     mol = gto.M(atom=atoms, basis=basis, parse_arg=False, unit='angstrom')
+
     # get hf
     if is_unrestricted:
-        raise NotImplementedError('Unrestricted or Generalized orbitals are not supported in this PySCF wrapper (yet).')
-    else:
-        hf = scf.RHF(mol)
+        raise NotImplementedError('Unrestricted or Generalized orbitals are not supported in this'
+                                  ' PySCF wrapper (yet).')
+    hf = scf.RHF(mol)
     # run hf
-    # hf.scf()
+    hf.scf()
     # energies
     E_nuc = hf.energy_nuc()
     E_tot = hf.kernel() # HF is solved here
@@ -68,9 +94,11 @@ def hartreefock(xyz_file, basis, is_unrestricted=False):
     # get 2e integral
     eri = ao2mo.full(mol, mo_coeff, verbose=0, intor='cint2e_sph')
     G = ao2mo.restore(1, eri, mol.nao_nr())
+    # NOTE: PySCF uses Chemist's notation
+    G = np.einsum('ijkl->ikjl', G)
     # results
-    result = {'energy' : E_elec,
-              'nuc_nuc' : E_nuc,
+    result = {'el_energy' : E_elec,
+              'nuc_nuc_energy' : E_nuc,
               'H' : (H,),
               'G' : (G,)}
     return result
@@ -99,6 +127,11 @@ def generate_fci_cimatrix(h1e, eri, nelec, is_chemist_notation=False):
     pspace : list(M)
         List of the Slater determinants (in bitstring) that corresponds to each
         row/column of the ci_matrix
+
+    Raises
+    ------
+    ValueError
+        If number of electrons is invalid
     """
     if not is_chemist_notation:
         eri = np.einsum('ijkl->ikjl', eri)
@@ -111,8 +144,10 @@ def generate_fci_cimatrix(h1e, eri, nelec, is_chemist_notation=False):
         nelecb = nelec//2
         # alpha
         neleca = nelec - nelecb
-    else:
+    elif isinstance(nelec, (tuple, list)) and len(nelec) == 2:
         neleca, nelecb = nelec
+    else:
+        raise ValueError('Unsupported electron number, {0}'.format(nelec))
     # integrals
     h1e = np.ascontiguousarray(h1e)
     eri = np.ascontiguousarray(eri)
@@ -127,16 +162,16 @@ def generate_fci_cimatrix(h1e, eri, nelec, is_chemist_notation=False):
     nb = link_indexb.shape[0] # number of "beta" Slater determinants
     num_sd = na*nb # number of Slater determinants in total
 
-    occslista = np.asarray(link_indexa[:,:neleca,0], order='C')
-    occslistb = np.asarray(link_indexb[:,:nelecb,0], order='C')
+    occslista = np.asarray(link_indexa[:, :neleca, 0], order='C')
+    occslistb = np.asarray(link_indexb[:, :nelecb, 0], order='C')
     # Diagonal of CI Hamiltonian matrix
     hdiag = np.empty(num_sd)
     # Coulomb integrals
-    jdiag = np.asarray(np.einsum('iijj->ij',eri), order='C')
+    jdiag = np.asarray(np.einsum('iijj->ij', eri), order='C')
     # Exchange integrals
-    kdiag = np.asarray(np.einsum('ijji->ij',eri), order='C')
+    kdiag = np.asarray(np.einsum('ijji->ij', eri), order='C')
     # Fucking Magic
-    libfci.FCImake_hdiag_uhf(hdiag.ctypes.data_as(ctypes.c_void_p),
+    LIBFCI.FCImake_hdiag_uhf(hdiag.ctypes.data_as(ctypes.c_void_p),
                              h1e.ctypes.data_as(ctypes.c_void_p),
                              h1e.ctypes.data_as(ctypes.c_void_p),
                              jdiag.ctypes.data_as(ctypes.c_void_p),
@@ -157,14 +192,12 @@ def generate_fci_cimatrix(h1e, eri, nelec, is_chemist_notation=False):
     # again, separate the alpha and the bet aparts
     addra, addrb = divmod(addr, nb)
     # bit strings for the alpha and beta parts
-    stra = np.array([cistring.addr2str(norb,neleca,ia) for ia in addra],
-                       dtype=np.uint64)
-    strb = np.array([cistring.addr2str(norb,nelecb,ib) for ib in addrb],
-                       dtype=np.uint64)
+    stra = np.array([cistring.addr2str(norb, neleca, ia) for ia in addra], dtype=np.uint64)
+    strb = np.array([cistring.addr2str(norb, nelecb, ib) for ib in addrb], dtype=np.uint64)
     # number of slater determinants
     ci_matrix = np.zeros((num_sd, num_sd))
     # More Fucking Magic
-    libfci.FCIpspace_h0tril(ci_matrix.ctypes.data_as(ctypes.c_void_p),
+    LIBFCI.FCIpspace_h0tril(ci_matrix.ctypes.data_as(ctypes.c_void_p),
                             h1e.ctypes.data_as(ctypes.c_void_p),
                             eri.ctypes.data_as(ctypes.c_void_p),
                             stra.ctypes.data_as(ctypes.c_void_p),
@@ -172,8 +205,8 @@ def generate_fci_cimatrix(h1e, eri, nelec, is_chemist_notation=False):
                             ctypes.c_int(norb), ctypes.c_int(num_sd))
 
     for i in range(num_sd):
-        ci_matrix[i,i] = hdiag[addr[i]]
-    ci_matrix = pyscf.lib.hermi_triu(ci_matrix)
+        ci_matrix[i, i] = hdiag[addr[i]]
+    ci_matrix = hermi_triu(ci_matrix)
 
     # convert  PySCF Slater determinant notation to one that we use
     pspace = []
--- wfns/wrapper/test/test_pyscf.py
+++ wfns/wrapper/test/test_pyscf.py
@@ -1,36 +1,92 @@
+""" Tests wfns.wrapper.pyscf
+"""
 from __future__ import absolute_import, division, print_function
-import numpy as np
-import scipy.linalg
 import os
+import numpy as np
+from nose.tools import assert_raises
+import scipy.linalg
 from wfns.wrapper.pyscf import hartreefock, generate_fci_cimatrix
 
+
+def check_data_h2_rhf_sto6g(data):
+    """ Checks data for h2 rhf sto6g calculation
+    """
+    assert np.allclose(data['el_energy'], -1.838434259892)
+    assert np.allclose(data['nuc_nuc_energy'], 0.713176830593)
+    assert np.allclose(data['H'], np.array([[-1.25637540e+00, 0.0000000000000],
+                                            [0.0000000000000, -4.80588203e-01]]))
+    assert np.allclose(data['G'], np.array([[[[6.74316543e-01, 0.000000000000],
+                                              [0.000000000000, 1.81610048e-01]],
+                                             [[0.000000000000, 6.64035234e-01],
+                                              [1.81610048e-01, 0.000000000000]]],
+                                            [[[0.000000000000, 1.81610048e-01],
+                                              [6.64035234e-01, 0.000000000000]],
+                                             [[1.81610048e-01, 0.000000000000],
+                                              [0.000000000000, 6.98855952e-01]]]]))
+
+
+def check_data_lih_rhf_sto6g(data):
+    """ Checks data for LiH rhf sto6g calculation
+    """
+    assert np.allclose(data['el_energy'], -7.95197153880 - 0.9953176337)
+    assert np.allclose(data['nuc_nuc_energy'], 0.9953176337)
+    # check types of the integrals
+    assert isinstance(data['H'], tuple)
+    assert len(data['H']) == 1
+    assert isinstance(data['H'][0], np.ndarray)
+    assert isinstance(data['G'], tuple)
+    assert len(data['G']) == 1
+    assert isinstance(data['G'][0], np.ndarray)
+    for matrix in data['H'] + data['G']:
+        assert np.all(np.array(matrix.shape) == data['H'][0].shape[0])
+
+
+def test_hartreefock_h2_rhf_sto6g():
+    """ Tests HF against LiH STO6G
+    """
+    # file location specified (absolute)
+    hf_dict = hartreefock("{0}/../../../data/test/h2.xyz".format(os.path.dirname(__file__)),
+                          "sto-6g")
+    check_data_h2_rhf_sto6g(hf_dict)
+
+    # file location specified (relative)
+    hf_dict = hartreefock("../../../data/test/h2.xyz", "sto-6g")
+    check_data_h2_rhf_sto6g(hf_dict)
+
+    # data reference
+    hf_dict = hartreefock("test/h2.xyz", "sto-6g")
+    check_data_h2_rhf_sto6g(hf_dict)
+
+    # bad file
+    assert_raises(ValueError, lambda: hartreefock('magic.xyz', 'sto-6g'))
+
+    # unrestricted
+    assert_raises(NotImplementedError, lambda: hartreefock("test/h2.xyz", "sto-6g",
+                                                           is_unrestricted=True))
+
+
 def test_hartreefock():
-    """ Tests wfns.wrapper.pyscf.hartreefock
+    """ Tests HF against LiH STO6G
 
     """
+    # file location specified
+    hf_dict = hartreefock("{0}/../../../data/test/lih.xyz".format(os.path.dirname(__file__)),
+                          "sto-6g")
+    check_data_lih_rhf_sto6g(hf_dict)
+
     hf_dict = hartreefock("test/lih.xyz", "sto-6g")
-    E_hf, H, G, nuc_nuc = hf_dict["energy"], hf_dict["H"], hf_dict["G"], hf_dict["nuc_nuc"]
+    check_data_lih_rhf_sto6g(hf_dict)
 
-    # compare energies with Gaussian results
-    # Total energy : -7.95197153880 Hartree
-    # Nuclear repulsion energy : 0.9953176337 Hartree
-    print(abs(E_hf - (-7.95197153880 - 0.9953176337)))
-    assert abs(E_hf - (-7.95197153880 - 0.9953176337)) < 1e-8
-    assert abs(nuc_nuc - (0.9953176337)) < 1e-8
-    # check types of the integrals
-    assert isinstance(H, tuple)
-    for i in H:
-        assert isinstance(i, np.ndarray)
-    assert isinstance(G, tuple)
-    for i in G:
-        assert isinstance(i, np.ndarray)
-    for matrix in H+G:
-        assert np.all(np.array(matrix.shape) == H[0].shape[0])
+    # bad file
+    assert_raises(ValueError, lambda: hartreefock('magic.xyz', 'sto-6g'))
 
-def test_generate_fci_cimatrix():
-    """ Tests wfns.wrapper.pyscf.generate_fci_cimatrix
+    # unrestricted
+    assert_raises(NotImplementedError, lambda: hartreefock("test/lih.xyz", "sto-6g",
+                                                           is_unrestricted=True))
 
-    Code is tested by seeing if CI matrix can be used to create the FCI energies of H2 and LiH
+
+def test_generate_fci_cimatrix_h2_631gs():
+    """ Tests generate_fci_cimatrix with H2 6-31G*
     """
     #### H2 ####
     # HF energy: -1.13126983927
@@ -39,15 +95,33 @@ def test_generate_fci_cimatrix():
     hf_dict = hartreefock(data_path, '6-31gs')
 
     nelec = 2
-    E_hf = hf_dict["energy"]
+    nuc_nuc = hf_dict["nuc_nuc_energy"]
     H = hf_dict["H"]
     G = hf_dict["G"]
-    nuc_nuc = hf_dict["nuc_nuc"]
 
-    ci_matrix, pspace = generate_fci_cimatrix(H[0], G[0], nelec, is_chemist_notation=True)
+    # nelec is number
+    ci_matrix, pspace = generate_fci_cimatrix(H[0], G[0], nelec, is_chemist_notation=False)
+    ground_energy = scipy.linalg.eigh(ci_matrix)[0][0] + nuc_nuc
+    assert abs(ground_energy - (-1.1651486697)) < 1e-7
+    # nelec is tuple
+    ci_matrix, pspace = generate_fci_cimatrix(H[0], G[0], (1, 1), is_chemist_notation=False)
+    ground_energy = scipy.linalg.eigh(ci_matrix)[0][0] + nuc_nuc
+    assert abs(ground_energy - (-1.1651486697)) < 1e-7
+    # invalid nelec
+    assert_raises(ValueError,
+                  lambda: generate_fci_cimatrix(H[0], G[0], '2', is_chemist_notation=False))
+    assert_raises(ValueError,
+                  lambda: generate_fci_cimatrix(H[0], G[0], (1, 1, 2), is_chemist_notation=False))
+    # chemist notation
+    ci_matrix, pspace = generate_fci_cimatrix(H[0], np.einsum('ikjl->ijkl', G[0]), 2,
+                                              is_chemist_notation=True)
     ground_energy = scipy.linalg.eigh(ci_matrix)[0][0] + nuc_nuc
     assert abs(ground_energy - (-1.1651486697)) < 1e-7
 
+
+def test_generate_fci_cimatrix_lih_sto6g():
+    """ Tests generate_fci_cimatrix with LiH STO-6G
+    """
     #### LiH ####
     # HF energy: -7.95197153880
     # FCI energy: -7.9723355823
@@ -55,11 +129,26 @@ def test_generate_fci_cimatrix():
     hf_dict = hartreefock(data_path, 'sto-6g')
 
     nelec = 4
-    E_hf = hf_dict["energy"]
+    E_hf = hf_dict["el_energy"]
+    nuc_nuc = hf_dict["nuc_nuc_energy"]
     H = hf_dict["H"]
     G = hf_dict["G"]
-    nuc_nuc = hf_dict["nuc_nuc"]
 
-    ci_matrix, pspace = generate_fci_cimatrix(H[0], G[0], nelec, is_chemist_notation=True)
+    # nelec is int
+    ci_matrix, pspace = generate_fci_cimatrix(H[0], G[0], nelec, is_chemist_notation=False)
+    ground_energy = scipy.linalg.eigh(ci_matrix)[0][0] + nuc_nuc
+    assert abs(ground_energy - (-7.9723355823)) < 1e-7
+    # nelec is tuple
+    ci_matrix, pspace = generate_fci_cimatrix(H[0], G[0], (1, 1), is_chemist_notation=False)
+    ground_energy = scipy.linalg.eigh(ci_matrix)[0][0] + nuc_nuc
+    assert abs(ground_energy - (-7.9723355823)) < 1e-7
+    # invalid nelec
+    assert_raises(ValueError,
+                  lambda: generate_fci_cimatrix(H[0], G[0], '2', is_chemist_notation=False))
+    assert_raises(ValueError,
+                  lambda: generate_fci_cimatrix(H[0], G[0], (1, 1, 2), is_chemist_notation=False))
+    # chemist notation
+    ci_matrix, pspace = generate_fci_cimatrix(H[0], np.einsum('ikjl->ijkl', G[0]), 2,
+                                              is_chemist_notation=True)
     ground_energy = scipy.linalg.eigh(ci_matrix)[0][0] + nuc_nuc
     assert abs(ground_energy - (-7.9723355823)) < 1e-7
