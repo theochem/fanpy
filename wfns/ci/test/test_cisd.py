48d02b73ded258703c1c5181ef4c4e8f62819362
Taewon D. Kim <kimt33@mcmaster.ca>
2017-01-02 13:11:53 -0500

    QA cisd
    What:
     1. Update documentation
     2. Make appropriate changes (Because CIWavefunction changed)
     3. Test methods

--- wfns/ci/test/test_cisd.py
+++ wfns/ci/test/test_cisd.py
@@ -1,11 +1,21 @@
+""" Tests wfns.ci.cisd
+"""
 from __future__ import absolute_import, division, print_function
 import numpy as np
 from wfns.ci.solver import solve
 from wfns.ci.cisd import CISD
+from wfns.sd_list import sd_list
 from wfns.wrapper.horton import gaussian_fchk
 
 
-def test_cisd_h2():
+def test_generate_civec():
+    """ Tests CISD.generate_civec
+    """
+    test = CISD(2, np.ones((3, 3)), np.ones((3, 3, 3, 3)))
+    assert test.generate_civec() == sd_list(test.nelec, test.nspatial, num_limit=None, spin=None,
+                                            seniority=None, exc_orders=[1, 2])
+
+def test_cisd_h2_631gdp():
     """ Tests CISD wavefunction using H2 (6-31G**)
 
     Compared to Gausssian results
@@ -19,23 +29,21 @@ def test_cisd_h2():
     # HF energy: -1.13126983927
     # FCI energy: -1.1651487496
     hf_dict = gaussian_fchk('test/h2_hf_631gdp.fchk')
-
     nelec = 2
     one_int = hf_dict["one_int"]
     two_int = hf_dict["two_int"]
     nuc_nuc = hf_dict["nuc_nuc_energy"]
+
     cisd = CISD(nelec=nelec, one_int=one_int, two_int=two_int, nuc_nuc=nuc_nuc, spin=0)
     ci_matrix = cisd.compute_ci_matrix()
     # compare HF numbers
     assert abs(ci_matrix[0, 0] + cisd.nuc_nuc - (-1.131269841877) < 1e-8)
-    # check that hamiltonian is symmetric
-    assert np.allclose(ci_matrix, ci_matrix.T)
     # solve
     solve(cisd)
     # compare with number from Gaussian
     assert abs(cisd.get_energy() - (-1.1651486697)) < 1e-7
 
-def test_cisd_lih():
+def test_cisd_lih_631g():
     """ Tests CISD wavefunction using LiH (6-31G)
 
     Compared to Molpro results
@@ -49,7 +57,6 @@ def test_cisd_lih():
     # HF energy: -7.97926895
     # CISD energy: -7.99826182
     hf_dict = gaussian_fchk('test/lih_hf_631g.fchk')
-
     nelec = 4
     one_int = hf_dict["one_int"]
     two_int = hf_dict["two_int"]
@@ -60,11 +67,7 @@ def test_cisd_lih():
 
     # compare HF numbers
     assert abs(ci_matrix[0, 0] + cisd.nuc_nuc - (-7.97926895) < 1e-8)
-    # check that hamiltonian is symmetric
-    assert np.allclose(ci_matrix, ci_matrix.T)
     # solve
     solve(cisd)
-
     # compare with number from Gaussian
-    print(cisd.get_energy(), (-7.99826182))
     assert abs(cisd.get_energy() - (-7.99826182)) < 1e-7
