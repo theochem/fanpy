faa2b0be00df25b128cedc2db905905495a65437
Taewon D. Kim <kimt33@mcmaster.ca>
2017-05-10 08:30:59 -0400

    Remove redundant pre-commit installer
    Already included in qa

--- git-hooks/install.sh
+++
@@ -1,8 +0,0 @@
-# Install git hooks for the project.
-
-DIR=$(dirname $(readlink -f ${BASH_SOURCE:-0}))
-cd ${DIR}
-for HOOK in *.hook
-do
-    ln -s ${DIR}/${HOOK} ${DIR}/../.git/hooks/$(echo ${HOOK} | sed "s/.hook$//")
-done
