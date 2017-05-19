068f45f45e895613c214d473c865175de534987d
Taewon D. Kim <kimt33@mcmaster.ca>
2017-05-19 00:47:29 -0400

    Use integral module to check integrals in ChemicalHamiltonian.assign_integrals

--- wfns/hamiltonian/chemical_hamiltonian.py
+++ wfns/hamiltonian/chemical_hamiltonian.py
@@ -13,8 +13,8 @@ sen0_hamiltonian(wfn, slater_d, orbtype, deriv=None)
     a Slater determinant
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-import numpy as np
 from .base_hamiltonian import BaseHamiltonian
+from ..backend.integrals import OneElectronIntegrals, TwoElectronIntegrals
 
 
 class ChemicalHamiltonian(BaseHamiltonian):
@@ -118,11 +118,11 @@ class ChemicalHamiltonian(BaseHamiltonian):
 
         Parameters
         ----------
-        one_int : 1- or 2-tuple np.ndarray(K,K)
+        one_int : {np.ndarray(K,K), 1- or 2-tuple np.ndarray(K,K), OneElectronIntegrals}
             One electron integrals for restricted, unrestricted, or generalized orbitals
             1-tuple for spatial (restricted) and generalized orbitals
             2-tuple for unrestricted orbitals (alpha-alpha and beta-beta components)
-        two_int : 1- or 3-tuple np.ndarray(K,K)
+        two_int : {np.ndarray(K,K), 1- or 3-tuple np.ndarray(K,K), TwoElectronIntegrals}
             Two electron integrals for restricted, unrestricted, or generalized orbitals
             In physicist's notation
             1-tuple for spatial (restricted) and generalized orbitals
@@ -132,17 +132,23 @@ class ChemicalHamiltonian(BaseHamiltonian):
         Raises
         ------
         TypeError
-            If `one_int` and `two_int` do not have the same type
-            If `one_int` is not a numpy array or 1- or 2-tuple of numpy arrays
-            If `two_int` is not a numpy array or 1- or 3-tuple of numpy arrays
-            If the orbital type does not match up with the given integrals
-            If `one_int` and `two_int` matrices are not numpy arrays
-            If `one_int` and `two_int` matrices do not have the same dtype (must be one of float and
-            complex)
-            If `one_int` and `two_int` matrices do not have the same dimensionality along each axis.
-            If `one_int` is not two dimensional
-            If `two_int` is not four dimensional
-        ValueError
+            If integrals are not provided as a numpy array or a tuple of numpy arrays
+            If integral matrices do not have dtype of `float` or `complex`
+            If integral matrices do not have the same dtype
+            If the number of one-electron integral matrices is not 1 or 2
+            If one-electron integral matrices are not two dimensional
+            If one-electron integral matrices are not square
+            If one-electron integrals (for the unrestricted orbitals) has different number of alpha
+            and beta orbitals
+            If the number of two-electron integral matrices is not 1 or 3
+            If two-electron integral matrices are not four dimensional
+            If two-electron integral matrices does not have same dimensionality in all axis
+            If two-electron integrals (for the unrestricted orbitals) has different number of alpha
+            and beta orbitals
+            If one- and two-electron integrals do not have the same number of orbitals
+            If one- and two-electron integrals do not correspond to the same orbital type (i.e.
+            restricted, unrestricted, generalized).
+            If orbital type of the integrals do not match with the given orbital type
         NotImplementedError
             If generalized orbitals and odd number of spin orbitals
 
@@ -163,44 +169,20 @@ class ChemicalHamiltonian(BaseHamiltonian):
                 two_int can be a np.ndarray or a 1-tuple of np.ndarray
                 orbtype = 'generalized'
         """
-        if type(one_int) != type(two_int):
-            raise TypeError('Both the one- and two-electron integrals must have the same type.')
-        elif not isinstance(one_int, (list, tuple)) and isinstance(one_int, np.ndarray):
-            one_int, two_int = (one_int, ), (two_int, )
-            possible_orbtype = ('restricted', 'generalized')
-        elif isinstance(one_int, (list, tuple)) and len(one_int) == 1 and len(two_int) == 1:
-            possible_orbtype = ('restricted', 'generalized')
-        elif isinstance(one_int, (list, tuple)) and len(one_int) == 2 and len(two_int) == 3:
-            possible_orbtype = ('unrestricted', )
-        else:
-            raise TypeError('One- and two-electron integrals must be given '
-                            'as a numpy array or a 1-tuple of numpy array for restricted and '
-                            ' generalized orbitals, '
-                            'or as a 2-tuple of numpy arrays (one-electron integrals) and 3-tuple '
-                            'of numpy arrays (two-electron integrals) for unrestricted orbitals.')
-
-        if self.orbtype not in possible_orbtype:
-            raise TypeError('The orbital type specified, `{0}`, does not match up with the '
-                             'possible orbital types of the given integrals.'.format(self.orbtype))
-
-        for matrix in one_int + two_int:
-            if isinstance(matrix, np.ndarray):
-                raise TypeError('Integral matrices must be a numpy array.')
-            if matrix.dtype != one_int[0].dtype in (float, complex):
-                raise TypeError('Integral matrices must consistent dtype and be one of float or '
-                                'complex.')
-            if not all(i == one_int[0].shape[0] for i in matrix.shape):
-                raise TypeError('All of the integral matrices must have the same dimensionality '
-                                'along each axis.')
-        if not all(len(matrix.shape) == 2 for matrix in one_int):
-            raise TypeError('One-electron integral matrices must be two dimensional.')
-        if not all(len(matrix.shape) == 4 for matrix in two_int):
-            raise TypeError('Two-electron integral matrices must be four dimensional.')
-
+        one_int = OneElectronIntegrals(one_int)
+        two_int = TwoElectronIntegrals(two_int)
+        if one_int.num_orbs != two_int.num_orbs:
+            raise TypeError('One- and two-electron integrals do not have the same number of '
+                            'orbitals.')
+        if one_int.possible_orbtypes != two_int.possible_orbtypes:
+            raise TypeError('One- and two-electron integrals do not correspond to the same orbital '
+                            'type (i.e. restricted, unrestricted, generalized).')
+        if self.orbtype not in one_int.possible_orbtypes:
+            raise TypeError('Orbital type of the integrals do not match with the given orbital '
+                            'type, {0}'.format(self.orbtype))
         if self.orbtype == 'generalized' and one_int[0].shape[0] % 2 == 1:
             raise NotImplementedError('Odd number of "spin" orbitals will cause problems when '
                                       ' constructing Slater determinants.')
-        # TODO: check that two electron integrals are in physicist's notation (check symmetry)
 
         self.one_int = one_int
         self.two_int = two_int
