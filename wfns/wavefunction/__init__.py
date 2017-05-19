486f4a001e48af16c67d88a74da4f9f942cb16ff
Taewon D. Kim <kimt33@mcmaster.ca>
2017-05-19 17:34:46 -0400

    Turn off initialization of solver and wavefunction
    Temporary fix for debugging/testing other directories

--- wfns/wavefunction/__init__.py
+++ wfns/wavefunction/__init__.py
@@ -1,18 +1,18 @@
 """Wavefunction."""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from . import wavefunction
-from . import proj_wavefunction
-from . import geminal
-from . import ap1rog
-from . import apr2g
-from . import apig
-from . import apfsg
-from . import apseqg
-from . import apsetg
-from . import apg
-from . import ci_wavefunction
-from . import fci
-from . import doci
-from . import cisd
-from . import ci_pairs
+# from . import wavefunction
+# from . import proj_wavefunction
+# from . import geminal
+# from . import ap1rog
+# from . import apr2g
+# from . import apig
+# from . import apfsg
+# from . import apseqg
+# from . import apsetg
+# from . import apg
+# from . import ci_wavefunction
+# from . import fci
+# from . import doci
+# from . import cisd
+# from . import ci_pairs
