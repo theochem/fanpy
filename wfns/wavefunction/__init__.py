e78329a9286662394ac6be1d696d96c31b747bc5
Taewon D. Kim <kimt33@mcmaster.ca>
2017-05-22 20:35:36 -0400

    Move geminal wavefunctions to geminals directory

--- wfns/wavefunction/__init__.py
+++ wfns/wavefunction/__init__.py
@@ -4,10 +4,3 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from . import base_wavefunction
 # from . import proj_wavefunction
 from . import geminals
-# from . import ap1rog
-# from . import apr2g
-# from . import apig
-# from . import apfsg
-# from . import apseqg
-# from . import apsetg
-# from . import apg
