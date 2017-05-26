df6de7af5271965adc9d8665a16fe2c225a3c15c
Taewon D. Kim <kimt33@mcmaster.ca>
2017-05-26 19:33:09 -0400

    Update __init__

--- wfns/wavefunction/__init__.py
+++ wfns/wavefunction/__init__.py
@@ -2,6 +2,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from . import base_wavefunction
-# from . import proj_wavefunction
 from . import ci
 from . import geminals
