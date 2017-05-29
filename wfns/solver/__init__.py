7ab37c20b3812917fb657cc0fcda50edae51c5ef
Taewon D. Kim <kimt33@mcmaster.ca>
2017-05-29 00:46:24 -0400

    Add base classes for solving Schrodinger equation
    What:
     1. Create base class
    Why:
     1. Because different solvers (I can think of at least 3) will have
     similar initialization steps

--- wfns/solver/__init__.py
+++ wfns/solver/__init__.py
@@ -1,5 +1,6 @@
 """Solver of the wavefunction given the Hamiltonian."""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from . import base_solver
 # from . import solver
 # from . import solver_ci
