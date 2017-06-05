9dd1ea01a951a977fe9f97d999e4f3dc10b0a476
Taewon D. Kim <kimt33@mcmaster.ca>
2017-06-05 14:08:51 -0400

    Try to make equation_solver more robust
     1. Add constrain method (for normalization)
     2. Update and clear cache when given parameter are sufficiently
     different from the existing parameters
     3. Add reference Slater determinant with respect to which the
     wavefunctgion is normalized

--- wfns/solver/equation_solver.py
+++ wfns/solver/equation_solver.py
@@ -8,8 +8,8 @@ from ..hamiltonian.chemical_hamiltonian import ChemicalHamiltonian
 
 
 # FIXME: copies most of system_solver for initialization and docstring
-def optimize_wfn_variational(wfn, ham, left_pspace=None, right_pspace=None, save_file='',
-                             solver=None, solver_kwargs=None):
+def optimize_wfn_variational(wfn, ham, left_pspace=None, right_pspace=None, ref_sds=None,
+                             save_file='', solver=None, solver_kwargs=None, norm_constrained=False):
     """Optimize the wavefunction with the given Hamiltonian as a single equation for energy.
 
     Solves the following equation
@@ -19,15 +19,13 @@ def optimize_wfn_variational(wfn, ham, left_pspace=None, right_pspace=None, save
         \hat{H}
         \sum_{\mathbf{n} \in P_{right}} \ket{\mathbf{n}} \bra{\mathbf{n}}
         \ket{\Psi}
-        &= E \bra{\Psi}
-        \sum_{\mathbf{m} \in P_{left}} \ket{\mathbf{m}} \bra{\mathbf{m}}
-        \sum_{\mathbf{n} \in P_{right}} \ket{\mathbf{n}} \bra{\mathbf{n}}
-        \ket{\Psi}\\
+        &= E \sum_{\mathbf{m} \in P_{ref}}
+        \braket{ \Psi | \mathbf{m}} \braket{\mathbf{m} | \Psi}\\
         \sum_{\mathbf{m} \in P_{left}} \sum_{\mathbf{n} \in P_{right}}
         \braket{\Psi | \mathbf{m}}
         \braket{\mathbf{m} | \hat{H} | \mathbf{n}}
         \braket{\mathbf{n} | \Psi}
-        &= E \sum_{\mathbf{m} \in P_{left} \cap P_{right}}
+        &= E \sum_{\mathbf{m} \in P_{ref}}
         \braket{ \Psi | \mathbf{m}} \braket{\mathbf{m} | \Psi}
 
     where :math:`P_{left}` and :math:`P_{right}` are the set of Slater determinants for which the
@@ -61,6 +59,9 @@ def optimize_wfn_variational(wfn, ham, left_pspace=None, right_pspace=None, save
         Slater determinants onto which the right side of the Schrodinger equation is projected
         Tuple of objects that are compatible with the internal_sd format
         By default, the largest space is used
+    ref_sds : tuple/list of int, None
+        One or more Slater determinant with respect to which the energy and the norm are calculated
+        Default is ground state HF
     save_file : str
         Name of the numpy file that contains the wavefunction parameters of the last optimization
         step
@@ -127,83 +128,110 @@ def optimize_wfn_variational(wfn, ham, left_pspace=None, right_pspace=None, save
         left_pspace = np.array([slater.internal_sd(sd) for sd in left_pspace])
         right_pspace = np.array([slater.internal_sd(sd) for sd in right_pspace])
 
+    if ref_sds is None:
+        ref_sds = sd_list.sd_list(wfn.nelec, wfn.nspatial, spin=wfn.spin, seniority=wfn.seniority)
+    else:
+        ref_sds = [slater.internal_sd(sd) for sd in ref_sds]
+
     if not isinstance(save_file, str):
         raise TypeError('save_file must be a string.')
+
+    def _constraint(params):
+        if not np.allclose(params, wfn.params.flat, atol=1e-12, rtol=0):
+            wfn.params = params.reshape(wfn.params_shape)
+            wfn.clear_cache()
+        return sum(wfn.get_overlap(sd)**2 for sd in ref_sds) - 1
+
+    def _d_constraint(params):
+        if not np.allclose(params, wfn.params.flat, atol=1e-12, rtol=0):
+            wfn.params = params.reshape(wfn.params_shape)
+            wfn.clear_cache()
+        return np.array([sum(2 * wfn.get_overlap(sd) * wfn.get_overlap(sd, deriv=j)
+                             for sd in ref_sds)
+                         for j in range(wfn.nparams)])
 
     # FIXME: incredibly slow implementation
     # objective
     def _objective(params):
         """Energy of the Schrodinger equation after projecting out the left and right sides."""
         # update wavefunction
-        wfn.params = params.reshape(wfn.params_shape)
+        if not np.allclose(params, wfn.params.flat, atol=1e-12, rtol=0):
+            wfn.params = params.reshape(wfn.params_shape)
+            wfn.clear_cache()
 
         # save params
         if save_file != '':
             np.save('{0}_temp.npy'.format(save_file), wfn.params)
 
-        # clear cache
-        wfn.clear_cache()
-
         # energy
         numerator = 0
-        # norm
-        denominator = 0
         for sd1 in left_pspace:
             if right_pspace is None:
                 numerator += wfn.get_overlap(sd1) * sum(ham.integrate_wfn_sd(wfn, sd1))
-                denominator += wfn.get_overlap(sd1)**2
             else:
                 for sd2 in right_pspace:
                     sd_energy = sum(ham.integrate_sd_sd(sd1, sd2))
                     # NOTE: caching here would be important
                     if sd_energy != 0:
                         numerator += wfn.get_overlap(sd1) * sd_energy * wfn.get_overlap(sd2)
-                    if sd1 == sd2:
-                        denominator += wfn.get_overlap(sd1)**2
+        # norm
+        if not norm_constrained:
+            denominator = _constraint(params) + 1
+        else:
+            denominator = 1.0
         return numerator / denominator
 
     # FIXME: incredibly slow implementation
     # gradiant
     def _gradient(params):
         """Gradient of the energy of the Schrodinger equation after projection."""
+        if not np.allclose(params, wfn.params.flat, atol=1e-12, rtol=0):
+            wfn.params = params.reshape(wfn.params_shape)
+            wfn.clear_cache()
+
         grad = np.zeros(wfn.nparams, dtype=wfn.dtype)
-        for j in range(grad.size):
-            # energy
-            numerator = 0.0
-            d_numerator = 0.0
-            # norm
-            denominator = 0.0
-            d_denominator = 0.0
-            for sd1 in left_pspace:
-                if right_pspace is None:
-                    sd_energy = sum(ham.integrate_wfn_sd(wfn, sd1))
-                    d_sd_energy = sum(ham.integrate_wfn_sd(wfn, sd1, deriv=j))
 
-                    numerator += wfn.get_overlap(sd1) * sd_energy
-                    denominator += wfn.get_overlap(sd1)**2
-                    d_numerator += wfn.get_overlap(sd1, deriv=j) * sd_energy
-                    d_numerator += wfn.get_overlap(sd1) * d_sd_energy
-                    d_denominator += 2 * wfn.get_overlap(sd1) * wfn.get_overlap(sd1, deriv=j)
-                else:
-                    for sd2 in right_pspace:
-                        # sd_energy is independent of wavefunction parameters
-                        sd_energy = sum(ham.integrate_sd_sd(sd1, sd2))
-                        # NOTE: caching here would be important
-                        if sd_energy != 0:
-                            numerator += wfn.get_overlap(sd1) * sd_energy * wfn.get_overlap(sd2)
-                            d_numerator += (wfn.get_overlap(sd1, deriv=j) * sd_energy
-                                            * wfn.get_overlap(sd2))
-                            d_numerator += (wfn.get_overlap(sd1) * sd_energy
-                                            * wfn.get_overlap(sd2, deriv=j))
-                        if sd1 == sd2:
-                            denominator += wfn.get_overlap(sd1)**2
-                            d_denominator += 2*wfn.get_overlap(sd1) * wfn.get_overlap(sd1, deriv=j)
+        if not norm_constrained:
+            # FIXME: might not be too efficient
+            denominator = _constraint(params) + 1
+            d_denominator = _d_constraint(params)
+        else:
+            denominator = 1.0
+            d_denominator = np.zeros(wfn.nparams, dtype=wfn.dtype)
 
-            grad[j] = (numerator * d_denominator - d_numerator * denominator) / denominator**2
+        # energy
+        numerator = 0.0
+        d_numerator = np.zeros(grad.size, dtype=wfn.dtype)
+        for sd1 in left_pspace:
+            if right_pspace is None:
+                sd_energy = sum(ham.integrate_wfn_sd(wfn, sd1))
+                numerator += wfn.get_overlap(sd1) * sd_energy
+                for j in range(grad.size):
+                    d_numerator[j] += wfn.get_overlap(sd1, deriv=j) * sd_energy
+                    d_numerator[j] += wfn.get_overlap(sd1) * sum(ham.integrate_wfn_sd(wfn, sd1,
+                                                                                      deriv=j))
+            else:
+                for sd2 in right_pspace:
+                    # sd_energy is independent of wavefunction parameters
+                    sd_energy = sum(ham.integrate_sd_sd(sd1, sd2))
+                    # NOTE: caching here would be important
+                    if sd_energy != 0:
+                        numerator += wfn.get_overlap(sd1) * sd_energy * wfn.get_overlap(sd2)
+                        for j in range(grad.size):
+                            d_numerator[j] += (wfn.get_overlap(sd1, deriv=j) * sd_energy
+                                               * wfn.get_overlap(sd2))
+                            d_numerator[j] += (wfn.get_overlap(sd1) * sd_energy
+                                               * wfn.get_overlap(sd2, deriv=j))
+
+        grad = (d_numerator * denominator - numerator * d_denominator) / denominator**2
         return grad
 
     # check solver
     if solver is None:
+        solver_name = 'bfgs'
+        solver = scipy.optimize.minimize
+    elif isinstance(solver, str) and solver.lower() in ['bfgs', 'powell']:
+        solver_name = solver.lower()
         solver = scipy.optimize.minimize
 
     # check keyword arguments
@@ -213,15 +241,11 @@ def optimize_wfn_variational(wfn, ham, left_pspace=None, right_pspace=None, save
         raise TypeError('solver_kwargs must be a dictionary or None')
 
     # set keyword arguments
-    if solver.__name__ == 'minimize' and 'scipy.optimize' in solver.__module__:
+    default_kwargs = {}
+    if solver_name == 'bfgs':
         default_kwargs = {'method': 'BFGS', 'jac': _gradient, 'options': {'gtol': 1e-8}}
-        # if no jacobian
-        if 'jac' in solver_kwargs and solver_kwargs['jac'] in [None, False]:
-            del solver_kwargs['jac']
-            del default_kwargs['jac']
-            default_kwargs.update({'method': 'BFGS'})
-    else:
-        default_kwargs = {}
+    elif solver_name == 'powell':
+        default_kwargs.update({'method': 'Powell', 'options': {'xtol': 1e-9, 'ftol': 1e-9}})
 
     # overwrite default keyword arguments
     default_kwargs.update(solver_kwargs)
