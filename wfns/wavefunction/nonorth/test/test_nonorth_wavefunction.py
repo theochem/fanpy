9be61e69e81d2ca3b3ac3d994a9eb8832d19a2bb
Taewon D. Kim <kimt33@mcmaster.ca>
2017-06-22 13:19:45 +0900

    Fix up NonorthWavefunction
     1. Add tests for get_overlap
     2. Add signatures of cofactors
     3. Reorganize
     4. Add derivative of get_overlap

--- wfns/wavefunction/nonorth/test/test_nonorth_wavefunction.py
+++ wfns/wavefunction/nonorth/test/test_nonorth_wavefunction.py
@@ -268,3 +268,180 @@ def test_nonorth_orbtype():
     # generalized
     test.assign_params(np.random.rand(10, 12))
     assert test.orbtype == 'generalized'
+    # else
+    test.params = np.random.rand(10, 12)
+    assert_raises(NotImplementedError, lambda: test.orbtype)
+
+
+def test_nonorth_get_overlap():
+    """Test NonorthWavefunction.get_overap."""
+    test = TestNonorthWavefunction()
+    test.nelec = 2
+    test.nspin = 4
+    test.dtype = np.float64
+    test.memory = 10
+    test.assign_wfn(None)
+    test._cache_fns = {}
+    test.wfn.params = np.arange(1, 7)
+    wfn_sd_coeff = {0b0101: 1, 0b0110: 2, 0b1100: 3, 0b0011: 4, 0b1001: 5, 0b1010: 6}
+
+    # generalized
+    test.params = [np.arange(1, 17).reshape(4, 4)]
+    # 0b0101 uses [[1, 2, 3, 4],
+    #              [9, 10, 11, 12]]
+    assert np.isclose(test.get_overlap(0b0101), ((1*10 - 2*9) * wfn_sd_coeff[0b0011] +
+                                                 (1*11 - 3*9) * wfn_sd_coeff[0b0101] +
+                                                 (1*12 - 4*9) * wfn_sd_coeff[0b1001] +
+                                                 (2*11 - 3*10) * wfn_sd_coeff[0b0110] +
+                                                 (2*12 - 4*10) * wfn_sd_coeff[0b1010] +
+                                                 (3*12 - 4*11) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # 0b0110 uses [[5, 6, 7, 8],
+    #              [9, 10, 11, 12]]
+    assert np.isclose(test.get_overlap(0b0110), ((5*10 - 6*9) * wfn_sd_coeff[0b0011] +
+                                                 (5*11 - 7*9) * wfn_sd_coeff[0b0101] +
+                                                 (5*12 - 8*9) * wfn_sd_coeff[0b1001] +
+                                                 (6*11 - 7*10) * wfn_sd_coeff[0b0110] +
+                                                 (6*12 - 8*10) * wfn_sd_coeff[0b1010] +
+                                                 (7*12 - 8*11) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # 0b1100 uses [[9, 10, 11, 12],
+    #              [13, 14, 15, 16]]
+    assert np.isclose(test.get_overlap(0b1100), ((9*14 - 10*13) * wfn_sd_coeff[0b0011] +
+                                                 (9*15 - 11*13) * wfn_sd_coeff[0b0101] +
+                                                 (9*16 - 12*13) * wfn_sd_coeff[0b1001] +
+                                                 (10*15 - 11*14) * wfn_sd_coeff[0b0110] +
+                                                 (10*16 - 12*14) * wfn_sd_coeff[0b1010] +
+                                                 (11*16 - 12*15) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+
+    # unrestricted
+    test.clear_cache()
+    test.params = [np.array([[1, 2], [5, 6]]), np.array([[11, 12], [15, 16]])]
+    # 0b0101 uses [[1, 2, 0, 0],
+    #              [0, 0, 11, 12]]
+    assert np.isclose(test.get_overlap(0b0101), ((1*0 - 2*0) * wfn_sd_coeff[0b0011] +
+                                                 (1*11 - 0*0) * wfn_sd_coeff[0b0101] +
+                                                 (1*12 - 0*0) * wfn_sd_coeff[0b1001] +
+                                                 (2*11 - 0*0) * wfn_sd_coeff[0b0110] +
+                                                 (2*12 - 0*0) * wfn_sd_coeff[0b1010] +
+                                                 (0*12 - 0*11) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # 0b0110 uses [[5, 6, 0, 0],
+    #              [0, 0, 11, 12]]
+    assert np.isclose(test.get_overlap(0b0110), ((5*0 - 6*0) * wfn_sd_coeff[0b0011] +
+                                                 (5*11 - 0*0) * wfn_sd_coeff[0b0101] +
+                                                 (5*12 - 0*0) * wfn_sd_coeff[0b1001] +
+                                                 (6*11 - 0*0) * wfn_sd_coeff[0b0110] +
+                                                 (6*12 - 0*0) * wfn_sd_coeff[0b1010] +
+                                                 (0*12 - 0*11) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # 0b1100 uses [[0, 0, 11, 12],
+    #              [0, 0, 15, 16]]
+    assert np.isclose(test.get_overlap(0b1100), ((0*0 - 0*0) * wfn_sd_coeff[0b0011] +
+                                                 (0*15 - 11*0) * wfn_sd_coeff[0b0101] +
+                                                 (0*16 - 12*0) * wfn_sd_coeff[0b1001] +
+                                                 (0*15 - 11*0) * wfn_sd_coeff[0b0110] +
+                                                 (0*16 - 12*0) * wfn_sd_coeff[0b1010] +
+                                                 (11*16 - 12*15) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # restricted
+    test.clear_cache()
+    test.params = [np.array([[1, 2], [5, 6]])]
+    # 0b0101 uses [[1, 2, 0, 0],
+    #              [0, 0, 1, 2]]
+    assert np.isclose(test.get_overlap(0b0101), ((1*0 - 2*0) * wfn_sd_coeff[0b0011] +
+                                                 (1*1 - 0*0) * wfn_sd_coeff[0b0101] +
+                                                 (1*2 - 0*0) * wfn_sd_coeff[0b1001] +
+                                                 (2*1 - 0*0) * wfn_sd_coeff[0b0110] +
+                                                 (2*2 - 0*0) * wfn_sd_coeff[0b1010] +
+                                                 (0*2 - 0*1) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # 0b0110 uses [[5, 6, 0, 0],
+    #              [0, 0, 1, 2]]
+    assert np.isclose(test.get_overlap(0b0110), ((5*0 - 6*0) * wfn_sd_coeff[0b0011] +
+                                                 (5*1 - 0*0) * wfn_sd_coeff[0b0101] +
+                                                 (5*2 - 0*0) * wfn_sd_coeff[0b1001] +
+                                                 (6*1 - 0*0) * wfn_sd_coeff[0b0110] +
+                                                 (6*2 - 0*0) * wfn_sd_coeff[0b1010] +
+                                                 (0*1 - 0*2) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # 0b1100 uses [[0, 0, 1, 2],
+    #              [0, 0, 5, 6]]
+    assert np.isclose(test.get_overlap(0b1100), ((0*0 - 0*0) * wfn_sd_coeff[0b0011] +
+                                                 (0*1 - 0*5) * wfn_sd_coeff[0b0101] +
+                                                 (0*2 - 0*6) * wfn_sd_coeff[0b1001] +
+                                                 (0*1 - 0*5) * wfn_sd_coeff[0b0110] +
+                                                 (0*2 - 0*6) * wfn_sd_coeff[0b1010] +
+                                                 (1*6 - 5*2) * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+
+
+def test_nonorth_get_overlap_deriv():
+    """Test NonorthWavefunction.get_overap with derivatization option."""
+    test = TestNonorthWavefunction()
+    test.nelec = 2
+    test.nspin = 4
+    test.dtype = np.float64
+    test.memory = 10
+    test.assign_wfn(None)
+    test._cache_fns = {}
+    test.wfn.params = np.arange(1, 7)
+    wfn_sd_coeff = {0b0101: 1, 0b0110: 2, 0b1100: 3, 0b0011: 4, 0b1001: 5, 0b1010: 6}
+
+    # generalized
+    test.params = [np.arange(1, 17).reshape(4, 4)]
+    # 0b0101 uses [[1, 2, 3, 4],
+    #              [9, 10, 11, 12]]
+    assert np.isclose(test.get_overlap(0b0101, deriv=0), (10 * wfn_sd_coeff[0b0011] +
+                                                          11 * wfn_sd_coeff[0b0101] +
+                                                          12 * wfn_sd_coeff[0b1001]),
+                      rtol=0, atol=1e-12)
+    assert np.isclose(test.get_overlap(0b0101, deriv=6), 0, rtol=0, atol=1e-12)
+    assert np.isclose(test.get_overlap(0b0101, deriv=8),  (-2 * wfn_sd_coeff[0b0011] +
+                                                           -3 * wfn_sd_coeff[0b0101] +
+                                                           -4 * wfn_sd_coeff[0b1001]),
+                      rtol=0, atol=1e-12)
+
+    # unrestricted
+    test.clear_cache()
+    test.params = [np.array([[1, 2], [5, 6]]), np.array([[11, 12], [15, 16]])]
+    # 0b0101 uses [[1, 2, 0, 0],
+    #              [0, 0, 11, 12]]
+    assert np.isclose(test.get_overlap(0b0101, deriv=0), (0 * wfn_sd_coeff[0b0011] +
+                                                          11 * wfn_sd_coeff[0b0101] +
+                                                          12 * wfn_sd_coeff[0b1001]),
+                      rtol=0, atol=1e-12)
+    assert np.isclose(test.get_overlap(0b0101, deriv=5), (1 * wfn_sd_coeff[0b1001] +
+                                                          2 * wfn_sd_coeff[0b1010] +
+                                                          0 * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    assert np.isclose(test.get_overlap(0b0101, deriv=6), 0, rtol=0, atol=1e-12)
+    assert np.isclose(test.get_overlap(0b0101, deriv=5), (1 * wfn_sd_coeff[0b1001] +
+                                                          2 * wfn_sd_coeff[0b1010] +
+                                                          0 * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+    # 0b1100 uses [[0, 0, 11, 12],
+    #              [0, 0, 15, 16]]
+    assert np.isclose(test.get_overlap(0b1100, deriv=6), -12 * wfn_sd_coeff[0b1100],
+                      rtol=0, atol=1e-12)
+
+    # restricted (one block)
+    test.clear_cache()
+    test.params = [np.array([[1, 2], [5, 6]])]
+    # 0b0110 uses [[5, 6, 0, 0],
+    #              [0, 0, 1, 2]]
+    assert np.isclose(test.get_overlap(0b0110, deriv=0), (5 * wfn_sd_coeff[0b0101] +
+                                                          6 * wfn_sd_coeff[0b0110] +
+                                                          0 * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
+
+    # restricted (two block)
+    # 0b0101 uses [[1, 2, 0, 0],
+    #              [0, 0, 1, 2]]
+    assert np.isclose(test.get_overlap(0b0101, deriv=0), (0 * wfn_sd_coeff[0b0011] +
+                                                          2*1 * wfn_sd_coeff[0b0101] +
+                                                          2 * wfn_sd_coeff[0b1001] +
+                                                          2 * wfn_sd_coeff[0b0110] +
+                                                          0 * wfn_sd_coeff[0b1100]),
+                      rtol=0, atol=1e-12)
