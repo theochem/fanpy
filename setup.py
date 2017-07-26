b50d569cebfe1f68d9691e062bfb48608acb3cdd
Taewon D. Kim <kimt33@mcmaster.ca>
2017-07-26 19:45:11 -0400

    Create distutils setup
    So package can be installed using pip.

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-from __future__ import absolute_import, division, print_function
+from distutils.core import setup
+
+
+setup(
+    name='wfns',
+    version='0.0',
+    description='Generalized wavefunction package.',
+    url='',
+    license='MIT',
+    author='Ayers Group',
+    author_email='',
+    package_dir={'wfns': 'wfns'},
+    packages=['wfns'],
+    # test_suite='nose.collector',
+    requires=['numpy', 'scipy', 'gmpy2'],
+)
