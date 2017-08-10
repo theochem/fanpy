dac7b7e78a7386c543c2a383cc80e3fa840765f6
Taewon D. Kim <kimt33@mcmaster.ca>
2017-08-09 23:26:32 -0400

    Update __init__.py for wrapper directory

--- wfns/wrapper/__init__.py
+++ wfns/wrapper/__init__.py
@@ -1,20 +1,4 @@
 from __future__ import division, print_function
 from __future__ import absolute_import
 
-horton_mia = False
-try:
-    from . import horton
-except ImportError as error:
-    print('WARNING: Error in loading horton wrapper. Check that you have HORTON installed'
-            ' and that PYTHONPATH is set properly')
-    print(error)
-    horton_mia = True
-
-pyscf_mia = False
-try:
-    from . import pyscf
-except ImportError as error:
-    print('WARNING: Error in loading pyscf wrapper. Check that you have PySCF installed'
-            ' and that PYTHONPATH is set properly')
-    print(error)
-    pyscf_mia = True
+from . import python_wrapper
