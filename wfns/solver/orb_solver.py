9ae59bf4ae01b2e28d98dbfe311f4d8a7b499533
Taewon D. Kim <kimt33@mcmaster.ca>
2017-08-15 19:39:46 -0400

    Polish up orbital optimizer using hamiltonian
    1. Add more options
    2. Add documentation
    3. Support unrestricted and generalized orbitals
    4. Use energy methods

--- wfns/solver/orb_solver.py
+++ wfns/solver/orb_solver.py
@@ -20,6 +20,7 @@ import scipy.optimize
 import wfns.backend.slater as slater
 import wfns.backend.sd_list as sd_list
 from wfns.solver.equation_solver import optimize_wfn_variational
+from wfns.solver import energies
 from wfns.wavefunction.base_wavefunction import BaseWavefunction
 from wfns.wavefunction.composite.jacobi import JacobiWavefunction
 from wfns.hamiltonian.chemical_hamiltonian import ChemicalHamiltonian
@@ -156,8 +157,9 @@ def optimize_wfn_orbitals_jacobi(wfn, ham, wfn_solver=None, save_file=''):
         print('Orbital optimization did not converge after {0} iterations'.format(num_iterations))
 
 
-def optimize_ham_orbitals_jacobi(wfn, ham, ref_sds=None, wfn_solver=None, wfn_solver_kwargs=None,
-                                 save_file=''):
+def optimize_ham_orbitals_jacobi(wfn, ham, pspace_norm=None,
+                                 l_pspace_energy=None, r_pspace_energy=None, use_norm=True,
+                                 wfn_solver=None, wfn_solver_kwargs=None, save_file=''):
     r"""Optimize orbitals of the given hamiltonian to minimize energy using Jacobi rotations.
 
     The Jacobi rotated wavefunction, :math:`\hat{\mathbf{J}}^\dagger_{pq} \ket{\Psi}`, is
@@ -190,12 +192,29 @@ def optimize_ham_orbitals_jacobi(wfn, ham, ref_sds=None, wfn_solver=None, wfn_so
         Wavefunction that defines the state of the system (number of electrons and excited state).
     ham : ChemicalHamiltonian
         Hamiltonian that defines the system under study.
+    pspace_norm : list/tuple of int
+        Projection space for calculating the norm.
+        Default is the all of the Slater determinants of the wavefunction.
+    l_pspace_energy : list/tuple of int
+        Projection space for the left side of the energy.
+        Default is the all of the Slater determinants of the wavefunction.
+    r_pspace_energy : list/tuple of int
+        Projection space for the right side of the energy.
+        Default is the all of the Slater determinants of the wavefunction.
+    use_norm : bool
+        Flag for using norm to calculate the energy.
+        Default is True.
     wfn_solver : function(wfn, ham)
         Function for solving the wavefunction in the given Hamiltonian.
         This solver will only have access to its required arguments (`wfn` and `ham`) so all keyword
         arguments need to be added before passing the function. i.e. with `lambda wfn, ham:
         solver(wfn, ham, **kwargs)` where `kwargs` have been defined somewhere.
         Default raises NotImplementedError.
+    wfn_solver_kwargs : dict
+        Optional keyword arguments for the wfn_solver.
+    save_file : str
+        Name of the file to save the orbitals.
+        Default is no save.
 
     Returns
     -------
@@ -223,54 +242,87 @@ def optimize_ham_orbitals_jacobi(wfn, ham, ref_sds=None, wfn_solver=None, wfn_so
         raise ValueError('Wavefunction and Hamiltonian do not have the same number of spin '
                          'orbitals.')
 
-    if ref_sds is None:
-        ref_sds = sd_list.sd_list(wfn.nelec, wfn.nspatial, spin=wfn.spin, seniority=wfn.seniority)
-    else:
-        ref_sds = [slater.internal_sd(sd) for sd in ref_sds]
-
     # FIXME: not memory efficient
-    def _objective(theta, p=0, q=1):
+    def _objective(theta, p, q, use_norm=True):
+        # rotate hamiltonian
         ham.orb_rotate_jacobi((p, q), theta)
-        norm = sum(wfn.get_overlap(sd)**2 for sd in ref_sds)
-        energy = sum(wfn.get_overlap(sd) * sum(ham.integrate_wfn_sd(wfn, sd))
-                     for sd in ref_sds)
+        # calculate energy
+        if l_pspace_energy is not None and r_pspace_energy is not None:
+            energy = energies.get_energy_two_proj(wfn, ham,
+                                                  l_pspace_energy=l_pspace_energy,
+                                                  r_pspace_energy=r_pspace_energy,
+                                                  pspace_norm=pspace_norm,
+                                                  use_norm=use_norm, return_grad=False)
+        elif r_pspace_energy is None:
+            energy = energies.get_energy_one_proj(wfn, ham,
+                                                  pspace_energy=l_pspace_energy,
+                                                  pspace_norm=pspace_norm,
+                                                  use_norm=use_norm,
+                                                  return_grad=False)
+        else:
+            energy = energies.get_energy_one_proj(wfn, ham,
+                                                  pspace_energy=r_pspace_energy,
+                                                  pspace_norm=pspace_norm,
+                                                  use_norm=use_norm,
+                                                  return_grad=False)
+        # rotate hamiltonian back
         ham.orb_rotate_jacobi((p, q), -theta)
-        return energy / norm
+        return energy
+
+    # initialize (depends on orbital type)
+    if ham.orbtype == 'restricted':
+        pair_generator = it.combinations(range(ham.nspatial), 2)
+        transformation = (np.identity(ham.nspatial), )
+    elif ham.orbtype == 'unrestricted':
+        def pair_generator():
+            yield from it.combinations(range(ham.nspatial), 2)
+            yield from it.combinations(range(ham.nspatial, ham.nspin), 2)
+        transformation = (np.identity(ham.nspatial), np.identity(ham.nspatial))
+    elif ham.orbtype == 'generalized':
+        pair_generator = it.combinations(range(ham.nspin), 2)
+        transformation = (np.identity(ham.nspin), )
 
     num_iterations = 50
-    # FIXME: orb_rot is specific to restricted orbitals
-    orb_rot = np.identity(wfn.nspatial)
-    # FIXME: thetas are not necessary
-    thetas = {orbpair: 0 for orbpair in it.combinations(range(wfn.nspatial), 2)}
 
     print('Orbital optimization')
     print('{0:<5}{1:>11}{2:>15}'.format('Iter', 'wfn energy', 'orb+wfn energy'))
     for i in range(num_iterations):
         delta = 0.0
 
+        # solve wavefunction
         if wfn_solver is not None:
             if wfn_solver_kwargs is None:
                 wfn_solver_kwargs = {}
             wfn_solver(wfn, ham, **wfn_solver_kwargs)
-        wfn_energy = _objective(0.0)
-        for p, q in it.combinations(range(wfn.nspatial), 2):
+        wfn_energy = _objective(0.0, 0, 1)
+
+        # rotate orbitals
+        for p, q in pair_generator:
             res = scipy.optimize.minimize_scalar(_objective, args=(p, q), method='brent')
             if res.success:
                 delta += res.x
                 ham.orb_rotate_jacobi((p, q), res.x)
-                p_col = orb_rot[:, p]
-                q_col = orb_rot[:, q]
-                (orb_rot[:, p], orb_rot[:, q]) = (np.cos(res.x)*p_col - np.sin(res.x)*q_col,
-                                                  np.sin(res.x)*p_col + np.cos(res.x)*q_col)
-                thetas[(p, q)] += res.x
 
-        print('{0:<5}{1:>11.8f}{2:>15.8f}'.format(i, wfn_energy, _objective(0.0)))
+                # transform
+                spin_index = 0
+                if ham.orb_type == 'unrestricted':
+                    spin_index = p // ham.num_orbs
+                    p, q = p % ham.num_orbs, q % ham.num_orbs
+                p_col = transformation[spin_index][:, p]
+                q_col = transformation[spin_index][:, q]
+                (transformation[spin_index][:, p],
+                 transformation[spin_index][:, q]) = (np.cos(res.x)*p_col - np.sin(res.x)*q_col,
+                                                      np.sin(res.x)*p_col + np.cos(res.x)*q_col)
 
+        rot_energy = _objective(0.0, 0, 1)
+        print('{0:<5}{1:>11.8f}{2:>15.8f}'.format(i, wfn_energy, rot_energy))
+
+        # save file
         if save_file != '':
-            np.save(save_file, orb_rot)
+            np.save(save_file, transformation)
         # if last rotation does nothing (i.e. converged)
         if np.isclose(delta, 0.0):
-            return orb_rot, thetas, _objective(0.0)
+            return transformation, rot_energy
     # if end of loop is reached (i.e. did not converge)
     else:
         print('Orbital optimization did not converge after {0} iterations'.format(num_iterations))
