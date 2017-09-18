1fb8613d29b52a9a259976fe4bf08e6873de25c3
Taewon D. Kim <kimt33@mcmaster.ca>
2017-09-18 15:32:09 -0400

    Use consistent API for permanent evaluation
    APr2G and other geminals have fundamentally differet differentiation
    approaches (b/c they are expressed wrt different sets of parameters

--- wfns/wavefunction/geminals/test/test_base_geminal.py
+++ wfns/wavefunction/geminals/test/test_base_geminal.py
@@ -188,28 +188,21 @@ def test_gem_compute_permanent():
     test.assign_params(np.arange(45, dtype=float).reshape(3, 15))
     assert np.equal(test.compute_permanent([0, 1, 2]),
                     0*(16*32 + 17*31) + 1*(15*32 + 17*30) + 2*(15*31 + 16*30))
-    assert np.equal(test.compute_permanent((0, 1, 2), deriv_row_col=(0, 0)),
-                    16*32 + 17*31)
-    assert np.equal(test.compute_permanent((0, 1, 2), deriv_row_col=(1, 1)),
-                    0*32 + 2*30)
-    assert np.equal(test.compute_permanent((0, 1, 2), deriv_row_col=(2, 4)),
-                    0)
-    assert np.equal(test.compute_permanent((0, 1, 2), deriv_row_col=(99, 99)),
-                    0)
+    assert np.equal(test.compute_permanent((0, 1, 2), deriv=0), 16*32 + 17*31)
+    assert np.equal(test.compute_permanent((0, 1, 2), deriv=16), 0*32 + 2*30)
+    assert np.equal(test.compute_permanent((0, 1, 2), deriv=34), 0)
+    assert np.equal(test.compute_permanent((0, 1, 2), deriv=9999), 0)
     assert np.equal(test.compute_permanent((3, 4, 5)),
                     3*(19*35 + 34*20) + 4*(18*35 + 33*20) + 5*(18*34 + 33*19))
-    assert np.equal(test.compute_permanent((3, 4, 5), deriv_row_col=(2, 5)),
-                    3*19 + 18*4)
+    assert np.equal(test.compute_permanent((3, 4, 5), deriv=35), 3*19 + 18*4)
     # row inds
-    assert np.equal(test.compute_permanent(col_inds=(1, 2), row_inds=(0, 1)),
-                    1*17 + 16*2)
+    assert np.equal(test.compute_permanent(col_inds=(1, 2), row_inds=[0, 1]), 1*17 + 16*2)
     # ryser
-    assert np.equal(test.compute_permanent(col_inds=(0, 1, 2, 3), row_inds=(0, 1)),
+    assert np.equal(test.compute_permanent(col_inds=(0, 1, 2, 3), row_inds=[0, 1]),
                     (0*16 + 15*1) + (0*17 + 15*2) + (0*18 + 15*3) + (1*17 + 2*16) + (1*18 + 3*16) +
                     (2*18 + 17*3))
     # one by one matrix derivatized
-    assert np.equal(test.compute_permanent(col_inds=(0, ), row_inds=(0, ), deriv_row_col=(0, 0)),
-                    1)
+    assert np.equal(test.compute_permanent(col_inds=(0, ), row_inds=(0, ), deriv=0), 1)
 
 
 def test_gem_compute_permanent_deriv():
@@ -218,9 +211,11 @@ def test_gem_compute_permanent_deriv():
     test.assign_dtype(float)
     test.assign_nelec(6)
     test.assign_nspin(6)
+    test.assign_memory(1000)
     test.assign_orbpairs()
     test.assign_ngem(3)
     test.assign_params(np.random.rand(3, 15)*10)
+    test.load_cache()
 
     original_params = test.params[:]
     delta = np.zeros(test.params_shape)
@@ -237,7 +232,7 @@ def test_gem_compute_permanent_deriv():
             two = test.compute_permanent(range(15))
             # FIXME: not quite sure why the difference between permanent derivative and finite
             #        difference increases as the step size decreases
-            assert np.allclose(test.compute_permanent(range(15), deriv_row_col=(i, j)),
+            assert np.allclose(test.compute_permanent(range(15), deriv=15*i+j),
                                (two - one) / step, rtol=0, atol=1e-5)
 
 
