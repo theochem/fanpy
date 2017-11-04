23a51d507e62701a30ffdc89c5dcdb14e8c3f962
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-04 18:28:52 -0400

    Use ParamMask in the BaseObjective
    1. Replace attributes `param_types` and `param_ranges` with
    `param_selection`
    2. Use ParamMask methods
    3. Add type and value checks in ParamMask.load_params

--- wfns/objective/base_objective.py
+++ wfns/objective/base_objective.py
@@ -178,11 +178,30 @@ class ParamMask(abc.ABC):
         params : np.ndarray
             Masked parameters that will need to be updated onto the stored objects.
 
+        Raises
+        ------
+        TypeError
+            If the parameter is not a numpy array.
+            If the parameter is not one dimensional.
+        ValueError
+            If the number of parameters does not match the selection.
+
         """
+        if not isinstance(params, np.ndarray):
+            raise TypeError('Given parameter must be a numpy array.')
+        if len(params.shape) != 1:
+            raise TypeError('Given parameter must be one-dimensional.')
+
+        num_sel = sum(len(sel) for sel in self._masks_object_params.values())
+        if num_sel != params.size:
+            raise ValueError('Number of given parameters does not match up with the selection.')
+
         for obj, sel in self._masks_object_params:
-            new_params = obj.params
+            new_params = obj.params.flatten()
             new_params[sel] = params[self._masks_objective_params[obj]]
             obj.assign_params[new_params]
+            if isinstance(obj, BaseWavefunction):
+                obj.clear_cache()
 
     def derivative_index(self, obj, index):
         """Return the index within the objective parameters as the index within the given object.
@@ -194,7 +213,12 @@ class ParamMask(abc.ABC):
             If the selected parameter is not part of the given object, then `None` is returned.
 
         """
-        if self._masks_objective_params[obj][index]:
+        try:
+            is_active = self._masks_objective_params[obj][index]
+        except KeyError:
+            return None
+
+        if is_active:
             # index from the list of active parameters in the object
             ind_active_object = np.sum(self._masks_objective_params[obj][:index])
             # ASSUMES: indices in self._masks_object_params[obj] are ordered from smallest to
@@ -219,23 +243,14 @@ class BaseObjective(abc.ABC):
         By default, the parameter values are not stored.
         If a file name is provided, then parameters are stored upon execution of the objective
         method.
-    param_types : {'wfn', 'ham', 'wfn_components'}
-        Types of parameters that will be used by the objective.
-        Each type must be one of 'wfn', 'ham', and 'wfn_components'.
-        Default is 'wfn'.
-        Type 'wfn' means that the objective will depend on the wavefunction parameters.
-        Type 'ham' means that the objective will depend on the hamiltonian parameters.
-        Type 'wfn_components' means that the objective will depend on the parameters of the
-        components of a composite wavefunction. Note that if the composite wavefunction has
-        components that are also composite wavefunctions, then only the top layer (first layer of
-        components) will be considered.
-    param_ranges : tuple of {2-tuple of int, tuple of 2-tuple of int}
-        Start and end indices (of the objective parameters) for each parameter type.
-        If the parameter type is 'wfn_components', then each component should be associated with a
-        start and end indices, resulting in tuple of 2-tuple of ints.
+    param_selection : ParamMask
+        Selection of parameters that will be used in the objective.
+        Default selects the wavefunction parameters.
+        Any subset of the wavefunction, composite wavefunction, and Hamiltonian parameters can be
+        selected.
 
     """
-    def __init__(self, wfn, ham, tmpfile='', param_types=None):
+    def __init__(self, wfn, ham, tmpfile='', param_selection=None):
         """Initialize the objective.
 
         Parameters
@@ -250,16 +265,10 @@ class BaseObjective(abc.ABC):
             By default, the parameter values are not stored.
             If a file name is provided, then parameters are stored upon execution of the objective
             method.
-        param_types : {list/tuple, str, None}
-            Types of parameters that will be used to construct the objective.
-            Each type must be one of 'wfn', 'ham', and 'wfn_components'.
-            Default is 'wfn'.
-            Type 'wfn' means that the objective will depend on the wavefunction parameters.
-            Type 'ham' means that the objective will depend on the hamiltonian parameters.
-            Type 'wfn_components' means that the objective will depend on the parameters of the
-            components of a composite wavefunction. Note that if the composite wavefunction has
-            components that are also composite wavefunctions, then only the top layer (first layer
-            of components) will be considered.
+        param_selection : {list, tuple, ParamMask, None}
+            Selection of parameters that will be used to construct the objective.
+            Each entry is a 2-tuple of the parameter object and the numpy indexing array for the
+            active parameters.
 
         Raises
         ------
@@ -267,12 +276,9 @@ class BaseObjective(abc.ABC):
             If wavefunction is not an instance (or instance of a child) of BaseWavefunction.
             If Hamiltonian is not an instance (or instance of a child) of ChemicalHamiltonian.
             If save_file is not a string.
-            If param_types is not one of 'wfn', 'ham', and 'wfn_components' or a tuple/list of these
-            strings.
         ValueError
             If wavefunction and Hamiltonian do not have the same data type.
             If wavefunction and Hamiltonian do not have the same number of spin orbitals.
-            If param_types has repeated elements.
 
         """
         if not isinstance(wfn, BaseWavefunction):
@@ -293,41 +299,14 @@ class BaseObjective(abc.ABC):
             raise TypeError('`tmpfile` must be a string.')
         self.tmpfile = tmpfile
 
-        if param_types is None:
-            param_types = ('wfn', )
-        elif isinstance(param_types, str):
-            param_types = (param_types, )
-
-        allowed_types = ('wfn', 'ham', 'wfn_components')
-        if (not isinstance(param_types, (tuple, list)) or
-                not all(i in allowed_types for i in param_types)):
-            raise TypeError("Parameter types must be one of 'wfn', 'ham', and 'wfn_components' or a"
-                            " tuple/list of these strings.")
-        elif len(set(param_types)) != len(param_types):
-            raise ValueError('Parameter types cannot have repeated elements.')
-        self.param_types = tuple(param_types)
-
-        # store parameter range
-        # it is stored instead of generating with every use b/c it will be used often.
-        param_ranges = []
-        ind = 0
-        for ptype in self.param_types:
-            if ptype == 'wfn_components':
-                wfn_component_ranges = []
-                for wfn in self._wfn_components:
-                    nparams = wfn.nparams
-                    wfn_component_ranges.append((ind, ind+nparams))
-                    ind += nparams
-                param_ranges.append(tuple(wfn_component_ranges))
-                continue
-
-            if ptype == 'wfn':
-                nparams = self.wfn.nparams
-            elif ptype == 'ham':
-                nparams = self.ham.nparams
-            param_ranges.append((ind, ind+nparams))
-            ind += nparams
-        self.param_ranges = tuple(param_ranges)
+        if param_selection is None:
+            param_selection = ParamMask((wfn, None))
+        elif isinstance(param_selection, (list, tuple)):
+            param_selection = ParamMask(*param_selection)
+        elif not isinstance(param_selection, ParamMask):
+            raise TypeError('Selection of parameters, `param_selection`, must be a list, tuple, or '
+                            'ParamMask instance.')
+        self.param_selection = param_selection
 
     @property
     def params(self):
@@ -339,39 +318,7 @@ class BaseObjective(abc.ABC):
             Parameters of the objective.
 
         """
-        params = np.array([])
-        for ptype in self.param_types:
-            if ptype == 'wfn':
-                params = np.hstack((params, self.wfn.params.flatten()))
-            elif ptype == 'ham':
-                params = np.hstack((params, self.ham.params.flatten()))
-            elif ptype == 'wfn_components':
-                for wfn in self._wfn_components:
-                    params = np.hstack((params, self.wfn.params.flatten()))
-        return params
-
-    @property
-    def _wfn_components(self):
-        """Return the wavefunction components.
-
-        Parameters
-        ----------
-        wfn_components : list of BaseWavefunction
-            Component wavefunctions of a composite wavefunction.
-
-        Raises
-        ------
-        TypeError
-            If the provided wavefunction is not a composite wavefunction.
-
-        """
-        if isinstance(self.wfn, BaseCompositeOneWavefunction):
-            return [self.wfn.wfn]
-        elif isinstance(self.wfn, LinearCombinationWavefunction):
-            return self.wfns
-        else:
-            raise TypeError('Provide wavefunction is not a composite wavefunction. It does not have'
-                            ' any component wavefunctions.')
+        return self.param_selection.extract_params()
 
     def save_params(self, params):
         """Save the given parameters in the temporary file.
@@ -399,68 +346,7 @@ class BaseObjective(abc.ABC):
             If `params` is not a one-dimensional numpy array.
 
         """
-        if not isinstance(params, np.ndarry) or len(params.shape) != 1:
-            raise TypeError('`params` must be a one-dimensional numpy array.')
-
-        for ptype, ind_ranges in zip(self.param_types, self.param_ranges):
-            if ptype == 'wfn_components':
-                for wfn, (ind_start, ind_end) in zip(self._wfn_components, ind_ranges):
-                    wfn.assign_params(params[ind_start: ind_end].reshape(wfn.params_shape))
-                    wfn.clear_cache()
-                continue
-
-            ind_start, ind_end = ind_ranges
-            if ptype == 'wfn':
-                self.wfn.assign_params(params[ind_start: ind_end].reshape(self.wfn.params_shape))
-                self.wfn.clear_cache()
-            elif ptype == 'ham':
-                self.ham.assign_params(params[ind_start: ind_end].reshape(self.ham.params_shape))
-
-        if params.size != ind_end:
-            raise ValueError('Number of parameter does not match the parameters selected by '
-                             '`param_types`.')
-
-    def wrapped_func(self, func, param_type, *sd, deriv):
-        """Wrap a function to be derivatized with respect to the parameters of the objective.
-
-        Parameters
-        ----------
-        func : function
-            Function that will be derivatized within the context of the parameters of the objective.
-            Has an argument `sd` and keyword argument `deriv`.
-        param_type : {'wfn', 'ham', 'wfn_components', None}
-            Type of the parameter the given function uses.
-            `None` means that derivatization of the given function will always produce zero, i.e.
-            function is not dependent on the parameters used in the objective.
-        sd : {int, mpz}
-            Slater Determinant against which the overlap is taken.
-        deriv : {int, None}
-            Index of the objective parameters with respect to which the overlap is derivatized.
-            Must be provided as a keyword.
-
-        Returns
-        -------
-        val : float
-            Value of the function given arguments `sd` and `deriv`.
-
-        """
-        if deriv is None:
-            return func(*sd)
-
-        if param_type is None:
-            return 0.0
-
-        try:
-            type_ind = self.param_types.index(param_type)
-            ind_start, ind_end = self.param_ranges[type_ind]
-            if ind_start <= deriv < ind_end:
-                deriv -= ind_start
-            else:
-                raise ValueError
-        except ValueError:
-            return 0.0
-        else:
-            return func(*sd, deriv=deriv)
+        self.param_selection.load_params(params)
 
     def wrapped_get_overlap(self, sd, deriv=None):
         """Wrap 'get_overlap' to be derivatized with respect to the parameters of the objective.
@@ -479,7 +365,15 @@ class BaseObjective(abc.ABC):
             Overlap of the wavefunction.
 
         """
-        return self.wrapped_func(self.wfn.get_overlap, 'wfn', sd, deriv=deriv)
+        if deriv is None:
+            return self.wfn.get_overlap(sd)
+
+        # change derivative index
+        deriv = self.param_selection.derivative_index(self.wfn, deriv)
+        if deriv is None:
+            return 0.0
+        else:
+            return self.wfn.get_overlap(sd, deriv)
 
     def wrapped_integrate_wfn_sd(self, sd, deriv=None):
         """Wrap 'integrate_wfn_sd' to be derivatized wrt the parameters of the objective.
@@ -503,21 +397,22 @@ class BaseObjective(abc.ABC):
         derivatized with respect to the paramters of the hamiltonian and of the wavefunction.
 
         """
-        def integrate_wfn_sd(sd, wfn_deriv=None, ham_deriv=None):
-            return sum(self.ham.integrate_wfn_sd(self.wfn, sd,
-                                                 wfn_deriv=wfn_deriv, ham_deriv=ham_deriv))
+        if deriv is None:
+            return self.ham.integrate_wfn_sd(self.wfn, sd)
 
-        integral = self.wrapped_func(lambda sd, deriv: integrate_wfn_sd(sd, ham_deriv=deriv),
-                                     'ham', sd, deriv=deriv)
-        if deriv is not None:
-            # we can add these two together because one of these will be zero (a derivative index
-            # only belong to one parameter type)
-            integral += self.wrapped_func(lambda sd, deriv: integrate_wfn_sd(sd, wfn_deriv=deriv),
-                                          'wfn', sd, deriv=deriv)
-        return integral
+        # change derivative index
+        wfn_deriv = self.param_selection.derivative_index(self.wfn, deriv)
+        ham_deriv = self.param_selection.derivative_index(self.ham, deriv)
+        if wfn_deriv is not None:
+            return self.ham.integrate_wfn_sd(self.wfn, sd, wfn_deriv=wfn_deriv)
+        # b/c the integral cannot be derivatized wrt both wfn and ham
+        elif ham_deriv is not None:
+            return self.ham.integrate_wfn_sd(self.wfn, sd, ham_deriv=ham_deriv)
+        else:
+            return 0.0
 
     def wrapped_integrate_sd_sd(self, sd1, sd2, deriv=None):
-        """
+        """Wrap 'integrate_sd_sd' to be derivatized wrt the parameters of the objective.
 
         Parameters
         ----------
@@ -535,9 +430,15 @@ class BaseObjective(abc.ABC):
             Value of the integral :math:`\braket{\Phi_i | \hat{H} | \Phi_j}`.
 
         """
-        return self.wrapped_func(lambda sd1, sd2, deriv: sum(self.ham.integrate_sd_sd(sd1, sd2,
-                                                                                      deriv=deriv)),
-                                 'ham', sd1, sd2, deriv=deriv)
+        if deriv is None:
+            return self.ham.integrate_sd_sd(sd1, sd2)
+
+        # change derivative index
+        deriv = self.param_selection.derivative_index(self.ham, deriv)
+        if deriv is None:
+            return 0.0
+        else:
+            return self.ham.integrate_sd_sd(sd1, sd2, deriv=deriv)
 
     def get_energy_one_proj(self, ref, deriv=None):
         """Return the energy of the Schrodinger equation with respect to a reference wavefunction.
