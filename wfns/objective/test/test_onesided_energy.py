7ec0dc3702a1ac13a8da21161a4c0b7ba20bbfe3
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-09 08:38:14 -0500

    Change API for OneSidedEnergy
    1. Add documentation
    2. Change pspace to refwfn
    3. Remove property and private attribute pspace
    4. Fix typo

--- wfns/objective/test/test_onesided_energy.py
+++ wfns/objective/test/test_onesided_energy.py
@@ -9,32 +9,25 @@ class TestOneSidedEnergy(OneSidedEnergy):
         pass
 
 
-def test_onesided_energy_assign_pspaces():
-    """Test OneSidedEnergy.assign_pspaces."""
+def test_onesided_energy_assign_refwfn():
+    """Test OneSidedEnergy.assign_refwfn."""
     test = TestOneSidedEnergy()
     test.wfn = CIWavefunction(2, 4)
 
-    test.assign_pspace(pspace=None)
-    assert test._pspace is None
+    test.assign_refwfn(refwfn=None)
+    assert list(test.refwfn) == [0b0101, 0b0110, 0b1100, 0b0011, 0b1001, 0b1010]
 
-    test.assign_pspace(pspace=[0b0101])
-    assert test._pspace == (0b0101, )
-    test.assign_pspace(pspace=(0b0101, ))
-    assert test._pspace == (0b0101, )
+    test.assign_refwfn(refwfn=[0b0101])
+    assert test.refwfn == (0b0101, )
+    test.assign_refwfn(refwfn=(0b0101, ))
+    assert test.refwfn == (0b0101, )
+    ciwfn = CIWavefunction(2, 4)
+    test.assign_refwfn(refwfn=ciwfn)
+    assert test.refwfn == ciwfn
 
-    assert_raises(TypeError, test.assign_pspace, pspace=set([0b0101, 0b1010]))
-    assert_raises(ValueError, test.assign_pspace, pspace=[0b1101])
-    assert_raises(ValueError, test.assign_pspace, pspace=[0b10001])
-    assert_raises(ValueError, test.assign_pspace, pspace=[CIWavefunction(3, 4)])
-    assert_raises(ValueError, test.assign_pspace, pspace=[CIWavefunction(2, 6)])
-    assert_raises(TypeError, test.assign_pspace, pspace=['0101'])
-
-
-def test_onesided_energy_pspace():
-    """Test OneSidedEnergy.pspace."""
-    test = TestOneSidedEnergy()
-    test.wfn = CIWavefunction(2, 4)
-    test.assign_pspace(pspace=[0b1001])
-    assert list(test.pspace) == [0b1001]
-    test.assign_pspace(pspace=None)
-    assert list(test.pspace) == [0b0101, 0b0110, 0b1100, 0b0011, 0b1001, 0b1010]
+    assert_raises(TypeError, test.assign_refwfn, refwfn=set([0b0101, 0b1010]))
+    assert_raises(ValueError, test.assign_refwfn, refwfn=[0b1101])
+    assert_raises(ValueError, test.assign_refwfn, refwfn=[0b10001])
+    assert_raises(ValueError, test.assign_refwfn, refwfn=CIWavefunction(3, 4))
+    assert_raises(ValueError, test.assign_refwfn, refwfn=CIWavefunction(2, 6))
+    assert_raises(TypeError, test.assign_refwfn, refwfn=['0101'])
