152a33f7778bb595910ae0a406efc6d62c303bc1
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-09 09:14:57 -0500

    Add tests for objective and gradient of the energy calculations

--- wfns/objective/test/test_onesided_energy.py
+++ wfns/objective/test/test_onesided_energy.py
@@ -1,7 +1,9 @@
 """Test wfns.objective.onesided_energy."""
 from nose.tools import assert_raises
+import numpy as np
 from wfns.objective.onesided_energy import OneSidedEnergy
 from wfns.wavefunction.ci.ci_wavefunction import CIWavefunction
+from wfns.hamiltonian.chemical_hamiltonian import ChemicalHamiltonian
 
 
 class TestOneSidedEnergy(OneSidedEnergy):
@@ -31,3 +33,37 @@ def test_onesided_energy_assign_refwfn():
     assert_raises(ValueError, test.assign_refwfn, refwfn=CIWavefunction(3, 4))
     assert_raises(ValueError, test.assign_refwfn, refwfn=CIWavefunction(2, 6))
     assert_raises(TypeError, test.assign_refwfn, refwfn=['0101'])
+
+
+def test_onesided_energy_objective():
+    """Test OneSidedEnergy.objective.
+
+    The actualy values of the objective is not checked because it is the same as
+    BaseObjective.get_energy_one_proj.
+
+    """
+    wfn = CIWavefunction(2, 4)
+    ham = ChemicalHamiltonian(np.arange(4, dtype=float).reshape(2, 2),
+                              np.arange(16, dtype=float).reshape(2, 2, 2, 2))
+    test = OneSidedEnergy(wfn, ham)
+    # check assignment
+    guess = np.random.rand(6)
+    test.objective(guess)
+    assert np.allclose(wfn.params, guess)
+
+
+def test_onesided_energy_gradient():
+    """Test OneSidedEnergy.gradient.
+
+    The actualy values of the gradient is not checked because it is the same as
+    BaseObjective.get_energy_one_proj with derivatization.
+
+    """
+    wfn = CIWavefunction(2, 4)
+    ham = ChemicalHamiltonian(np.arange(4, dtype=float).reshape(2, 2),
+                              np.arange(16, dtype=float).reshape(2, 2, 2, 2))
+    test = OneSidedEnergy(wfn, ham)
+    # check assignment
+    guess = np.random.rand(6)
+    test.gradient(guess)
+    assert np.allclose(wfn.params, guess)
--- wfns/objective/test/test_twosided_energy.py
+++ wfns/objective/test/test_twosided_energy.py
@@ -36,3 +36,37 @@ def test_twosided_energy_assign_pspacess():
     assert_raises(ValueError, test.assign_pspaces, pspace_r=[0b1101])
     assert_raises(ValueError, test.assign_pspaces, pspace_r=[0b10001])
     assert_raises(TypeError, test.assign_pspaces, pspace_n=['0101'])
+
+
+def test_twosided_energy_objective():
+    """Test TwoSidedEnergy.objective.
+
+    The actualy values of the objective is not checked because it is the same as
+    BaseObjective.get_energy_two_proj.
+
+    """
+    wfn = CIWavefunction(2, 4)
+    ham = ChemicalHamiltonian(np.arange(4, dtype=float).reshape(2, 2),
+                              np.arange(16, dtype=float).reshape(2, 2, 2, 2))
+    test = TwoSidedEnergy(wfn, ham)
+    # check assignment
+    guess = np.random.rand(6)
+    test.objective(guess)
+    assert np.allclose(wfn.params, guess)
+
+
+def test_twosided_energy_gradient():
+    """Test TwoSidedEnergy.gradient.
+
+    The actualy values of the gradient is not checked because it is the same as
+    BaseObjective.get_energy_two_proj with derivatization.
+
+    """
+    wfn = CIWavefunction(2, 4)
+    ham = ChemicalHamiltonian(np.arange(4, dtype=float).reshape(2, 2),
+                              np.arange(16, dtype=float).reshape(2, 2, 2, 2))
+    test = TwoSidedEnergy(wfn, ham)
+    # check assignment
+    guess = np.random.rand(6)
+    test.gradient(guess)
+    assert np.allclose(wfn.params, guess)
