3b03cee8bd4a40b5f622b20d2d2c65e3cc5c4b6a
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-10 09:40:40 -0500

    Change API of CI solver

--- wfns/solver/ci_solver.py
+++ wfns/solver/ci_solver.py
@@ -1,69 +1,70 @@
-"""Eigenvalue/vector solver for CI wavefunctions."""
+"""Solver for CI wavefunctions."""
 from __future__ import absolute_import, division, print_function
 import numpy as np
 import scipy.linalg
 from wfns.wavefunction.ci.ci_wavefunction import CIWavefunction
-from wfns.hamiltonian.chemical_hamiltonian import ChemicalHamiltonian
+from wfns.hamiltonian.base_hamiltonian import BaseHamiltonian
 from wfns.wrapper.docstring import docstring
 
 __all__ = []
 
 
-# TODO: implement Davidson method
-# FIXME: incredibly slow implementation
-# FIXME: change return value
 @docstring(indent_level=1)
-def eigen_solve(wfn, ham, exc_lvl=0):
-    """Optimize the CI wavefunction with the given Hamiltonian using eigenvalue decomposition.
+def brute(wfn, ham, savefile=''):
+    """Solve the wavefunction by eigenvalue decomposition of the CI matrix.
 
     Parameters
     ----------
-    ci_wfn : CIWavefunction
-        Wavefunction that defines the state of the system (number of electrons and excited state).
-    ham : ChemicalHamiltonian
-        Hamiltonian that defines the system under study.
-    exc_lvl : int
-        Level of excitation of the CI wavefunction.
+    wfn : CIWavefunction
+        CI wavefunction.
+    ham : BaseHamiltonian
+        Hamiltonian.
+    savefile : str
+        File to which the eigenvectors and the eigenvalue will be saved.
+        File is saved as a numpy array where the first row corresponds to the energies and the
+        subsequent rows correspond to the coefficients for these energies.
 
     Returns
     -------
-    energy : float
-        Energy of the wavefunction.
+    eigval : np.ndarray(K,)
+        Energy of each excited state.
+    eigvec : np.ndarray(K, K)
+        CI coefficients of each excited state.
+        Column `eigvec[:, i]` corresponds to the eigenvalue `eigval[i]`.
 
     Raises
     ------
     TypeError
         If wavefunction is not an instance (or instance of a child) of CIWavefunction.
         If Hamiltonian is not an instance (or instance of a child) of ChemicalHamiltonian.
-        If exc_lvl is not a positive integer.
     ValueError
         If wavefunction and Hamiltonian do not have the same data type.
         If wavefunction and Hamiltonian do not have the same number of spin orbitals.
 
     """
-    # Preprocess variables
+    # check parameters
     if not isinstance(wfn, CIWavefunction):
         raise TypeError('Given wavefunction is not an instance of BaseWavefunction (or its child).')
-    if not isinstance(ham, ChemicalHamiltonian):
-        raise TypeError('Given Hamiltonian is not an instance of BaseWavefunction (or its child).')
-    if wfn.dtype != ham.dtype:
+    elif not isinstance(ham, BaseHamiltonian):
+        raise TypeError('Given Hamiltonian is not an instance of BaseHamiltonian (or its child).')
+    elif wfn.dtype != ham.dtype:
         raise ValueError('Wavefunction and Hamiltonian do not have the same data type.')
-    if wfn.nspin != ham.nspin:
+    elif wfn.nspin != ham.nspin:
         raise ValueError('Wavefunction and Hamiltonian do not have the same number of spin '
                          'orbitals')
-    if not (isinstance(exc_lvl, int) and exc_lvl >= 0):
-        raise TypeError('Given exc_lvl is not a positive integer')
+    elif not isinstance(savefile, str):
+        raise TypeError('The save file must be given as a string.')
 
     ci_matrix = np.zeros((wfn.nsd, wfn.nsd), dtype=wfn.dtype)
     for i, sd1 in enumerate(wfn.sd_vec):
         for j, sd2 in enumerate(wfn.sd_vec[i:]):
             ci_matrix[i, i+j] += sum(ham.integrate_sd_sd(sd1, sd2))
-    ci_matrix += ci_matrix.T - np.diag(ci_matrix.diagonal())
+    # ci_matrix += ci_matrix.T - np.diag(np.diag(ci_matrix))
 
-    eigval, eigvec = scipy.linalg.eigh(ci_matrix, lower=True, overwrite_a=True, turbo=False, type=1)
+    eigval, eigvec = scipy.linalg.eigh(ci_matrix, lower=False, overwrite_a=True, turbo=False, type=1)
     del ci_matrix
 
-    wfn.params[:] = eigvec[:, exc_lvl].flat
-    energy = eigval[exc_lvl]
+    if savefile != '':
+        np.save(savefile, np.vstack(eigval, eigvec))
 
-    return energy
+    return eigval, eigvec
