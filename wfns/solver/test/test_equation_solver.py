726bb38d532add04a644fb99f02c77d0553739c0
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-11 09:12:13 -0500

    Add cma solver to solve single equation objectives

--- wfns/solver/test/test_equation_solver.py
+++ wfns/solver/test/test_equation_solver.py
@@ -1,23 +1,16 @@
 """Test wfns.solver.equation_solver."""
-from __future__ import absolute_import, division, print_function
 from nose.tools import assert_raises
 import numpy as np
-import scipy.optimize
 from wfns.wavefunction.base_wavefunction import BaseWavefunction
 from wfns.hamiltonian.chemical_hamiltonian import ChemicalHamiltonian
+from wfns.objective.schrodinger.onesided_energy import OneSidedEnergy
+from wfns.objective.schrodinger.least_squares import LeastSquaresEquations
+from wfns.objective.schrodinger.system_nonlinear import SystemEquations
 import wfns.solver.equation_solver as equation_solver
 
 
 class TestBaseWavefunction(BaseWavefunction):
     """Base wavefunction that bypasses abc structure and overwrite properties and attributes."""
-    nelec = 4
-    nspin = 8
-    dtype = np.float64
-    params = np.random.rand(10)
-    nparams = 10
-    _spin = None
-    _seniority = None
-
     def __init__(self):
         pass
 
@@ -45,149 +38,39 @@ class TestBaseWavefunction(BaseWavefunction):
 
     @property
     def spin(self):
-        return self._spin
+        pass
 
     @property
     def seniority(self):
-        return self._seniority
+        pass
 
     @property
     def template_params(self):
-        return None
+        return 10*(np.random.rand(2) - 0.5)
 
 
-class TestChemicalHamiltonian(ChemicalHamiltonian):
-    """ChemicalHamiltonian that bypasses abc structure and overwrite properties and attributes."""
-    dtype = np.float64
-    nspin = 8
-
-    def __init__(self):
-        pass
-
-
-def test_equation_solver_initialize():
-    """Test input checks of wfns.solver.equation_solver.optimize_wfn_variational.
-
-    Note
-    ----
-    Properties and attributesof the base classes are overwritten. This means that you shouldn't use
-    the following code as a reference for setting wavefunction and hamiltonian instances
-    """
-    # check wfn and hamiltonian
-    test_wfn = TestBaseWavefunction()
-    test_ham = TestChemicalHamiltonian()
-    assert_raises(TypeError, equation_solver.optimize_wfn_variational, test_wfn, None, None, None,
-                  '', None, None)
-    assert_raises(TypeError, equation_solver.optimize_wfn_variational, None, test_ham, None, None,
-                  '', None, None)
-    test_wfn = TestBaseWavefunction()
-    test_ham = TestChemicalHamiltonian()
-    test_ham.dtype = np.complex128
-    assert_raises(ValueError, equation_solver.optimize_wfn_variational, test_wfn, test_ham,
-                  None, None, '', None, None)
-    test_wfn = TestBaseWavefunction()
-    test_ham = TestChemicalHamiltonian()
-    test_ham.nspin = 6
-    assert_raises(ValueError, equation_solver.optimize_wfn_variational, test_wfn, test_ham,
-                  None, None, '', None, None)
-
-    # save_files
-    test_wfn = TestBaseWavefunction()
-    test_ham = TestChemicalHamiltonian()
-    assert_raises(TypeError, equation_solver.optimize_wfn_variational, test_wfn, test_ham, None,
-                  None, None, None, None, None)
-
-    # solver
-    test_wfn = TestBaseWavefunction()
-    test_ham = TestChemicalHamiltonian()
-    assert_raises(TypeError, equation_solver.optimize_wfn_variational, test_wfn, test_ham, None,
-                  None, '', None, None, [])
-
-
-def trial_run_equation_solver(num_runs, rtol_num_errors, atol=None, init_guess=None,
-                              solver_type=None, solver_kwargs=None):
-    """ Runs a test multiple times, keeping track of number of errors
-
-    Parameters
-    ----------
-    num_runs : int
-        Number of runs
-    rtol_num_errors : float
-        Percentage of the runs that are allowed to fail
-    atol : float
-        Tolerance for the absolute difference with the answer
-        Default is 1e-8 (numpy default)
-    init_guess : function
-        Function that generates a (random) guess for the initial parameters
-    solver_type : function
-        Solver to use to solve the system of nonlinear equations
-    solver_kwargs : dict
-        Keyword arguments for the solver
-    """
+def test_cma_energy():
+    """Test wnfs.solver.equation_solver.cma."""
     wfn = TestBaseWavefunction()
-    wfn.nelec = 2
-    wfn.nspin = 4
-    wfn.dtype = np.float64
-    wfn.nparams = 2
-    wfn.params = 10*(np.random.rand(2) - 0.5)
     wfn._cache_fns = {}
-    ham = TestChemicalHamiltonian()
-    ham.assign_orbtype(None)
-    ham.assign_energy_nuc_nuc(None)
-    ham.assign_integrals(np.ones((2, 2)), np.ones((2, 2, 2, 2)))
-    pspace = [0b0011, 0b1100]
+    wfn.assign_nelec(2)
+    wfn.assign_nspin(4)
+    wfn.assign_dtype(float)
+    wfn.assign_params()
+    ham = ChemicalHamiltonian(np.ones((2, 2)), np.ones((2, 2, 2, 2)))
 
-    num_errors = 0
-    for i in range(num_runs):
-        try:
-            wfn.params = init_guess()
-            result = equation_solver.optimize_wfn_variational(wfn, ham, left_pspace=pspace,
-                                                              right_pspace=None, solver=solver_type,
-                                                              solver_kwargs=solver_kwargs)
-            if result['success']:
-                assert np.allclose(result['fun'], 2, atol=atol)
-                assert np.allclose((wfn.params[0] - 3)**2 * (wfn.params[1] - 2)**2, 1,
-                                   atol=atol)
-        except (AssertionError, ValueError):
-            num_errors += 1
-    if (num_runs - num_errors)/num_runs < rtol_num_errors:
-        raise AssertionError('Optimizer, {0}, given by cannot solve the problem'
-                             ''.format(solver_type))
+    results = equation_solver.cma(OneSidedEnergy(wfn, ham, refwfn=[0b0011, 0b1100]))
+    assert results['success']
+    assert np.allclose(results['energy'], 2)
+    assert np.allclose(results['function'], 2)
+    assert results['message'] == 'Following termination conditions are satisfied: tolfun: 1e-11.'
 
+    results = equation_solver.cma(LeastSquaresEquations(wfn, ham, refstate=0b0011,
+                                                        pspace=[0b0011, 0b1100]))
+    assert results['success']
+    assert np.allclose(results['energy'], 2)
+    assert np.allclose(results['function'], 0, atol=1e-7)
+    assert results['message'] == 'Following termination conditions are satisfied: ftarget: 1e-07.'
 
-def test_optimize_wfn_variational():
-    """Test wfns.solver.equation_solver.optimize_wfn_variational."""
-    # minimize
-    # bad init guess
-    trial_run_equation_solver(20, 0.5, atol=1e-8, init_guess=lambda: 10*(np.random.rand(2) - 0.5),
-                              solver_type=scipy.optimize.minimize, solver_kwargs={'jac': None})
-    trial_run_equation_solver(20, 0.5, atol=1e-8, init_guess=lambda: 10*(np.random.rand(2) - 0.5),
-                              solver_type=scipy.optimize.minimize, solver_kwargs={})
-    # less bad init guess
-    trial_run_equation_solver(20, 0.5, atol=1e-8, init_guess=lambda: 2*(np.random.rand(2) - 0.5),
-                              solver_type=scipy.optimize.minimize, solver_kwargs={'jac': None})
-    trial_run_equation_solver(20, 0.5, atol=1e-8, init_guess=lambda: 2*(np.random.rand(2) - 0.5),
-                              solver_type=scipy.optimize.minimize, solver_kwargs={})
-    # good init guess
-    trial_run_equation_solver(20, 0.5, atol=1e-8, init_guess=lambda: 0.5*(np.random.rand(2) - 0.5),
-                              solver_type=scipy.optimize.minimize, solver_kwargs={'jac': None})
-    trial_run_equation_solver(20, 0.5, atol=1e-8, init_guess=lambda: 0.5*(np.random.rand(2) - 0.5),
-                              solver_type=scipy.optimize.minimize, solver_kwargs={})
-
-    # root need equal number of projections as there are parameters
-    # cma
-    try:
-        import paraopt
-        trial_run_equation_solver(20, 0.5, atol=1e-8,
-                                  init_guess=lambda: 10*(np.random.rand(2) - 0.5),
-                                  solver_type=paraopt.cma, solver_kwargs={})
-        trial_run_equation_solver(20, 0.5, atol=1e-8,
-                                  init_guess=lambda: 2*(np.random.rand(2) - 0.5),
-                                  solver_type=paraopt.cma, solver_kwargs={})
-        trial_run_equation_solver(20, 0.5, atol=1e-8, energy_is_param=False,
-                                  init_guess=lambda: 0.5*(np.random.rand(2) - 0.5),
-                                  solver_type=paraopt.cma, solver_kwargs={})
-    except ModuleNotFoundError:
-        print('Module, paraopt, is not available.')
-
-# FIXME: need tests for the gradient
+    assert_raises(TypeError, equation_solver.cma, lambda x, y: (x-3)*(y-2) + x**3 + y**2)
+    assert_raises(ValueError, equation_solver.cma, SystemEquations(wfn, ham, refstate=0b0011))
