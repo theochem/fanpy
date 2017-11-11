354a5616371d816ed55e9508d0e76922933e9663
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-11 09:12:13 -0500

    Add cma solver to solve single equation objectives

--- wfns/solver/equation_solver.py
+++ wfns/solver/equation_solver.py
@@ -1,205 +1,94 @@
-"""Single equation solver for wavefunctions."""
+"""Solvers for single equations."""
 from __future__ import absolute_import, division, print_function
 import numpy as np
-import scipy.optimize
-from wfns.solver import energies
-from wfns.wavefunction.base_wavefunction import BaseWavefunction
-from wfns.hamiltonian.chemical_hamiltonian import ChemicalHamiltonian
-import wfns.backend.slater as slater
-import wfns.backend.sd_list as sd_list
+from wfns.objective.base_objective import BaseObjective
+from wfns.objective.schrodinger.onesided_energy import OneSidedEnergy
+from wfns.objective.schrodinger.twosided_energy import TwoSidedEnergy
+from wfns.objective.schrodinger.least_squares import LeastSquaresEquations
 from wfns.wrapper.docstring import docstring
 
 
-# FIXME: copies most of system_solver for initialization and docstring
 @docstring(indent_level=1)
-def optimize_wfn_variational(wfn, ham, left_pspace=None, right_pspace=None, ref_sds=None,
-                             save_file='', solver=None, solver_kwargs=None, norm_constrained=False):
-    r"""Optimize the wavefunction with the given Hamiltonian as a single equation for energy.
+def cma(objective, save_file='', **kwargs):
+    """Solve an equation using Covariance Matrix Adaptation Evolution Strategy.
 
-    Solves the following equation:
-
-    .. math::
-
-        \bra{\Psi}
-        \sum_{\mathbf{m} \in P_{left}} \ket{\mathbf{m}} \bra{\mathbf{m}}
-        \hat{H}
-        \sum_{\mathbf{n} \in P_{right}} \ket{\mathbf{n}} \bra{\mathbf{n}}
-        \ket{\Psi}
-        &= E \sum_{\mathbf{m} \in P_{ref}}
-        \braket{ \Psi | \mathbf{m}} \braket{\mathbf{m} | \Psi}\\
-        \sum_{\mathbf{m} \in P_{left}} \sum_{\mathbf{n} \in P_{right}}
-        \braket{\Psi | \mathbf{m}}
-        \braket{\mathbf{m} | \hat{H} | \mathbf{n}}
-        \braket{\mathbf{n} | \Psi}
-        &= E \sum_{\mathbf{m} \in P_{ref}}
-        \braket{ \Psi | \mathbf{m}} \braket{\mathbf{m} | \Psi}
-
-    where :math:`P_{left}` and :math:`P_{right}` are the set of Slater determinants for which the
-    left and right sides of the Schrodinger equation are projected.
-
-    Wavefunction is optimized by minimizing the energy.
-
-    .. math::
-
-        E =
-        \frac{
-            \sum_{\mathbf{m} \in P_{left}} \sum_{\mathbf{n} \in P_{right}}
-            \braket{\Psi | \mathbf{m}}
-            \braket{\mathbf{m} | \hat{H} | \mathbf{n}}
-            \braket{\mathbf{n} | \Psi}
-        }{
-            \sum_{\mathbf{m} \in P_{left} \cap P_{right}}
-            \braket{ \Psi | \mathbf{m}} \braket{\mathbf{m} | \Psi}
-        }
+    See module `cma` for details.
 
     Parameters
     ----------
-    wfn : BaseWavefunction
-        Wavefunction that defines the state of the system (number of electrons and excited state).
-    ham : ChemicalHamiltonian
-        Hamiltonian that defines the system under study.
-    left_pspace : {tuple/list, None}
-        Slater determinants onto which the left side of the Schrodinger equation is projected.
-        Tuple of objects that are compatible with the internal_sd format.
-        By default, the largest space is used.
-    right_pspace : {tuple/list, None}
-        Slater determinants onto which the right side of the Schrodinger equation is projected.
-        Tuple of objects that are compatible with the internal_sd format.
-        By default, the largest space is used.
-    ref_sds : {tuple/list of int, None}
-        One or more Slater determinant with respect to which the energy and the norm are calculated.
-        Default is ground state HF.
+    objective : BaseObjective
+        Instance that contains the function that will be optimized.
     save_file : str
-        Name of the numpy file that contains the wavefunction parameters of the last optimization
-        step.
-        By default, does not save.
-    energy_is_param : bool
-        Flag to control whether energy is calculated with respect to the reference Slater
-        determinants or is optimized as a parameter.
-        By default, energy is not a parameter.
-    energy_guess : float
-        Starting guess for the energy of the wavefunction.
-        By default, energy is calculated with respect to the reference Slater determinants.
-        Energy must be a parameter.
-    solver : {function, None}
-        Solver that will solve the objective function (system of equations).
-        By default scipy's least_squares function will be used.
-    solver_kwargs : dict
-        Keyword arguments for the solver.
-        In order to disable default keyword arguments, the appropriate key need to be created with
-        value `None`.
-        Default keyword arguments depend on the solver.
+        File to which the results of the optimization is saved.
+        By default, the results are not saved.
+    kwargs : dict
+        Keyword arguments to `cma.fmin`. See its documentation for details.
+        By default, 'sigma0' is set to 0.01, 'gradf' to the gradient of the objective, and 'options'
+        to `{'ftarget': 1e-7}`.
 
     Returns
     -------
-    output : dict
-        Output of the solver.
+    Dictionary with the following keys and values:
+    success : bool
+        True if optimization succeeded.
+    params : np.ndarray
+        Parameters at the end of the optimization.
+    energy : float
+        Energy after optimization.
+        Only available for objectives that are BaseSchrodinger instances.
+    message : str
+        Termination reason.
+    internal : list
+        Returned value of the `cma.fmin`.
 
     Raises
     ------
     TypeError
-        If wavefunction is not an instance (or instance of a child) of BaseWavefunction.
-        If Hamiltonian is not an instance (or instance of a child) of ChemicalHamiltonian.
-        If save_file is not a string.
-        If solver_kwargs is not a dictionary or `None`.
+        If objective is not BaseObjective instance.
     ValueError
-        If wavefunction and Hamiltonian do not have the same data type.
-        If wavefunction and Hamiltonian do not have the same number of spin orbitals.
-
-    Notes
-    -----
-    Optimized wavefunction may not be variational; especially if the left and right projection space
-    are not equal.
+        If objective has more than one equation.
 
     """
-    # Preprocess variables
-    if not isinstance(wfn, BaseWavefunction):
-        raise TypeError('Given wavefunction is not an instance of BaseWavefunction (or its '
-                        'child).')
-    if not isinstance(ham, ChemicalHamiltonian):
-        raise TypeError('Given Hamiltonian is not an instance of BaseWavefunction (or its '
-                        'child).')
-    if wfn.dtype != ham.dtype:
-        raise ValueError('Wavefunction and Hamiltonian do not have the same data type.')
-    if wfn.nspin != ham.nspin:
-        raise ValueError('Wavefunction and Hamiltonian do not have the same number of '
-                         'spin orbitals')
+    import cma
 
-    if not isinstance(save_file, str):
-        raise TypeError('save_file must be a string.')
+    if not isinstance(objective, BaseObjective):
+        raise TypeError('Objective must be a BaseObjective instance.')
+    elif objective.num_eqns != 1:
+        raise ValueError('Objective must contain only one equation.')
 
-    # FIXME: incredibly slow implementation
-    def _objective(params):
-        """Energy of the Schrodinger equation after projecting out the left and right sides."""
-        # update wavefunction
-        if not np.allclose(params, wfn.params.flat, atol=1e-12, rtol=0):
-            wfn.params = np.array(params).reshape(wfn.params_shape)
-            wfn.clear_cache()
+    if kwargs == {}:
+        kwargs = {'sigma0': 0.01, 'gradf': objective.gradient, 'options': {'ftarget': 1e-7}}
 
-        # save params
-        if save_file != '':
-            np.save('{0}_temp.npy'.format(save_file), wfn.params)
+    results = cma.fmin(objective.objective, objective.params, **kwargs)
 
-        if left_pspace is not None and right_pspace is not None:
-            return energies.get_energy_two_proj(wfn, ham, l_pspace_energy=left_pspace,
-                                                r_pspace_energy=right_pspace, pspace_norm=ref_sds,
-                                                use_norm=not norm_constrained, return_grad=False)
-        elif right_pspace is None:
-            return energies.get_energy_one_proj(wfn, ham, pspace_energy=left_pspace,
-                                                pspace_norm=ref_sds, use_norm=not norm_constrained,
-                                                return_grad=False)
-        else:
-            return energies.get_energy_one_proj(wfn, ham, pspace_energy=right_pspace,
-                                                pspace_norm=ref_sds, use_norm=not norm_constrained,
-                                                return_grad=False)
+    output = {}
+    output['success'] = results[-3] != {}
+    output['params'] = results[0]
+    output['function'] = results[1]
 
-    # gradiant
-    def _gradient(params):
-        """Gradient of the energy of the Schrodinger equation after projection."""
-        if not np.allclose(params, wfn.params.flat, atol=1e-12, rtol=0):
-            wfn.params = np.array(params).reshape(wfn.params_shape)
-            wfn.clear_cache()
+    if isinstance(objective, LeastSquaresEquations):
+        output['energy'] = objective.energy.params
+    elif isinstance(objective, (OneSidedEnergy, TwoSidedEnergy)):
+        output['energy'] = results[1]
 
-        if left_pspace is not None and right_pspace is not None:
-            return energies.get_energy_two_proj(wfn, ham, l_pspace_energy=left_pspace,
-                                                r_pspace_energy=right_pspace, pspace_norm=ref_sds,
-                                                use_norm=not norm_constrained, return_grad=True)
-        elif right_pspace is None:
-            return energies.get_energy_one_proj(wfn, ham, pspace_energy=left_pspace,
-                                                pspace_norm=ref_sds, use_norm=not norm_constrained,
-                                                return_grad=True)
-        else:
-            return energies.get_energy_one_proj(wfn, ham, pspace_energy=right_pspace,
-                                                pspace_norm=ref_sds, use_norm=not norm_constrained,
-                                                return_grad=True)
-
-    # check solver
-    if solver is None:
-        solver = scipy.optimize.minimize
-        default_kwargs = {'method': 'BFGS', 'jac': _gradient, 'options': {'gtol': 1e-8}}
-    elif solver.lower() == 'powell':
-        solver = scipy.optimize.minimize
-        default_kwargs = {'method': 'Powell', 'options': {'xtol': 1e-9, 'ftol': 1e-9}}
-    elif solver == 'cma':
-        import cma
-        solver = cma.fmin
-        default_kwargs = {'sigma0': 0.01, 'gradf': _gradient, 'options': {'ftarget': 1e-7}}
+    if output['success']:
+        output['message'] = ('Following termination conditions are satisfied:' +
+                             ''.join(' {0}: {1},'.format(key, val)
+                                     for key, val in results[-3].items()))
+        output['message'] = output['message'][:-1] + '.'
     else:
-        default_kwargs = {}
+        output['message'] = 'Optimization did not succeed.'
 
-    # check keyword arguments
-    if solver_kwargs is None:
-        solver_kwargs = {}
-    elif not isinstance(solver_kwargs, dict):
-        raise TypeError('solver_kwargs must be a dictionary or None')
+    output['internal'] = results
 
-    # overwrite default keyword arguments
-    default_kwargs.update(solver_kwargs)
-    solver_kwargs = default_kwargs
+    if save_file != '':
+        np.save(save_file, objective.all_params)
 
-    # add energy to results
-    results = solver(_objective, wfn.params.flat, **solver_kwargs)
+    return output
 
-    return results
-
-# TODO: add support for least squares problem
+    # if solver is None:
+    #     solver = scipy.optimize.minimize
+    #     default_kwargs = {'method': 'BFGS', 'jac': _gradient, 'options': {'gtol': 1e-8}}
+    # elif solver.lower() == 'powell':
+    #     solver = scipy.optimize.minimize
+    #     default_kwargs = {'method': 'Powell', 'options': {'xtol': 1e-9, 'ftol': 1e-9}}
