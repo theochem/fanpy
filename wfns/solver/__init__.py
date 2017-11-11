9884e339835167cd8e2be4ccd9a7bbe589d5276b
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-11 08:07:34 -0500

    Removev import of outdated modules

--- wfns/solver/__init__.py
+++ wfns/solver/__init__.py
@@ -1,8 +1,1 @@
 """Module for optimizing a wavefunction and/or Hamiltonian."""
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from wfns.solver import system_solver
-from wfns.solver import equation_solver
-from wfns.solver import ci_solver
-from wfns.solver import orb_solver
-from wfns.solver import energies
