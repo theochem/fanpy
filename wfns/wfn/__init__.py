9c2f8ca76228910e394bb9369f7ef1c43e6423d9
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-12 10:51:39 -0500

    Simplify __init__'s (Issue #43)
    Because only explicit absolute imports are used.

--- wfns/wfn/__init__.py
+++ wfns/wfn/__init__.py
@@ -1,7 +1,1 @@
 """Module for creating wavefunctions."""
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from wfns.wfn import base
-from wfns.wfn import composite
-from wfns.wfn import ci
-from wfns.wfn import geminal
--- wfns/wfn/geminal/__init__.py
+++ wfns/wfn/geminal/__init__.py
@@ -1,10 +1,1 @@
 """Module for creating Geminal wavefunction."""
-from wfns.wfn.geminal import base
-from wfns.wfn.geminal import rank2_approx
-from wfns.wfn.geminal import ap1rog
-from wfns.wfn.geminal import apr2g
-from wfns.wfn.geminal import apig
-# from wfns.wfn.geminal import apfsg
-# from wfns.wfn.geminal import apseqg
-from wfns.wfn.geminal import apsetg
-from wfns.wfn.geminal import apg
