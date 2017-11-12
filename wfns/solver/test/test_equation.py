640e186b46e83d219415c0f1fb6673d4e5a13bd1
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-12 10:15:35 -0500

    Change names of the modules in solver (Issue #42)

--- wfns/solver/test/test_equation.py
+++ wfns/solver/test/test_equation.py
@@ -1,4 +1,4 @@
-"""Test wfns.solver.equation_solver."""
+"""Test wfns.solver.equation."""
 from nose.tools import assert_raises
 import numpy as np
 from wfns.wavefunction.base_wavefunction import BaseWavefunction
@@ -6,7 +6,7 @@ from wfns.ham.chemical import ChemicalHamiltonian
 from wfns.objective.schrodinger.onesided_energy import OneSidedEnergy
 from wfns.objective.schrodinger.least_squares import LeastSquaresEquations
 from wfns.objective.schrodinger.system_nonlinear import SystemEquations
-import wfns.solver.equation_solver as equation_solver
+import wfns.solver.equation as equation
 
 
 class TestBaseWavefunction(BaseWavefunction):
@@ -42,7 +42,7 @@ class TestBaseWavefunction(BaseWavefunction):
 
 
 def test_cma():
-    """Test wnfs.solver.equation_solver.cma."""
+    """Test wnfs.solver.equation.cma."""
     wfn = TestBaseWavefunction()
     wfn._cache_fns = {}
     wfn.assign_nelec(2)
@@ -51,25 +51,25 @@ def test_cma():
     wfn.assign_params()
     ham = ChemicalHamiltonian(np.ones((2, 2)), np.ones((2, 2, 2, 2)))
 
-    results = equation_solver.cma(OneSidedEnergy(wfn, ham, refwfn=[0b0011, 0b1100]))
+    results = equation.cma(OneSidedEnergy(wfn, ham, refwfn=[0b0011, 0b1100]))
     assert results['success']
     assert np.allclose(results['energy'], 2)
     assert np.allclose(results['function'], 2)
     assert results['message'] == 'Following termination conditions are satisfied: tolfun: 1e-11.'
 
-    results = equation_solver.cma(LeastSquaresEquations(wfn, ham, refwfn=0b0011,
+    results = equation.cma(LeastSquaresEquations(wfn, ham, refwfn=0b0011,
                                                         pspace=[0b0011, 0b1100]))
     assert results['success']
     assert np.allclose(results['energy'], 2)
     assert np.allclose(results['function'], 0, atol=1e-7)
     assert results['message'] == 'Following termination conditions are satisfied: ftarget: 1e-07.'
 
-    assert_raises(TypeError, equation_solver.cma, lambda x, y: (x-3)*(y-2) + x**3 + y**2)
-    assert_raises(ValueError, equation_solver.cma, SystemEquations(wfn, ham, refwfn=0b0011))
+    assert_raises(TypeError, equation.cma, lambda x, y: (x-3)*(y-2) + x**3 + y**2)
+    assert_raises(ValueError, equation.cma, SystemEquations(wfn, ham, refwfn=0b0011))
 
 
 def test_minimize():
-    """Test wnfs.solver.equation_solver.minimize."""
+    """Test wnfs.solver.equation.minimize."""
     wfn = TestBaseWavefunction()
     wfn._cache_fns = {}
     wfn.assign_nelec(2)
@@ -78,16 +78,16 @@ def test_minimize():
     wfn.assign_params()
     ham = ChemicalHamiltonian(np.ones((2, 2)), np.ones((2, 2, 2, 2)))
 
-    results = equation_solver.minimize(OneSidedEnergy(wfn, ham, refwfn=[0b0011, 0b1100]))
+    results = equation.minimize(OneSidedEnergy(wfn, ham, refwfn=[0b0011, 0b1100]))
     assert results['success']
     assert np.allclose(results['energy'], 2)
     assert np.allclose(results['function'], 2)
 
-    results = equation_solver.minimize(LeastSquaresEquations(wfn, ham, refwfn=0b0011,
+    results = equation.minimize(LeastSquaresEquations(wfn, ham, refwfn=0b0011,
                                                              pspace=[0b0011, 0b1100]))
     assert results['success']
     assert np.allclose(results['energy'], 2)
     assert np.allclose(results['function'], 0, atol=1e-7)
 
-    assert_raises(TypeError, equation_solver.minimize, lambda x, y: (x-3)*(y-2) + x**3 + y**2)
-    assert_raises(ValueError, equation_solver.minimize, SystemEquations(wfn, ham, refwfn=0b0011))
+    assert_raises(TypeError, equation.minimize, lambda x, y: (x-3)*(y-2) + x**3 + y**2)
+    assert_raises(ValueError, equation.minimize, SystemEquations(wfn, ham, refwfn=0b0011))
