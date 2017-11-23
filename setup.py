9eb35bd2651eb704c19c33a4249d332b25bd3520
Taewon D. Kim <kimt33@mcmaster.ca>
2017-11-23 17:09:47 -0500

    Replace distutils with setuptools
    setuptools is the preferred module for installing python modules (has more
    functionality and is not deprecated)

--- setup.py
+++ setup.py
@@ -1,21 +1,38 @@
 """Setup script for installing the package."""
-from distutils.core import setup
+from setuptools import setup
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
 
-setup(
-    name='wfns',
-    version='0.0',
-    description='Generalized wavefunction package.',
-    url='',
-    license='GNU3',
-    author='Ayers Group',
-    author_email='',
-    package_dir={'wfns': 'wfns'},
-    packages=['wfns', 'wfns.backend', 'wfns.ham',
-              'wfns.objective', 'wfns.objective.schrodinger', 'wfns.objective.constraints',
-              'wfns.solver',
-              'wfns.wfn', 'wfns.wfn.ci', 'wfns.wfn.composite', 'wfns.wfn.geminal',
-              'wfns.wrapper'],
-    # test_suite='nose.collector',
-    requires=['numpy', 'scipy', 'gmpy2'],
-)
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='wfns',
+      version='0.0.0',
+      description='Package for solving the Schrodinger equation.',
+      long_description=long_description,
+      url='https://github.com/quantumelephant/olsens',
+      license='GNU Version 3',
+      author='Taewon D. Kim',
+      author_email='kimt33@mcmaster.ca',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers of new methods for solving the Schrodinger '
+                   'equation',
+                   'Topic :: Method Development :: Schrodinger equation',
+                   'License :: OSI Approved :: GNU Version 3',
+                   'Programming Language :: Python :: 3.6'],
+      keywords='wavefunction hamiltonian optimization',
+      package_dir={'wfns': 'wfns'},
+      packages=['wfns', 'wfns.backend', 'wfns.ham',
+                'wfns.objective', 'wfns.objective.schrodinger', 'wfns.objective.constraints',
+                'wfns.solver',
+                'wfns.wfn', 'wfns.wfn.ci', 'wfns.wfn.composite', 'wfns.wfn.geminal',
+                'wfns.wrapper'],
+      # test_suite='nose.collector',
+      install_requires=['numpy', 'scipy', 'gmpy2'],
+      extras_require={'horton': [], 'pyscf': []},
+      tests_requires=['nose', 'cardboardlint'],
+      package_data={},
+      data_files=[],
+      )
