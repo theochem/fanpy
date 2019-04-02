50a87533f3ef07b3f99a42ce53498ce53f73c977
Taewon D. Kim <kimt33@mcmaster.ca>
2019-04-02 01:36:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,101 @@
-"""Setup script for installing the package."""
-import codecs
+"""Setup for fanpy.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+Templated from:
+https://github.com/pypa/sampleproject
+"""
+
 from os import path
-from setuptools import setup
+
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
-with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='wfns',
-      version='0.0.0',
-      description='Package for solving the Schrodinger equation.',
-      long_description=long_description,
-      url='https://github.com/quantumelephant/fanpy',
-      license='GNU Version 3',
-      author='Taewon D. Kim',
-      author_email='kimt33@mcmaster.ca',
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers of new methods for solving the Schrodinger '
-                   'equation',
-                   'Topic :: Method Development :: Schrodinger equation',
-                   'License :: OSI Approved :: GNU Version 3',
-                   'Programming Language :: Python :: 3.6'],
-      keywords='wavefunction hamiltonian optimization',
-      package_dir={'wfns': 'wfns'},
-      packages=['wfns', 'wfns.backend', 'wfns.ham',
-                'wfns.objective', 'wfns.objective.schrodinger', 'wfns.objective.constraints',
-                'wfns.solver',
-                'wfns.wfn', 'wfns.wfn.ci', 'wfns.wfn.composite', 'wfns.wfn.geminal',
-                'wfns.wfn.network',
-                'wfns.wrapper'],
-      install_requires=['numpy', 'scipy', 'gmpy2'],
-      extras_require={'horton': [], 'pyscf': [], 'keras': []},
-      tests_requires=['pytest', 'cardboardlint'],
-      package_data={},
-      data_files=[],
-      )
+setup(
+    name="fanpy",
+    version="0.0.0",
+    description="A module for evaluating, differentiating, and integrating Gaussian functions.",
+    long_description=long_description,
+    # Denotes that our long_description is in Markdown; valid values are
+    # text/plain, text/x-rst, and text/markdown
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    # This should be your name or the name of the organization which owns the
+    # project.
+    author="Taewon D. Kim",
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email="david.kim.91@gmail.com",
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[
+        # How mature is this project? Common values are
+        #   2 - Pre-Alpha
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 1 - Planning",
+        # Indicate who your project is intended for
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics",
+        # Pick your license as you wish
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        # Specify the Python versions you support here.
+        # These classifiers are *not* checked by 'pip install'. See instead
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords='wavefunction hamiltonian optimization schrodinger equation quantum chemistry',
+    packages=find_packages(exclude=["docs", "tests"]),
+    python_requires=">=3.4",
+    install_requires=["numpy", "scipy", 'cma', 'gmpy2'],
+    extras_require={
+        "dev": [
+            "tox",
+            "pytest",
+            "pytest-cov",
+            "flake8",
+            "flake8-pydocstyle",
+            "flake8-import-order",
+            "pep8-naming",
+            "pylint",
+            "bandit",
+            "black",
+        ],
+        "test": ["tox", "pytest", "pytest-cov"],
+        "horton": ["horton"],
+        "pyscf": ["pyscf"],
+        "keras": ["keras"],
+    },
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    package_data={},
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+    # List additional URLs that are relevant to your project as a dict.
+    project_urls={
+        "Bug Reports": "https://github.com/theochem/gbasis/issues",
+        "Organization": "https://github.com/quantumelephant/",
+        "Source": "https://github.com/theochem/gbasis/",
+    },
+)
