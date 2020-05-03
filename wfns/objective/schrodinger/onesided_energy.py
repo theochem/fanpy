1d2b0842abbe78587c7715a10f81b1b8ba4cb65e
Taewon D. Kim <davidkim91@gmail.com>
2020-05-03 18:36:18 -0400

    Fix bug in OneSidedEnergy
    get_energy_one_proj with derivative does not work with numpy array. Underlying
    code `cext.get_energy_one_proj_deriv` has a bug

--- wfns/objective/schrodinger/onesided_energy.py
+++ wfns/objective/schrodinger/onesided_energy.py
@@ -272,4 +272,6 @@ class OneSidedEnergy(BaseSchrodinger):
         # Save params
         self.save_params()
 
-        return np.array(self.get_energy_one_proj(self.refwfn, np.arange(params.size, dtype=int)))
+        # FIXME: numpy array derivatization is wrong
+        # return np.array(self.get_energy_one_proj(self.refwfn, np.arange(params.size, dtype=int)))
+        return np.array([self.get_energy_one_proj(self.refwfn, i) for i in range(params.size)])
