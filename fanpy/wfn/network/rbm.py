498188fa2107cad3ad0058d282cf393b9f3866c8
Taewon D. Kim <davidkim91@gmail.com>
2021-04-23 20:22:54 -0400

    Add Restricted Boltzmann Machine wavefunction

---
+++ fanpy/wfn/network/rbm.py
@@ -0,0 +1,463 @@
+"""Restricted Boltzmann Machine wavefunction."""
+from fanpy.tools import slater
+from fanpy.wfn.base import BaseWavefunction
+
+import numpy as np
+
+
+class RestrictedBoltzmannMachine(BaseWavefunction):
+    def __init__(
+        self, nelec, nspin, nbath, params=None, memory=None, num_layers=1, orders=(1, 2)
+    ):
+        super().__init__(nelec, nspin, memory=memory)
+        self.nbath = nbath
+        self.orders = np.array(orders)
+        self.num_layers = num_layers
+        self._template_params = None
+        # self.enable_cache(include_derivative=True)
+        self.assign_params(params=params)
+        self.forward_cache_lin = []
+        self.forward_cache_act = []
+        # self.probable_sds = {}
+        # self.olp_threshold = 42
+
+    @property
+    def params(self):
+        return np.hstack([i.flat for i in self._params])
+
+    # @params.setter
+    # def params(self, val):
+    #     self.assign_params(val)
+
+    @property
+    def nparams(self):
+        """Return the number of wavefunction parameters.
+
+        Returns
+        -------
+        nparams : int
+            Number of parameters.
+
+        """
+        # NOTE: first layer is K x B
+        return (
+            np.sum(self.nbath * self.nspin ** self.orders) +
+            (self.num_layers - 1) * (self.nbath ** 2)
+        )
+        # NOTE: first layer is K x K and K x B
+        # return (
+        #     np.sum(self.nspin * self.nspin ** self.orders) +
+        #     self.nspin * self.nbath +
+        #     (self.num_layers - 1) * (self.nbath ** 2)
+        # )
+
+    @property
+    def params_shape(self):
+        """Return the shape of the wavefunction parameters.
+
+        Returns
+        -------
+        params_shape : tuple of int
+            Shape of the parameters.
+
+        Notes
+        -----
+        Instance must have attribut `model`.
+
+        """
+        # NOTE: first layer is K x B
+        return (
+            [(self.nbath, *(self.nspin, ) * order) for order in self.orders] +
+            (self.num_layers - 1) * [(self.nbath, self.nbath)]
+        )
+        # NOTE: first layer is K x K and K x B
+        # return (
+        #     [(self.nspin, *(self.nspin, ) * order) for order in self.orders] +
+        #     [(self.nspin, self.nbath)] +
+        #     (self.num_layers - 1) * [(self.nbath, self.nbath)]
+        # )
+
+    @property
+    def template_params(self):
+        """Return the template of the parameters of the given wavefunction.
+
+        Returns
+        -------
+        template_params : np.ndarray
+            Default parameters of the wavefunction.
+
+        Notes
+        -----
+        May depend on params_shape and other attributes/properties.
+
+        """
+        return self._template_params
+
+    def assign_template_params(self):
+        r"""Assign the intial guess for the HF ground state wavefunction.
+
+        Since the template parameters are calculated/approximated, they are computed and stored away
+        rather than generating each one on the fly.
+
+        Raises
+        ------
+        ValueError
+            If any of the layers of the model has more than one type of weights. For example, bias
+            is not allowed.
+            If the number of units `K` in the final hidden layer is greater than
+            :math:`1 + (K-N)N + \binom{K-N}{2} \binom{N}{2}`.
+
+        Notes
+        -----
+        The template parameters can only be created for networks without bias and sufficiently large
+        final hidden layer. Additionally, the produced parameters may not be a good initial guess
+        for the HF ground state.
+
+        """
+        params = []
+        # scale = 1 / np.tanh(1)
+        scale = 1 / self.activation(0)
+
+        for i, param_shape in enumerate(self.params_shape):
+            params.append(np.zeros(param_shape))
+
+            # For one layer input with different subsequent activation
+            # if i < len(self.orders):
+            #     params.append(np.zeros(param_shape))
+            # else:
+            #     params.append(np.eye(*param_shape) * scale)
+
+            # For two layer input
+            # elif i == self.orders:
+            #     params.append(np.eye(param_shape))
+            # else:
+            #     params.append(np.eye(*param_shape) * scalle)
+
+        # self.output_scale = scale
+        self.output_scale = 0.5
+        self._template_params = params
+
+    def assign_params(self, params=None, add_noise=False):
+        """Assign the parameters of the wavefunction.
+
+        Parameters
+        ----------
+        params : {np.ndarray, None}
+            Parameters of the wavefunction.
+        add_noise : bool
+            Flag to add noise to the given parameters.
+
+        Raises
+        ------
+        TypeError
+            If `params` is not a numpy array.
+            If `params` does not have data type of `float`, `complex`, `np.float64` and
+            `np.complex128`.
+            If `params` has complex data type and wavefunction has float data type.
+        ValueError
+            If `params` does not have the same shape as the template_params.
+
+        Notes
+        -----
+        Depends on dtype, template_params, and nparams.
+
+        """
+        if params is None:
+            if self._template_params is None:
+                self.assign_template_params()
+            params = self.template_params
+        if isinstance(params, np.ndarray):
+            structured_params = []
+            for param_shape in self.params_shape:
+                structured_params.append(params[:np.prod(param_shape)].reshape(*param_shape))
+                params = params[np.prod(param_shape):]
+            params = structured_params
+
+        # store parameters
+        self._params = params
+        # super().assign_params(params=params, add_noise=add_noise)
+
+        self.clear_cache()
+
+    @staticmethod
+    def activation(x):
+        # return np.tanh(x)
+        return 1 + np.exp(x)
+        # return 2 * np.cosh(x)
+
+    @staticmethod
+    def activation_deriv(x):
+        # return 1 - np.tanh(x) ** 2
+        return np.exp(x)
+        # return 2 * np.sinh(x)
+
+    def get_overlap(self, sd, deriv=None):
+        r"""Return the overlap of the wavefunction with a Slater determinant.
+
+        .. math::
+
+            \left< \mathbf{m} \middle| \Psi \right>
+
+        Parameters
+        ----------
+        sd : {int, mpz}
+            Slater Determinant against which the overlap is taken.
+        deriv : int
+            Index of the parameter to derivatize.
+            Default does not derivatize.
+
+        Returns
+        -------
+        overlap : float
+            Overlap of the wavefunction.
+
+        Raises
+        ------
+        TypeError
+            If given Slater determinant is not compatible with the format used internally.
+
+        Notes
+        -----
+        Overlaps and their derivatives are not cached.
+
+        """
+        # if no derivatization
+        if deriv is None:
+            return self._olp(sd)
+        return self._olp_deriv(sd)
+
+    def _olp(self, sd):
+        output = np.prod(self._olp_helper(sd) * self.output_scale)
+
+        # if abs(output) > self.olp_threshold:
+        #     self.probable_sds[sd] = output
+
+        return output
+
+    def _olp_helper(self, sd, cache=False):
+        """Return output of the network (before the product layer)."""
+        occ_indices = np.array(slater.occ_indices(sd))
+        occ_mask = np.zeros(self.nspin, dtype=bool)
+        occ_mask[occ_indices] = True
+        # output[np.array(slater.occ_indices(sd))] = 1
+
+        if cache:
+            self.forward_cache_act = []
+            self.forward_cache_lin = []
+
+        output = np.zeros(self.params_shape[0][0])
+        # first layer
+        for order, layer_params in zip(self.orders, self._params):
+            # NOTE: if you want to use a different operator, you would change the corresponding
+            # input here
+            # get input vector/tensor
+            input_layer = np.zeros((self.nspin, ) * order)
+            mask = occ_mask
+            for _ in range(order - 1):
+                mask = occ_mask & np.expand_dims(mask, -1)
+            input_layer[mask] = 1
+            if cache:
+                # FIXME: large memory usage
+                # store
+                self.forward_cache_act.append(input_layer.flatten())
+
+            # contraction
+            # NOTE: this assumes that the inputs are 0 or 1
+            # for _ in range(order):
+            #     layer_params = np.sum(layer_params[:, ..., occ_indices], axis=-1)
+            # if cache:
+            #     self.forward_cache_lin.append(layer_params)
+            # output += layer_params
+
+            # OR more generally (if inputs are not 0 or 1)
+            next_layer = np.sum(
+                layer_params * input_layer, axis=tuple(np.arange(1, layer_params.ndim))
+            )
+            output += next_layer
+
+        if cache:
+            for _ in self.orders:
+                self.forward_cache_lin.append(output)
+
+        output = self.activation(output)
+
+        for layer_params in self._params[len(self.orders):]:
+            if cache:
+                self.forward_cache_act.append(output)
+            output = layer_params.dot(output)
+            if cache:
+                self.forward_cache_lin.append(output)
+            output = self.activation(output)
+        return output
+
+    def _olp_deriv(self, sd):
+        grads = []
+        # FIXME: hard coded
+        dy_da_prev = np.identity(self.nbath)
+
+        # load forward cache
+        vals = self._olp_helper(sd, cache=True)
+
+        for i, layer_params in enumerate(self._params[::-1]):
+            i = len(self._params) - 1 - i
+            dy_da_curr = dy_da_prev
+            a_prev = self.forward_cache_act[i]
+            z_curr = self.forward_cache_lin[i]
+            w_curr = self._params[i]
+
+            # z_curr_i = \sum_j w_curr_ij a_prev_j
+            # a_curr_j = sigma(z_curr_i)
+            da_curr_dz_curr = self.activation_deriv(z_curr)
+            dy_dz_curr = dy_da_curr * da_curr_dz_curr
+
+            dz_curr_dw_curr = a_prev[None, None, :]
+            dy_dw_curr = dy_dz_curr[:, :, None] * dz_curr_dw_curr
+
+            if i >= len(self.orders):
+                dz_curr_da_prev = w_curr
+                dy_da_prev = dy_dz_curr.dot(dz_curr_da_prev)
+
+            grads.append(dy_dw_curr * self.output_scale)
+
+        grads = grads[::-1]
+
+        # get product of the network output that have not been derivatived
+        indices = np.arange(self.nbath)
+        indices = np.array([np.roll(indices, - i - 1, axis=0)[:-1] for i in indices])
+        other_vals = np.prod(vals[indices] * self.output_scale, axis=1)
+
+        output = []
+        for grads_layer in grads[:-1]:
+            output.append(np.sum(other_vals[:, None, None] * grads_layer, axis=0).flat)
+        output.append(np.sum(other_vals[:, None] * grads[-1], axis=0).flatten())
+        return np.hstack(output)
+
+    def get_overlaps(self, sds, deriv=None):
+        if len(sds) == 0:
+            return np.array([])
+        occ_indices = np.array([slater.occ_indices(sd) for sd in sds])
+
+        vals = np.zeros((len(sds), self.nbath))
+        vals = vals.T
+
+        self.forward_cache_act = []
+        self.forward_cache_lin = []
+
+        occ_mask = np.zeros((len(sds), self.nspin), dtype=bool)
+        occ_mask[np.arange(len(sds))[:, None], occ_indices] = True
+        for order, layer_params in zip(self.orders, self._params):
+            # NOTE: if you want to use a different operator, you would change the corresponding
+            # input here
+            # get input vector/tensor
+            input_layer = np.zeros((len(sds),) + (self.nspin, ) * order)
+            mask = occ_mask
+            for i in range(order - 1):
+                occ_mask = np.expand_dims(occ_mask, 1)
+                mask = np.expand_dims(mask, -1) & occ_mask
+            # NOTE: mask has the shape: number of sds * nspin * nspin * ...
+            input_layer[mask] = 1
+            # FIXME: larger memory usage
+            self.forward_cache_act.append(input_layer.reshape(input_layer.shape[0], -1).T)
+
+            # contraction
+            # NOTE: layer_params has shape: nbath * nspin * nspin * ...
+            # 1st dimension will be that of bath states
+            # 2nd dimension will be that of the sds
+            next_layer = np.sum(
+                layer_params[:, None] * input_layer[None],
+                axis=tuple(np.arange(2, layer_params.ndim + 1)),
+            )
+            vals += next_layer
+
+        for _ in self.orders:
+            self.forward_cache_lin.append(vals)
+        vals = self.activation(vals)
+
+        for layer_params in self._params[len(self.orders):]:
+            self.forward_cache_act.append(vals)
+            vals = layer_params.dot(vals)
+            self.forward_cache_lin.append(vals)
+            vals = self.activation(vals)
+
+        if deriv is None:
+            return np.prod(vals * self.output_scale, axis=0)
+
+        grads = []
+        # last index is for the slater determinants
+        dy_da_prev = np.identity(self.nbath)[:, :, None]
+
+        for i, layer_params in enumerate(self._params[::-1]):
+            i = len(self._params) - 1 - i
+            dy_da_curr = dy_da_prev
+            a_prev = self.forward_cache_act[i]
+            z_curr = self.forward_cache_lin[i]
+            w_curr = self._params[i]
+
+            # z_curr_i = \sum_j w_curr_ij a_prev_j
+            # a_curr_j = sigma(z_curr_i)
+            da_curr_dz_curr = self.activation_deriv(z_curr)
+            dy_dz_curr = dy_da_curr * da_curr_dz_curr
+
+            dz_curr_dw_curr = a_prev[None, None, :, :]
+            dy_dw_curr = dy_dz_curr[:, :, None, :] * dz_curr_dw_curr
+
+            if i >= len(self.orders):
+                dz_curr_da_prev = w_curr
+                dy_da_prev = np.tensordot(dy_dz_curr, dz_curr_da_prev, axes=(1, 0))
+                dy_da_prev = np.swapaxes(dy_da_prev, 1, 2)
+
+            grads.append(dy_dw_curr * self.output_scale)
+        grads = grads[::-1]
+
+        # get product of the network output that have not been derivatived
+        indices = np.arange(self.nbath)
+        indices = np.array([np.roll(indices, - i - 1, axis=0)[:-1] for i in indices])
+        other_vals = np.prod(vals[indices] * self.output_scale, axis=1)
+
+        output = []
+        for grads_layer in grads[:-1]:
+            output.append(
+                np.moveaxis(
+                    np.sum(other_vals[:, None, None] * grads_layer, axis=0), 2, 0
+                ).reshape(len(sds), -1)
+            )
+        output.append(
+            np.moveaxis(np.sum(other_vals[:, None, None, :] * grads[-1], axis=0), 2, 0).reshape(len(sds), -1)
+        )
+        return np.hstack(output)
+
+    def normalize(self, pspace=None):
+        if pspace is not None:
+            norm = sum(self.get_overlap(sd)**2 for sd in pspace)
+            print(
+                norm,
+                sorted([abs(self.get_overlap(sd)) for sd in pspace], reverse=True)[:5],
+                'norm'
+            )
+        else:
+            norm = sum(self.get_overlap(sd)**2 for sd in self.pspace_norm)
+            print(
+                norm,
+                sorted([abs(self.get_overlap(sd)) for sd in self.pspace_norm], reverse=True)[:5],
+                'norm'
+            )
+        # norm = np.sum([olp ** 2 for olp in self.probable_sds.values()])
+        # norm = max(self.probable_sds.values()) ** 2
+        self.output_scale *= norm ** (-0.5 / self.params_shape[-1][0])
+        self.clear_cache()
+
+    # def update_pspace_norm(self, refwfn=None):
+    #     if refwfn:
+    #         self.pspace_norm = refwfn
+    #     else:
+    #         # self.pspace_norm = list(self.probable_sds.keys())
+    #         self.pspace_norm.add(max(self.probable_sds.keys(), key=lambda x: self.probable_sds[x]))
+    #         # max_sd = max(self.probable_sds.keys(), key=lambda x: self.probable_sds[x])
+    #         # if self.probable_sds[max_sd] > self.get_overlap(self.probable_sds.pop()):
+    #         #     self.pspace_norm = [max_sd]
+    #     print('Adapt normalization pspace')
+    #     print(
+    #         sum(self.get_overlap(sd)**2 for sd in self.pspace_norm),
+    #         len(self.probable_sds), len(self.pspace_norm), max(self.probable_sds.values()),
+    #         'norm_pspace'
+    #     )
---
+++ tests/test_wfn_network_rbm.py
@@ -0,0 +1,255 @@
+"""Tests for the fanpy.wfn.network.rbm."""
+from fanpy.wfn.network.numpy_network import NumpyNetwork
+from fanpy.wfn.network.rbm import RestrictedBoltzmannMachine
+from fanpy.tools.sd_list import sd_list
+from fanpy.tools.slater import occ_indices
+
+import numdifftools as nd
+
+import numpy as np
+
+import pytest
+
+from utils import skip_init
+
+
+def test_init():
+    """Test RestrictedBoltzmannMachine.__init__."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    assert test.nelec == 4
+    assert test.nspin == 10
+    assert test.nbath == 5
+    assert test.orders == (1, )
+    assert test.num_layers == 2
+    assert np.allclose(test._template_params[0], np.zeros((5, 10)))
+    assert np.allclose(test._template_params[1], np.zeros((5, 5)))
+    assert np.allclose(test.params, np.hstack((np.zeros((5, 10)).flat, np.zeros((5, 5)).flat)))
+    assert test.forward_cache_lin == []
+    assert test.forward_cache_act == []
+
+
+def test_params():
+    """Test RestrictedBoltzmannMachine.params."""
+    params = np.random.rand(75)
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2, params=params)
+    assert np.allclose(test.params, params)
+
+
+def test_nparams():
+    """Test RestrictedBoltzmannMachine.nparams."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    assert test.params.size == test.nparams
+    test = RestrictedBoltzmannMachine(7, 14, 21, orders=(1, 2), num_layers=3)
+    assert test.params.size == test.nparams
+
+
+def test_params_shape():
+    """Test RestrictedBoltzmannMachine.params_shape."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    assert test.params_shape[0] == (5, 10)
+    assert test.params_shape[1] == (5, 5)
+
+    test = RestrictedBoltzmannMachine(7, 14, 21, orders=(1, 2), num_layers=3)
+    assert test.params_shape[0] == (21, 14)
+    assert test.params_shape[1] == (21, 14, 14)
+    assert test.params_shape[2] == (21, 21)
+    assert test.params_shape[3] == (21, 21)
+
+
+def test_template_params():
+    """Test RestrictedBoltzmannMachine.template_params."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    test._template_params = 23123
+    assert np.allclose(test.template_params, test._template_params)
+
+
+def test_assign_template_params():
+    """Test RestrictedBoltzmannMachine.assign_template_params."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    del test._template_params
+    assert not hasattr(test, "_template_params")
+    test.assign_template_params()
+    assert np.allclose(test._template_params[0], np.zeros((5, 10)))
+    assert np.allclose(test._template_params[1], np.zeros((5, 5)) * 0.5)
+
+    test = RestrictedBoltzmannMachine(7, 14, 21, orders=(1, 2), num_layers=3)
+    del test._template_params
+    assert not hasattr(test, "_template_params")
+    test.assign_template_params()
+    assert np.allclose(test._template_params[0], np.zeros((21, 14)))
+    assert np.allclose(test._template_params[1], np.zeros((21, 14, 14)))
+    assert np.allclose(test._template_params[2], np.zeros((21, 21)) * 0.5)
+    assert np.allclose(test._template_params[3], np.zeros((21, 21)) * 0.5)
+
+
+def test_assign_params():
+    """Test RestrictedBoltzmannMachine.assign_params."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    del test._params
+    assert not hasattr(test, "_params")
+    test.assign_params()
+    assert np.allclose(test._params[0], np.zeros((5, 10)))
+    assert np.allclose(test._params[1], np.zeros((5, 5)) * 0.5)
+
+    params = np.random.rand(test.nparams)
+    test.assign_params(params)
+    assert np.allclose(test._params[0], params[:50].reshape(5, 10))
+    assert np.allclose(test._params[1], params[50:].reshape(5, 5))
+
+
+def test_activation():
+    """Test RestrictedBoltzmannMachine.activation."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    assert test.activation(10) == np.exp(10) + 1
+    assert test.activation(0.87) == np.exp(0.87) + 1
+
+
+def test_activation_deriv():
+    """Test RestrictedBoltzmannMachine.activation_deriv."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=2)
+    grad = nd.Gradient(test.activation)(10)
+    assert np.allclose(test.activation_deriv(10), grad)
+    grad = nd.Gradient(test.activation)(0.87)
+    assert np.allclose(test.activation_deriv(0.87), grad)
+
+
+def test_olp():
+    """Test RestrictedBoltzmannMachine._olp."""
+    test = RestrictedBoltzmannMachine(4, 10, 4, orders=(1, ), num_layers=1)
+    for sd in sd_list(4, 10):
+        assert test._olp(sd) == 1
+
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+    for sd in sd_list(4, 10):
+        input_layer = np.zeros(10)
+        input_layer[np.array(occ_indices(sd))] = 1
+        assert np.allclose(
+            test._olp(sd),
+            np.prod(0.5 * test.activation(test._params[0].dot(input_layer)))
+        )
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=1)
+    for sd in sd_list(4, 10):
+        assert test._olp(sd) == 1
+
+    test.assign_params(np.random.rand(test.nparams))
+    for sd in sd_list(4, 10):
+        indices = np.array(occ_indices(sd))
+
+        input_layer1 = np.zeros(10)
+        input_layer1[indices] = 1
+        network1 = test._params[0].dot(input_layer1)
+
+        input_layer2 = np.zeros((10, 10))
+        input_layer2[indices[:, None], indices[None, :]] = 1
+        network2 = np.einsum('ijk,jk->i', test._params[1], input_layer2)
+
+        assert np.allclose(test._olp(sd), np.prod(0.5 * test.activation(network1 + network2)))
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=2)
+    for sd in sd_list(4, 10):
+        assert test._olp(sd) == 1
+
+
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+    for sd in sd_list(4, 10):
+        indices = np.array(occ_indices(sd))
+
+        input_layer1 = np.zeros(10)
+        input_layer1[indices] = 1
+        network1 = test._params[0].dot(input_layer1)
+
+        input_layer2 = np.zeros((10, 10))
+        input_layer2[indices[:, None], indices[None, :]] = 1
+        network2 = np.einsum('ijk,jk->i', test._params[1], input_layer2)
+
+        assert np.allclose(
+            test._olp(sd),
+            np.prod(
+                0.5 * test.activation(test._params[2].dot(test.activation(network1 + network2)))
+            ),
+        )
+
+
+def test_olp_deriv():
+    """Test RestrictedBoltzmannMachine._olp_deriv."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=1)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+    for sd in sd_list(4, 10):
+        def func(x):
+            test.assign_params(x)
+            return test._olp(sd)
+
+        grad = nd.Gradient(func, step=1e-8)(test.params)
+        assert np.allclose(test._olp_deriv(sd), grad)
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=1)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+    for sd in sd_list(4, 10):
+        def func(x):
+            test.assign_params(x)
+            return test._olp(sd)
+
+        grad = nd.Gradient(func, step=1e-8)(test.params)
+        assert np.allclose(test._olp_deriv(sd), grad, atol=1e-7)
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=2)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+    for sd in sd_list(4, 10):
+        def func(x):
+            test.assign_params(x)
+            return test._olp(sd)
+
+        grad = nd.Gradient(func, step=1e-8)(test.params)
+        assert np.allclose(test._olp_deriv(sd), grad, atol=1e-7)
+
+
+def test_get_overlaps():
+    """Test RestrictedBoltzmannMachine.get_overlaps."""
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=1)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+    sds = sd_list(4, 10)
+    assert np.allclose(test.get_overlaps(sds), [test._olp(sd) for sd in sds])
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=2)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+    assert np.allclose(test.get_overlaps(sds), [test._olp(sd) for sd in sds])
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=2)
+    assert np.allclose(test.get_overlaps(sds), [test._olp(sd) for sd in sds])
+
+
+def test_get_overlaps_deriv():
+    """Test RestrictedBoltzmannMachine.get_overlaps."""
+    sds = sd_list(4, 10)
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, ), num_layers=1)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+
+    def func(x):
+        test.assign_params(x)
+        return test.get_overlaps(sds)
+
+    grad = nd.Gradient(func, step=1e-8)(test.params)
+    assert np.allclose(test.get_overlaps(sds, deriv=True), grad)
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=1)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+
+    def func(x):
+        test.assign_params(x)
+        return test.get_overlaps(sds)
+
+
+    grad = nd.Gradient(func, step=1e-8)(test.params)
+    assert np.allclose(test.get_overlaps(sds, deriv=True), grad)
+
+    test = RestrictedBoltzmannMachine(4, 10, 5, orders=(1, 2), num_layers=2)
+    test.assign_params(np.random.rand(test.nparams) * 0.001)
+
+    def func(x):
+        test.assign_params(x)
+        return test.get_overlaps(sds)
+
+    grad = nd.Gradient(func, step=1e-8)(test.params)
+    assert np.allclose(test.get_overlaps(sds, deriv=True), grad, atol=1e-7)
