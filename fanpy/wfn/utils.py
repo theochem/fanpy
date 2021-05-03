491935c05efe8eee2ea70c2f07331b62f6a0c196
Taewon D. Kim <davidkim91@gmail.com>
2021-05-02 22:52:13 -0400

    Add interface from fanpy to FanCI

--- fanpy/wfn/utils.py
+++ fanpy/wfn/utils.py
@@ -1,5 +1,8 @@
 """Utility function for constructing Wavefunction instances."""
 from fanpy.wfn.base import BaseWavefunction
+from fanpy.tools import slater
+
+import numpy as np
 
 
 def wfn_factory(olp, olp_deriv, nelec, nspin, params, memory=None, assign_params=None):
@@ -143,3 +146,212 @@ def wfn_factory(olp, olp_deriv, nelec, nspin, params, memory=None, assign_params
             return self._olp_deriv(sd)[deriv]
 
     return GeneratedWavefunction(nelec, nspin, params=params, memory=memory)
+
+
+def convert_to_fanci(wfn, ham, nproj=None, proj_wfn=None, **kwargs):
+    """Covert the given wavefunction instance to that of FanCI class.
+
+    https://github.com/QuantumElephant/FanCI
+
+    Parameters
+    ----------
+    wfn : BaseWavefunction
+    ham : pyci.hamiltonian
+        PyCI Hamiltonian.
+    nproj : int, optional
+        Number of determinants in projection ("P") space.
+    wfn : pyci.doci_wfn, optional
+        If specified, this PyCI wave function defines the projection ("P") space.
+    kwargs : Any, optional
+        Additional keyword arguments for base FanCI class.
+
+    Returns
+    -------
+    new_wfn : FanCI
+
+    """
+    from typing import Any, Tuple, Union
+    import pyci
+    from fanci.fanci import FanCI
+
+    class GeneratedFanCI(FanCI):
+        """Generated FanCI wavefunction class from the fanpy wavefunction.
+
+        Does not work for composite wavefunctions.
+
+        """
+        def __init__(
+            self,
+            fanpy_wfn: BaseWavefunction,
+            ham: pyci.hamiltonian,
+            nocc: int,
+            nproj: int = None,
+            wfn: pyci.doci_wfn = None,
+            **kwargs: Any,
+        ) -> None:
+            r"""
+            Initialize the FanCI problem.
+
+            Parameters
+            ----------
+            fanpy_wfn : BaseWavefunction
+                Wavefunction from fanpy.
+            ham : pyci.hamiltonian
+                PyCI Hamiltonian.
+            nocc : int
+                Number of occupied orbitals.
+            nproj : int, optional
+                Number of determinants in projection ("P") space.
+            wfn : pyci.doci_wfn, optional
+                If specified, this PyCI wave function defines the projection ("P") space.
+            kwargs : Any, optional
+                Additional keyword arguments for base FanCI class.
+
+            """
+            if not isinstance(ham, pyci.hamiltonian):
+                raise TypeError(f"Invalid `ham` type `{type(ham)}`; must be `pyci.hamiltonian`")
+
+            # Compute number of parameters
+            # FIXME: i'm guessing that energy is always a parameter
+            nparam = wfn.nparams + 1
+
+            # Handle default nproj
+            nproj = nparam if nproj is None else nproj
+
+            # Handle default wfn (P space == single pair excitations)
+            if wfn is None:
+                wfn = pyci.doci_wfn(ham.nbasis, nocc, nocc)
+                wfn.add_excited_dets(1)
+            elif not isinstance(wfn, pyci.doci_wfn):
+                raise TypeError(f"Invalid `wfn` type `{type(wfn)}`; must be `pyci.doci_wfn`")
+            elif wfn.nocc_up != nocc or wfn.nocc_dn != nocc:
+                raise ValueError(f"wfn.nocc_{{up,dn}} does not match `nocc={nocc}` parameter")
+
+            # Initialize base class
+            FanCI.__init__(self, ham, wfn, nproj, nparam, **kwargs)
+
+            # Get results of 'searchsorted(i)' from i=0 to i=nbasis for each det. in "S" space
+            arange = np.arange(self._wfn.nbasis, dtype=pyci.c_long)
+            sspace_data = [occs.searchsorted(arange) for occs in self._sspace]
+            pspace_data = sspace_data[: self._nproj]
+
+            # Save sub-class -specific attributes
+            self._fanpy_wfn = fanpy_wfn
+            self._sspace_data = sspace_data
+            self._pspace_data = pspace_data
+
+        def compute_overlap(self, x: np.ndarray, occs_array: Union[np.ndarray, str]) -> np.ndarray:
+            r"""
+            Compute the FanCI overlap vector.
+
+            Parameters
+            ----------
+            x : np.ndarray
+                Parameter array, [p_0, p_1, ..., p_n].
+            occs_array : (np.ndarray | 'P' | 'S')
+                Array of determinant occupations for which to compute overlap. A string "P" or "S" can
+                be passed instead that indicates whether ``occs_array`` corresponds to the "P" space
+                or "S" space, so that a more efficient, specialized computation can be done for these.
+
+            Returns
+            -------
+            ovlp : np.ndarray
+                Overlap array.
+
+            """
+            if isinstance(occs_array, np.ndarray):
+                pass
+            elif occs_array == "P":
+                occs_array = self._pspace
+            elif occs_array == "S":
+                occs_array = self._sspace
+            else:
+                raise ValueError("invalid `occs_array` argument")
+
+            # FIXME: converting occs_array to slater determinants to be converted back to indices is
+            # a waste
+            # convert slater determinants
+            sds = []
+            for i, occs in enumerate(occs_array):
+                # FIXME: CHECK IF occs IS BOOLEAN OR INTEGERS
+                # convert occupation vector to sd
+                if occs.dtype == bool:
+                    sd = slater.create(0, *np.where(occs)[0])
+                else:
+                    sd = slater.create(0, *occs)
+                sds.append(sd)
+
+            # Feed in parameters into fanpy wavefunction
+            self._fanpy_wfn.assign_params(x)
+
+            # initialize
+            y = np.empty(occs_array.shape[0], dtype=pyci.c_double)
+
+            # Compute overlaps of occupation vectors
+            if hasattr(self._fanpy_wfn, "get_overlaps"):
+                y += self._fanpy_wfn.get_overlaps(sds)
+            else:
+                for i, sd in enumerate(sds):
+                    y[i] = self._fanpy_wfn.get_overlap(sd)
+            return y
+
+        def compute_overlap_deriv(
+            self, x: np.ndarray, occs_array: Union[np.ndarray, str]
+        ) -> np.ndarray:
+            r"""
+            Compute the FanCI overlap derivative matrix.
+
+            Parameters
+            ----------
+            x : np.ndarray
+                Parameter array, [p_0, p_1, ..., p_n].
+            occs_array : (np.ndarray | 'P' | 'S')
+                Array of determinant occupations for which to compute overlap. A string "P" or "S" can
+                be passed instead that indicates whether ``occs_array`` corresponds to the "P" space
+                or "S" space, so that a more efficient, specialized computation can be done for these.
+
+            Returns
+            -------
+            ovlp : np.ndarray
+                Overlap derivative array.
+
+            """
+            if isinstance(occs_array, np.ndarray):
+                pass
+            elif occs_array == "P":
+                occs_array = self._pspace
+            elif occs_array == "S":
+                occs_array = self._sspace
+            else:
+                raise ValueError("invalid `occs_array` argument")
+
+            # FIXME: converting occs_array to slater determinants to be converted back to indices is
+            # a waste
+            # convert slater determinants
+            sds = []
+            for i, occs in enumerate(occs_array):
+                # FIXME: CHECK IF occs IS BOOLEAN OR INTEGERS
+                # convert occupation vector to sd
+                if occs.dtype == bool:
+                    sd = slater.create(0, *np.where(occs)[0])
+                else:
+                    sd = slater.create(0, *occs)
+                sds.append(sd)
+
+            # Feed in parameters into fanpy wavefunction
+            self._fanpy_wfn.assign_params(x)
+
+            # Shape of y is (no. determinants, no. active parameters excluding energy)
+            y = np.zeros((occs_array.shape[0], self._nactive - self._mask[-1]), dtype=pyci.c_double)
+
+            # Compute derivatives of overlaps
+            if hasattr(self._fanpy_wfn, "get_overlaps"):
+                y += self._fanpy_wfn.get_overlaps(sds, deriv=True)
+            else:
+                for i, sd in enumerate(sds):
+                    y[i] = self._fanpy_wfn.get_overlap(
+                        sd, deriv=np.arange(self.nparam)[self._mask[:-1]]
+                    )
+            return y
+
+    return GeneratedFanCI(wfn, ham, wfn.nelec, nproj=nproj, wfn=proj_wfn, **kwargs)
